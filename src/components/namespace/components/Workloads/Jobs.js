6c42f1d21cae14e3abdc2ee7ea49384bf65d8185
StephenAraka <stevenaraka@gmail.com>
2019-06-28 14:48:28 +0300

    Add Jobs component

--- src/components/namespace/components/Workloads/Jobs.js
+++ src/components/namespace/components/Workloads/Jobs.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
 
 class Jobs extends Component {
+    constructor () {
+        super ()
+    }
+
+    jobsSucceding() {
+        const suceededJobs = 6;
+        return (
+            <div className="col-sm-6">
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Jobs Suceeded</h5>
+                        <h1 className="card-title text-center">{suceededJobs}</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    jobsFailing() {
+        const failedJobs = 1;
+        return (
+            <div className="col-sm-6">
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Jobs Failed</h5>
+                        <h1 className="card-title text-center">{failedJobs}</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+
+    renderJobs() {
+        return (
+            <div className="card parent">
+                <div className="card-header">
+                    Jobs
+                </div>
+                <div className="card-body">
+                    <div className="row">
+                        {this.jobsSucceding()}
+                        {this.jobsFailing()}
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+
     render() {
         return (
-            <div>
-                TODO: Jobs component here
-            </div>
+            this.renderJobs()
         );
     }
 }
