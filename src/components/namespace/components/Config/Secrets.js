2749efc1868afa6995f9273f9b9b3d86ee191cdd
StephenAraka <stevenaraka@gmail.com>
2019-07-01 10:06:07 +0300

    Add secrets component

--- src/components/namespace/components/Config/Secrets.js
+++ src/components/namespace/components/Config/Secrets.js
@@ -1,12 +1,97 @@
 import React, { Component } from 'react';
 
 class Secrets extends Component {
+
+    secretsArray = [
+        {
+            secretID: 1,
+            name: "trial-prometheus-node-exporter-token-9w57j",
+            type: "kubernetes.io/service-account-token",
+            age: "10 days"
+        },
+        {
+            secretID: 2,
+            name: "nfs-client-provisioner-token-xjh55",
+            type: "kubernetes.io/service-account-token",
+            age: "21 days"
+        },
+        {
+            secretID: 3,
+            name: "ambassador-token-jfkp4",
+            type: "kubernetes.io/service-account-token",
+            age: "1 month"
+        },
+        {
+            secretID: 4,
+            name: "tensorflow   ",
+            type: "kubernetes.io/tls",
+            age: "6 months"
+        }
+    ]
+
+    createTable() {
+        return (<div>
+            <table className="table table-striped" id="deployments-table">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.secretsArray.map((element) => {
+                            return (
+                                <tr key={this.secretsArray.indexOf(element)}>
+                                    <td> {element.name}</td>
+                                    <td> {element.type} </td>
+                                    <td> {element.age} </td>
+                                    <td> {this.dropDown()} </td>
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+        );
+    }
+
+    dropDown() {
+        return (
+            <div className="dropdown">
+                <div data-toggle="dropdown">
+                    <a href="#"> <span className="fa fa-ellipsis-v" aria-hidden="true"></span></a>
+                </div>
+                <div className="dropdown-menu">
+                    <button className="dropdown-item" type="button">Edit YAML</button>
+                    <button className="dropdown-item" type="button">Delete Service</button>
+                </div>
+            </div>
+        )
+    }
+
+    renderSecretsTable() {
+        return (
+            <div className="card col-sm-12">
+                <div className="card-header text-center">
+                    Secrets
+                    </div>
+                <div className="card-body">
+                    {this.createTable()}
+                </div>
+            </div>
+
+        );
+    }
+    
+    
+
     render() {
         return (
-            <div>
-                TODO: Secrets component here
-            </div>
-        );
+            this.renderSecretsTable()
+        )
     }
 }
 
