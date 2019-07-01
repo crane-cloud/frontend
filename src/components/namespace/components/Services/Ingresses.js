2968d7119c058a2ea1fd5508d421465e0aa29d63
StephenAraka <stevenaraka@gmail.com>
2019-07-01 05:35:34 +0300

    Add Ingresses component

--- src/components/namespace/components/Services/Ingresses.js
+++ src/components/namespace/components/Services/Ingresses.js
@@ -1,12 +1,82 @@
 import React, { Component } from 'react';
 
 class Ingresses extends Component {
+
+    ingressesArray = [
+        {
+            ingressID: 346,
+            name: "cm-acme-http-solver-b28fg",
+            endpoints: "-",
+            age: "9 days"
+        },
+        {
+            ingressID: 347,
+            name: "cm-acme-http-solver-85lbb",
+            endpoints: "-",
+            age: "9 days"
+        },
+        {
+            ingressID: 348,
+            name: "kuardz",
+            endpoints: "-",
+            age: "8 months"
+        },
+        {
+            ingressID: 349,
+            name: "httpbin",
+            endpoints: "-",
+            age: "8 months"
+        }
+    ]
+
+    createTable() {
+        return (<div>
+            <table className="table table-striped" id="deployments-table">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Endpoints</th>
+                        <th scope="col">Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.ingressesArray.map((element) => {
+                            return (
+                                <tr key={this.ingressesArray.indexOf(element)}>
+                                    <td> {element.name}</td>
+                                    <td> {element.endpoints} </td>
+                                    <td> {element.age} </td>
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+        );
+    }
+
+    renderIngressesTable() {
+        return (
+            <div className="card col-sm-12">
+                <div className="card-header text-center">
+                    Ingresses
+                    </div>
+                <div className="card-body">
+                    {this.createTable()}
+                </div>
+            </div>
+
+        );
+    }
+    
+    
+
     render() {
         return (
-            <div>
-                TODO: Ingresses component here
-            </div>
-        );
+            this.renderIngressesTable()
+        )
     }
 }
 
