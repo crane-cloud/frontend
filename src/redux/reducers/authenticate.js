f927da695b8ea929db1c9f925672fea4874d1edb
Colin Wagaba <ckwagaba@gmail.com>
2019-07-10 10:25:06 +0300

    setup store

---
+++ src/redux/reducers/authenticate.js
@@ -0,0 +1,12 @@
+const initialState = {
+    loggedIn: false
+};
+
+const auth = (state=initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+};
+
+export default auth;
