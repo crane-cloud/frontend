0458c4d8ed019aa4a7275aa62ac3b3c4cded7e02
ObothA <khyper234@gmail.com>
2019-07-14 12:50:26 +0300

    Add more meat on user dashabord landing

--- src/components/user_dashboard/components/MainSection/MainSection.js
+++ src/components/user_dashboard/components/MainSection/MainSection.js
@@ -3,12 +3,14 @@ import React , { Component } from "react";
 import Header from "./components/header/header";
 import LandingContent from "./components/LandingContent/landingContent";
 
+import "./mainSection.css";
+
 export default class MainSection extends Component{
     render(){
         return (
             <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
                 <Header />
-                <h2 className="text-center">Your Osprey Cloud</h2>
+                <h2 className="text-center headingMargin ">Your Osprey Cloud</h2>
                 <LandingContent />
             </main>);
     }
--- src/components/user_dashboard/components/MainSection/components/LandingContent/landingContent.js
+++ src/components/user_dashboard/components/MainSection/components/LandingContent/landingContent.js
@@ -1,18 +1,79 @@
 import React , { Component } from "react";
 
+import "./LandingContent.css";
+
 export default class LandingContent extends Component{
+    state = {
+        deploymentsListVisible : false,
+        organizationsListVisible : false,
+    }
+
+    toggleDeployments = () => {
+        const status = this.state.deploymentsListVisible;
+        this.setState({ deploymentsListVisible : !status });
+    }
+
+    toggleOrganizations = () => {
+        const status = this.state.organizationsListVisible;
+        this.setState({ organizationsListVisible : !status });
+    }
+
     render(){
         return (
             <div class="row">
                 <div class="col">
-                    
+                    <div class="card">
+                        { this.returnDeploymentsCard() }
+                    </div>
                 </div>
 
                 <div class="col">
-
+                <div class="card">
+                        { this.returnOrganizationsCard() }
+                    </div>
                 </div>
             </div>
             );
     }
+
+    returnDeploymentsCard = () => {
+        return (<div class="card-body text-center">
+            <h4 onClick={this.toggleDeployments} className="hoverCursor">
+                <span className={this.state.deploymentsListVisible ? "oi oi-minus pushPlusLeft" : "oi oi-plus pushPlusLeft"} ></span>
+                Deployments
+            <span class="badge badge-success aLittleMargin">Okay 5</span>
+                <span class="badge badge-danger aLittleMargin">Errors 2</span>
+            </h4>
+
+            <div className={this.state.deploymentsListVisible ? "deploymentsListVisible" : "deploymentsListInvisible"}>
+                <p> hshshs shshshs shshsh  <span class="badge badge-success aLittleMargin">Okay</span> </p>
+                <p> hshshs shshshs shshsh  <span class="badge badge-success aLittleMargin">Okay</span> </p>
+                <p> hshshs shshshs shshsh  <span class="badge badge-success aLittleMargin">Okay</span> </p>
+                <p> hshshs shshshs shshsh  <span class="badge badge-success aLittleMargin">Okay</span> </p>
+                <p> hshshs shshshs shshsh  <span class="badge badge-danger aLittleMargin">Check Error</span> </p>
+                <p> hshshs shshshs shshsh  <span class="badge badge-danger aLittleMargin">Check Error</span> </p>
+            </div>
+        </div>)
+    } 
+
+    returnOrganizationsCard = () => {
+        return (<div class="card-body text-center">
+            <h4 onClick={this.toggleOrganizations} className="hoverCursor">
+                <span className={this.state.organizationsListVisible ? "oi oi-minus pushPlusLeft" : "oi oi-plus pushPlusLeft"} ></span>
+                Organizations
+            <span class="badge badge-success aLittleMargin">Okay 5</span>
+                <span class="badge badge-danger aLittleMargin">Errors 2</span>
+            </h4>
+
+            <div className={this.state.organizationsListVisible ? "organizationsListVisible" : "organizationsListInvisible"}>
+                <p> hshshs shshshs shshsh  <span class="badge badge-success aLittleMargin">Okay</span> </p>
+                <p> hshshs shshshs shshsh  <span class="badge badge-success aLittleMargin">Okay</span> </p>
+                <p> hshshs shshshs shshsh  <span class="badge badge-success aLittleMargin">Okay</span> </p>
+                <p> hshshs shshshs shshsh  <span class="badge badge-success aLittleMargin">Okay</span> </p>
+                <p> hshshs shshshs shshsh  <span class="badge badge-danger aLittleMargin">Check Error</span> </p>
+                <p> hshshs shshshs shshsh  <span class="badge badge-danger aLittleMargin">Check Error</span> </p>
+            </div>
+        </div>)
+    } 
 
 }
