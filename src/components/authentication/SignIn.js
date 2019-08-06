349cce4dfe275586ce1467ac64b247ce5523c0a8
Colin Wagaba <ckwagaba@gmail.com>
2019-08-06 23:40:26 +0300

    fix login

--- src/components/authentication/SignIn.js
+++ src/components/authentication/SignIn.js
@@ -1,82 +1,90 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import "../../assets/css/signin.css";
 
-import loginSuccess from '../../redux/actions/auth/loginSuccess';
-import axios from 'axios';
-import { BASE_URL } from '../../config';
+import loginSuccess from "../../redux/actions/auth/loginSuccess";
+import axios from "axios";
+import { BASE_URL } from "../../config";
 
 class SignInForm extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: '',
-      password: ''
-    }
+      email: "",
+      password: ""
+    };
   }
 
-  handleChange = (event) => {
+  handleChange = event => {
     this.setState({
       [event.target.name]: event.target.value
     });
-  }
+  };
 
-  handleSubmit = (event) => {
+  handleSubmit = event => {
     event.preventDefault();
-    // console.log(this.state);
     /**
      * make api call
      */
-    axios(BASE_URL + '/login', {
-      method: 'POST',
-      data: this.state.data,
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(response => {
-    // if (response.status === 200 && response.data.jwt && response.data.expireAt) {
-    //   let jwt = response.data.jwt;
-    //   let expire_at = response.data.expireAt;
-
-    //   localStorage.setItem("access_token", jwt);
-    //   localStorage.setItem("expire_at", expire_at);
-
-    // dispatch action on success
-      console.log(response);
-
-      loginSuccess({
+    axios
+      .post(BASE_URL + "/login", {
+        ...this.state
+      })
+      .then(response => {
+        // dispatch action on success
+        loginSuccess({
           user: this.state,
           accessToken: response.access_token
-        })
-      }).catch(error => {
+        });
+      })
+      .catch(error => {
         console.log(error);
       });
-    }
+  };
 
   render() {
     return (
       <div className="FormCenter">
-
         <form className="FormFields" onSubmit={this.handleSubmit}>
-
           <div className="FormField">
-            <label className="FormField__Label" htmlFor="email">E-mail Address</label>
-            <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange} />
+            <label className="FormField__Label" htmlFor="email">
+              E-mail Address
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="FormField__Input"
+              placeholder="Enter your email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
           </div>
 
           <div className="FormField">
-            <label className="FormField__Label" htmlFor="name">Password</label>
-            <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={this.state.password} onChange={this.handleChange} />
+            <label className="FormField__Label" htmlFor="name">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="FormField__Input"
+              placeholder="Enter your password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
           </div>
 
           <div className="FormField">
-            <button className="FormField__Button mr-20">Sign In</button><Link to="/register" className="FormField__Link">Create an account</Link>
+            <button className="FormField__Button mr-20">Sign In</button>
+            <Link to="/register" className="FormField__Link">
+              Create an account
+            </Link>
           </div>
-
         </form>
-
       </div>
     );
   }
