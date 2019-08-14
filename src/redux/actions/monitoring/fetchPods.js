b6452c3390c5d4724a0f557eb28f5dfdff5cad28
StephenAraka <stevenaraka@gmail.com>
2019-08-14 17:10:00 +0300

    feature(admin):render pods table

--- src/redux/actions/monitoring/fetchPods.js
+++ src/redux/actions/monitoring/fetchPods.js
@@ -2,7 +2,8 @@ import {
     FETCH_PODS_RUNNING,
     FETCH_PODS_PENDING,
     FETCH_PODS_SUCCESS,
-    FETCH_PODS_FAILED
+    FETCH_PODS_FAILED,
+    FETCH_PODS_TABLE
 } from '../actionTypes';
 
 import axios from 'axios';
@@ -11,6 +12,18 @@ import { BASE_URL, PROXY_URL } from "../../../config";
 // export function fetchPosts() {
 //     return function(dispatch) {
 // in ES6 we'd write this stuff (above) like this (below)
+
+export const fetchPodsTable = () => dispatch => {
+    // this is where we create our fetch.. the one to be used in the jobs component
+    const apiRoute = `${BASE_URL}/pods`;
+
+    axios.get(PROXY_URL + apiRoute)
+        .then(response => dispatch({
+            type: FETCH_PODS_TABLE,
+            payload: response.data.items
+        }))
+        .catch(error => console.log("Can't access " + apiRoute, error));
+}
 
 export const fetchPodsRunning = () => dispatch => {
     // this is where we create our fetch.. the one to be used in the jobs component
