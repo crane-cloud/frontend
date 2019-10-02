d22eaa466365992a71427335a4665a0208f46e8d
obotha <khyper234@gmail.com>
2019-10-02 11:48:20 +0300

    Add new namespace to an organisation

--- src/components/authentication/SignIn.js
+++ src/components/authentication/SignIn.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { connect } from "react-redux";
-import { Redirect, withRouter, Link } from "react-router-dom";
-
-import logOutAction from "../../redux/actions/auth/logoutAction";
+import { connect } from 'react-redux';
+import { Redirect, withRouter, Link } from 'react-router-dom';
 import axios from 'axios';
+
+import logOutAction from '../../redux/actions/auth/logoutAction';
 import { BASE_URL } from '../../config';
 import loginSuccess from '../../redux/actions/auth/loginSuccess';
 import HeaderComponent from '../homepage/header';
@@ -18,84 +18,84 @@ class SignInForm extends Component {
       email: '',
       password: '',
       submitButtonValue: 'Sign In',
-      buttonClass : 'form-field-button',
-      loginError : ''
+      buttonClass: 'form-field-button',
+      loginError: ''
     };
   }
 
-  handleChange = event => {
+  handleChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value,
       loginError: ''
     });
   };
 
-  handleSubmit = event => {
+  handleSubmit = (event) => {
     event.preventDefault();
     this.setState({
-      submitButtonValue: "Processing ....",
+      submitButtonValue: 'Processing ....',
       buttonClass: 'form-field-button-processing'
-    })
+    });
     /**
      * make api call
      */
     const { email, password } = this.state;
     const payload = { email, password };
 
-    axios.post(BASE_URL + '/login', payload)
-        .then((response) => {
-            if(response.status === 200){
-              loginSuccess({ accessToken: response.data.access_token });
-            }
-          })
-          .catch((loginError) => {
-            if (loginError.response && loginError.response.status && loginError.response.status === 401) {
-              this.setState({
-                password: '',
-                submitButtonValue: 'Sign In',
-                buttonClass : 'form-field-button',
-                loginError: 'Wrong email or password'
-              });
-            } else if(loginError.response && loginError.response.data && loginError.response.data.message){
-            this.setState({
-              password: '',
-              submitButtonValue: 'Sign In',
-              buttonClass : 'form-field-button',
-              loginError: loginError.response.data.message
-            });
-            } else if (loginError.response && loginError.response.statusText) {
-              this.setState({
-                password: '',
-                submitButtonValue: 'Sign In',
-                buttonClass : 'form-field-button',
-                loginError: `Error occured: ${loginError.response.statusText}. Please try again `
-              });
-            }else {
-              this.setState({
-                password: '',
-                submitButtonValue: 'Sign In',
-                buttonClass : 'form-field-button',
-                loginError: `Error occured: ${loginError.message}. Please try again`
-              });
-            }
-          })
+    axios.post(`${BASE_URL}/login`, payload)
+      .then((response) => {
+        if (response.status === 200) {
+          loginSuccess({ accessToken: response.data.access_token });
+        }
+      })
+      .catch((loginError) => {
+        if (loginError.response && loginError.response.status && loginError.response.status === 401) {
+          this.setState({
+            password: '',
+            submitButtonValue: 'Sign In',
+            buttonClass: 'form-field-button',
+            loginError: 'Wrong email or password'
+          });
+        } else if (loginError.response && loginError.response.data && loginError.response.data.message) {
+          this.setState({
+            password: '',
+            submitButtonValue: 'Sign In',
+            buttonClass: 'form-field-button',
+            loginError: loginError.response.data.message
+          });
+        } else if (loginError.response && loginError.response.statusText) {
+          this.setState({
+            password: '',
+            submitButtonValue: 'Sign In',
+            buttonClass: 'form-field-button',
+            loginError: `Error occured: ${loginError.response.statusText}. Please try again `
+          });
+        } else {
+          this.setState({
+            password: '',
+            submitButtonValue: 'Sign In',
+            buttonClass: 'form-field-button',
+            loginError: `Error occured: ${loginError.message}. Please try again`
+          });
+        }
+      });
   };
 
+  // eslint-disable-next-line class-methods-use-this
   componentDidMount() {
     /* dispatch logout action after call to /login */
-    logOutAction()
+    logOutAction();
   }
 
 
   displayLoginError = (loginError) => {
-    if(loginError){
+    if (loginError) {
       return (
         <div className="alert alert-danger text-center">
-        { loginError }
-        </div> )
-    } else {
-      return;
+          {loginError}
+        </div>);
     }
+    return undefined;
   }
 
   render() {
@@ -108,13 +108,13 @@ class SignInForm extends Component {
 
     return (
       <>
-          <div className="home-container">
+        <div className="home-container">
           <HeaderComponent />
         </div>
         <div className="auth-form">
           <form onSubmit={this.handleSubmit}>
 
-            { this.displayLoginError(loginError) }
+            {this.displayLoginError(loginError)}
 
             <div className="form-title">
               Sign In
@@ -155,7 +155,7 @@ class SignInForm extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    loggedIn : state.auth.loggedIn
+    loggedIn: state.auth.loggedIn
   }
 }
 
