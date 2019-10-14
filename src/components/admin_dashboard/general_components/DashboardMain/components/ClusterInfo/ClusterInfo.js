0eaea0531764776fb72505df60274318e430311e
StephenAraka <stevenaraka@gmail.com>
2019-10-14 11:09:01 +0300

    refactor code: /modalComponents

--- src/components/admin_dashboard/general_components/DashboardMain/components/ClusterInfo/ClusterInfo.js
+++ src/components/admin_dashboard/general_components/DashboardMain/components/ClusterInfo/ClusterInfo.js
@@ -1,16 +1,17 @@
+/* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
-import ClusterHealth from "./ClusterHealth";
-import Containers from "./Containers";
+import ClusterHealth from './ClusterHealth';
+import Containers from './Containers';
 
 class ClusterInfo extends Component {
-    render() {
-        return (
-            <div className="col-md-9 ml-sm-auto col-lg-10 px-4">
-                <ClusterHealth />
-                <Containers />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="col-md-9 ml-sm-auto col-lg-10 px-4">
+        <ClusterHealth />
+        <Containers />
+      </div>
+    );
+  }
 }
 
 export default ClusterInfo;
