4fd2d9c06349439be4d101f81d095b54a7b73ded
Derrick Sekidde <kidricederek@gmail.com>
2020-01-16 11:41:46 +0300

    feat(User Dash): Logout functionality - Create logout functionality [finishes #4321]

--- src/redux/actions/auth/logoutAction.js
+++ src/redux/actions/auth/logoutAction.js
@@ -2,8 +2,11 @@ import store from '../../../store';
 import * as ACTIONTYPES from '../actionTypes';
 
 const logOutAction = () => {
+  sessionStorage.removeItem('creds')
   store.dispatch({
+    
     type: ACTIONTYPES.LOGOUT
+
   });
 };
 
