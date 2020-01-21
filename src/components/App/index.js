32b2d1e14ecb0629f823962da4f5da9a7ea496a0
Derrick Sekidde <kidricederek@gmail.com>
2020-01-21 14:31:02 +0300

    Add styling to resource card

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import ResourceCard from '../ResourceCard';
 
+/* These below are for showing purposes */
+
+const title = "Nodes";
+const resourceNumber = 7;
 function App() {
-  return <div></div>;
+  return <div>
+    <ResourceCard title={title} resourceNumber={resourceNumber}/>
+  </div>;
 }
 
 export default App;
--- src/components/ResourceCard/index.js
+++ src/components/ResourceCard/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './ResourceCard.css';
 
 const ResourceCard = (props) => (
     <div className="card">
       <div className="cardHeader">{props.title}</div>
-      <div className="resourceDigit">{props.resourceNumber}</div>
+      <div className="resourceDigit"><h1>{props.resourceNumber}</h1></div>
     </div>
   )
 
