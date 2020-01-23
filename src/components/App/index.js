b2a03c5c66c816536a902be08e5556179b0cc815
Derrick Sekidde <kidricederek@gmail.com>
2020-01-23 10:25:45 +0300

    Work on PR feedback.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import Footer from '../Footer/';
+// import Footer from '../Footer/';
 
 function App() {
-  return <div><Footer /></div>;
+  return <div></div>;
 }
 
 export default App;
