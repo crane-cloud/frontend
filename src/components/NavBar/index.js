7de30ec7834b964d4150a1be03d683cfe91112fb
Derrick Sekidde <kidricederek@gmail.com>
2020-01-29 10:28:21 +0300

    Align items in list.

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -35,6 +35,29 @@ const NavBar = () => {
           </nav>
         </div>
       </section>
+      <div class="nav">
+        <input type="checkbox" id="nav-check">
+        <div class="nav-header">
+          <div class="nav-title">
+            JoGeek
+          </div>
+        </div>
+        <div class="nav-btn">
+          <label for="nav-check">
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+        </div>
+        
+        <div class="nav-links">
+          <a href="//github.io/jo_geek" target="_blank">Github</a>
+          <a href="http://stackoverflow.com/users/4084003/" target="_blank">Stackoverflow</a>
+          <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">LinkedIn</a>
+          <a href="https://codepen.io/jo_Geek/" target="_blank">Codepen</a>
+          <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">JsFiddle</a>
+        </div>
+      </div>
       {/* <div className="Header">
         <Link className="Link" to="/">
           <img className="Logo" src={logo} alt="Crane_Cloud" />
