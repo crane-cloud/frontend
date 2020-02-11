5d2d2873619b8a0ce82d688eb457115d70aa771f
Derrick Sekidde <kidricederek@gmail.com>
2020-02-11 16:28:11 +0300

    refactor logo

--- src/components/AdminDashboard/index.js
+++ src/components/AdminDashboard/index.js
@@ -4,11 +4,13 @@ import Footer from '../Footer/';
 import ClusterResources from '../ClusterResources/';
 import NavBar from '../NavBar/';
 
+
 class AdminDashboard extends React.Component{
   render(){
     return (
       <div>
         <NavBar />
+        
         <div className="Disposable">
           <div><ClusterResources /></div>
         </div>
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,6 +4,7 @@ import logo from '../../assets/images/logo.png';
 import PROFILE from '../../assets/images/profile.svg';
 import './NavBar.css';
 
+
 const namespaces = [
   {
     "name": "Cranecloud-m1",
@@ -71,7 +72,7 @@ const NavBar = () => {
           <Link to="/contact">Contact</Link>
           <Link to="/services">Services</Link>
           <Link to="/login">Sign In</Link> */}
-          <div className="Logged-In">
+          {/* <div className="Logged-In">
             <div className="Nav-dropdown Drop Namespaces">
                 <select>
                   <option value="">Namespaces</option>
@@ -89,7 +90,7 @@ const NavBar = () => {
                 <Link to="/logout">Sign Out</Link>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </React.Fragment>
