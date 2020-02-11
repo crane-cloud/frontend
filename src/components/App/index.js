215794370178ce4271fd02496406147dcb0acb99
MutegekiHenry <henrymutegeki117@gmail.com>
2020-02-11 20:04:28 +0300

    Button for creating resources created

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,49 +1,19 @@
 import React from 'react';
 import './App.css';
 import Footer from '../Footer/';
-import Content from '../ResourceCard/';
+import Content from '../ResourceCard/Example';
 import ClusterResources from '../ClusterResources/';
-import Button from '../ButtonComponent';
 
 /* These below are for showing purposes */
 /* THIS IS GOING TO BE REMOVED IN THE DASHBOARD FEATURE */
 
-// LogIn a user
-const logIn = () =>{
-  alert ("User is logged in Successfully")
-}
-
-// Register a user
-const registerUser = () =>{
-  alert ("User is registered in Successfully")
-}
-
-const loginName="Login"
-
-
-const App = () => {
-  return (
-  <div>
-    {/* login button */}
-    <Button
-      buttonName={loginName} 
-      onClick={logIn}
-    />
-
-     {/* Register button */}
-     <Button 
-      buttonName="Register"
-      onClick= {registerUser}
-      />
-
-
-    
+function App() {
+  return <div>
     <div className="Disposable">
       <div><ClusterResources /></div>
     </div>
     <Footer />
   </div>
-  );
 }
 
 export default App;
--- src/components/ButtonComponent/index.js
+++ src/components/ButtonComponent/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {ReactComponent as ButtonPlus} from '../../assets/images/buttonplus.svg'
 import './ButtonComponent.css'
 
- const Button =(props) =>( 
+ const CreateButton =(props) =>( 
       <button className="RoundAddButton" onClick={props.onClick}>
         < ButtonPlus/>
       </button>
    );
 
-export default Button;
+export default CreateButton;
