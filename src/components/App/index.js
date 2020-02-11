f3cc6fe045a9852e146cf96622f8a6d041f542e8
MutegekiHenry <henrymutegeki117@gmail.com>
2020-02-11 20:01:05 +0300

    Button for creating resources created

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,49 @@
 import React from 'react';
 import './App.css';
 import Footer from '../Footer/';
-import Content from '../ResourceCard/Example';
+import Content from '../ResourceCard/';
 import ClusterResources from '../ClusterResources/';
+import Button from '../ButtonComponent';
 
 /* These below are for showing purposes */
 /* THIS IS GOING TO BE REMOVED IN THE DASHBOARD FEATURE */
 
-function App() {
-  return <div>
+// LogIn a user
+const logIn = () =>{
+  alert ("User is logged in Successfully")
+}
+
+// Register a user
+const registerUser = () =>{
+  alert ("User is registered in Successfully")
+}
+
+const loginName="Login"
+
+
+const App = () => {
+  return (
+  <div>
+    {/* login button */}
+    <Button
+      buttonName={loginName} 
+      onClick={logIn}
+    />
+
+     {/* Register button */}
+     <Button 
+      buttonName="Register"
+      onClick= {registerUser}
+      />
+
+
+    
     <div className="Disposable">
       <div><ClusterResources /></div>
     </div>
     <Footer />
   </div>
+  );
 }
 
 export default App;
