7161da1accf321e595b229a64434bc72788d7d50
Mubangizi <mubangizia22@gmail.com>
2020-02-12 20:38:17 +0300

    feat(Cluster Card)Adds cluster Card - shows cluster card list - adds hover shrink effect

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './App.css';
-import Footer from '../Footer/';
-import Content from '../ResourceCard/Example';
-import ClusterResources from '../ClusterResources/';
+import ClusterList from '../ClustersList';
+// import Content from '../ResourceCard/Example';
+import ClusterResources from '../ClusterResources';
 
 /* These below are for showing purposes */
 /* THIS IS GOING TO BE REMOVED IN THE DASHBOARD FEATURE */
 
 function App() {
-  return <div>
-    <div className="Disposable">
-      <div><ClusterResources /></div>
+  return (
+    <div className="Index">
+      <div className="RowNav" />
+
+      <div className="RowMain">
+        <ClusterList/>
+        {/* <div className="Content"><div className="Disposable"><ClusterResources /></div></div> */}
+      </div>
     </div>
-    <Footer />
-  </div>
+  );
 }
 
 export default App;
