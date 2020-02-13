a7afbc39ec588c4088e8576ec5699efed416f546
StephenAraka <stevenaraka@gmail.com>
2020-02-13 16:14:30 +0300

    add landing page content

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import PrimaryButton from '../PrimaryButton';
+import { ReactComponent as InfrastrucurePic } from '../../assets/images/infrastructure.svg';
+import './LandingPage.css';
 
 const LandingPage = () => (
   <div className="LandingPageMain">
-    <div className="section1">1</div>
-    <div className="section2">2</div>
-    <div className="section3">3</div>
+    <div className="LandingPageMainContent">
+      <div className="LandingPageMainContentInfo">
+        <h1 className="uppercase extra-bold">managed kubernetes platform</h1>
+        <h3 className="bold">Automated container deployment, scaling and management.</h3>
+        <PrimaryButton label="get started" />
+      </div>
+      <div className="LandingPageMainContentImg">
+        <InfrastrucurePic className="LandingPageInfrastructurePic" />
+      </div>
+    </div>
   </div>
 );
 
