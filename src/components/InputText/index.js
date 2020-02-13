651f8c8dd304b51c37b67d0f909caf15f3b89b6d
Derrick Sekidde <kidricederek@gmail.com>
2020-02-13 11:14:47 +0300

    Pull request feedback

--- src/components/InputText/index.js
+++ src/components/InputText/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './InputText.css';
 
-const InputText = props => {
-    return <input 
-        className="Input-Text"
-        type="text"
-        // value={props.value}
-        placeholder={props.placeholder} />;
-};
+const InputText = (props) => (
+  <input
+    className="InputText"
+    type="text"
+    placeholder={props.placeholder}
+  />
+);
 
 export default InputText;
