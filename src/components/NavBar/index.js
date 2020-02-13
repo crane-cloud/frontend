ce4e0dec431e060e6d444e3d2875eecbfa189a71
Derrick Sekidde <kidricederek@gmail.com>
2020-02-13 11:00:40 +0300

    fix items alignment

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -14,28 +14,31 @@ const NavBar = () =>
   (
     <>
       <div className="Navigation">
-        <input type="checkbox" id="nav-check" />
+        {/* <input type="checkbox" id="nav-check" /> */}
         <div className="NavContainer">
           <NavLogo />
         </div>
-        <div className="Nav-Btn">
+        {/* <div className="Nav-Btn">
           <label htmlFor="Nav-Check">
             <span />
             <span />
             <span />
           </label>
-        </div>
+        </div> */}
         <div className="Nav-Links">
           {/* <Link to="/pricing">PRICING</Link>
           <Link to="/docs">DOCS</Link> */}
           <div className="Logged-In">
             <div className="DropdownIcon Showing">
               <img src={PROFILE} size="1x" alt="profile" />
-              <div className="UserProfileName">Colin Wagaba</div>
+            </div>
+            <div className="UserProfileName">
+              <div className="NameUser"><strong>Colin Wagaba</strong></div>
             </div>
             <div className="Nav-dropdown Drop Profile">
 
               <div className="DropdownIcon">
+                <i class="arrow down"></i>
               </div>
               <div className="Dropdown-Content">
                 <Link to="/logout">Sign Out</Link>
