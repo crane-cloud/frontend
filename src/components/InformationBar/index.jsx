a4cf75cf1f6c6b87c20183533daf6a2bb0a383a0
Mubangizi <mubangizia22@gmail.com>
2020-02-14 10:31:48 +0300

    feat (Info Bar) adds namespace dropdown

--- src/components/InformationBar/index.jsx
+++ src/components/InformationBar/index.jsx
@@ -4,6 +4,13 @@ import './InformationBar.css';
 function InformationBar() {
   return (
     <div className="InformationBar">
+      <div className="DropDownDiv">
+        <span className="NamespaceDropDown">name space</span>
+        <div className="DropDownContent">
+          <div>default</div>
+          <div>monitoring</div>
+        </div>
+      </div>
       <div className="InfoHeader">Header</div>
     </div>
   );
