3643bf7459bbe11b01d7c605b8e215e73af30249
Mubangizi <mubangizia22@gmail.com>
2020-02-14 12:23:12 +0300

    added create Button

--- src/components/InformationBar/index.jsx
+++ src/components/InformationBar/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './InformationBar.css';
+import CreateButton from '../ButtonComponent';
 
 function InformationBar() {
   return (
     <div className="InformationBar">
-      <div className="DropDownDiv">
+
+      <div className="InfoHeader">Header</div>
+      <div className="CreateButton"><CreateButton /></div>
+      {/* <div className="DropDownDiv">
         <span className="NamespaceDropDown">name space</span>
         <div className="DropDownContent">
           <div>default</div>
           <div>monitoring</div>
         </div>
-      </div>
-      <div className="InfoHeader">Header</div>
+      </div> */}
     </div>
   );
 }
