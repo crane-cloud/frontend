c63fd7a5a4ddba0f8435aa88e3cc00da735c022b
MutegekiHenry <henrymutegeki117@gmail.com>
2020-02-14 12:59:34 +0300

    create login and add routes

---
+++ src/components/LoginPage/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class LoginPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/store';
 import { saveState } from './helpers/localStorage';
+import LoginPage from './components/LoginPage';
+
 
 // update localstorage whenever state changes
 store.subscribe(() => {
-    saveState(store.getState());
-  });
+  saveState(store.getState());
+});
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={LoginPage} />
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
