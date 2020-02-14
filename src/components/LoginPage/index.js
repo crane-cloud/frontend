63109134ab56072ea2301d1aa396f676735a88ee
MutegekiHenry <henrymutegeki117@gmail.com>
2020-02-14 13:16:11 +0300

    add header to login

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Header from '../Header';
+import './LoginPage.css';
 
 class LoginPage extends React.Component {
   constructor() {
@@ -8,8 +10,11 @@ class LoginPage extends React.Component {
 
   render() {
     return (
-      <div>
-        hello
+      <div className="LoginPageContainer">
+        <Header />
+        <div>
+          hello
+        </div>
       </div>
     );
   }
