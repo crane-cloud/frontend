7d332625a6e4f94a9b5c9c74ba4801e65a106b89
MutegekiHenry <henrymutegeki117@gmail.com>
2020-02-17 08:19:14 -0500

    add register page

---
+++ src/components/RegisterPage/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class RegisterPage extends Component {
+  render() {
+    return (
+      <div>
+          Register Page
+      </div>
+    );
+  }
+}
