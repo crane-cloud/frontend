59449f3f652e5c20291a42091bd2c9ae795c01ce
StephenAraka <stevenaraka@gmail.com>
2020-02-17 13:27:21 +0300

    add login button

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -4,6 +4,7 @@ import Header from '../Header';
 import LandingFooter from '../LandingFooter';
 import InputText from '../InputText';
 import InputPassword from '../InputPassword';
+import PrimaryButton from '../PrimaryButton';
 import './LoginPage.css';
 
 class LoginPage extends React.Component {
@@ -32,7 +33,13 @@ class LoginPage extends React.Component {
             <div className="LoginLinkContainer">
               <Link to='/forgot-password' className="LoginContentLink">Forgot your password?</Link>
             </div>
-            
+
+            <PrimaryButton label="login" />
+
+            <div className="LoginContentBottomLink LoginLinkContainer">
+              Not signed up?  <Link to='/register' className="LoginContentLink">Create an account.</Link>
+            </div>
+
           </div>
         </div>
         <LandingFooter />
