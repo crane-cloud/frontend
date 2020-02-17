2be9ca9d0283738428bca7a7d5cfd5dc8de0a946
Derrick Sekidde <kidricederek@gmail.com>
2020-02-17 12:28:55 +0300

    Add routing for component

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import AdminDashboard from './components/AdminDashboard';
+import ClusterResourcesPage from './components/ClusterResourcesPage';
 // import NavBar from './components/NavBar/';
 
 const routes = () => (
@@ -8,6 +9,9 @@ const routes = () => (
     <Switch>
       <Route exact path="/">
         <AdminDashboard />
+      </Route>
+      <Route exact path="/resources">
+        <ClusterResourcesPage />
       </Route>
     </Switch>
   </BrowserRouter>
