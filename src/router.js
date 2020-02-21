03a3d320ba728ff9d39784a498ac7d1d255db506
Derrick Sekidde <kidricederek@gmail.com>
2020-02-21 10:49:20 +0300

    Add store to router file.

--- src/router.js
+++ src/router.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import {Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './components/App';
 import LoginPage from './components/LoginPage';
 import PasswordReset from './components/PasswordReset';
 import RegisterPage from './components/RegisterPage';
+import ClusterResourcesPage from './components/ClusterResourcesPage';
+import store from './redux/store';
 
 const Routes = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/forgot-password" component={PasswordReset} />
-      <Route path="/register" component={RegisterPage} />
-    </Switch>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/forgot-password" component={PasswordReset} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/resources" component={ClusterResourcesPage} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default Routes;
