509e62f671c523cc2447aad814150668be45a0ea
Derrick Sekidde <kidricederek@gmail.com>
2020-02-21 12:25:32 +0300

    Add uppercasing for all card titles

--- src/components/ResourceCard/index.js
+++ src/components/ResourceCard/index.js
@@ -3,7 +3,7 @@ import './ResourceCard.css';
 
 const ResourceCard = (props) => (
     <div className="Card">
-      <div className="CardHeader">{props.title}</div>
+      <div className="CardHeader uppercase">{props.title}</div>
       <div className="ResourceDigit">{props.count}</div>
     </div>
   )
