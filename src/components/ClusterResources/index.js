ff50afbd59f711af6eda1cebcb3b5338894b23c2
Derrick Sekidde <kidricederek@gmail.com>
2020-02-25 01:29:32 +0300

    Add cluster id

--- src/components/ClusterResources/index.js
+++ src/components/ClusterResources/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Link, useParams } from 'react-router-dom';
 import ResourceCard from '../ResourceCard';
 import './ClusterResource.css';
 
 function ClusterResources(props) {
   const { resourceCount } = props;
+  let { clusterID } = useParams();
   return (
     <div className="ClusterContainer">
       {
         resourceCount.length !== 0 ? (resourceCount.map(
-          (resource) => <ResourceCard title={resource.name} count={resource.count} />
+          (resource) => (
+            <Link to={{ pathname: `?${clusterID}/resources/${resource.name.toLowerCase()}` }}>
+              <ResourceCard title={resource.name} count={resource.count} />
+            </Link>
+          )
         )) : (
           <h3 className="EmptyList">No Resources Available</h3>
         )
