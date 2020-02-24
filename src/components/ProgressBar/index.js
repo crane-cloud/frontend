40a8a8414878593824f0aff82d16e6c945a0cc20
StephenAraka <stevenaraka@gmail.com>
2020-02-24 13:20:34 +0300

    create component

---
+++ src/components/ProgressBar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProgressBar = (props) => {
+  return (
+    <div className="ProgressBar">
+      <div className="ProgressBarFiller">
+        
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
