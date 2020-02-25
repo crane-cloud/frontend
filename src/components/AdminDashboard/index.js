31d9f17c1337b3139d154edae92ead5de2d616cd
Derrick Sekidde <kidricederek@gmail.com>
2020-02-25 11:03:27 +0300

    Table component

--- src/components/AdminDashboard/index.js
+++ src/components/AdminDashboard/index.js
@@ -1,11 +1,22 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import './AdminDashboard.css';
-import Footer from '../Footer';
-import ClusterResources from '../ClusterResources';
+// import Footer from '../Footer';
+// import ClusterResources from '../ClusterResources';
 import NavBar from '../NavBar';
+import TableList from '../TableList';
 
 class AdminDashboard extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      responseData:[
+        {'name': 'Derek', 'status': 'Ready', 'age': '2 Days ago'},
+        {'name': 'owen', 'status': 'Unknown', 'age': '3 Hrs ago'},
+        {'name': 'makair', 'status': 'Ready', 'age': '46 Days ago'},
+      ]
+    }
+  }
   render() {
     return (
       <div>
@@ -13,7 +24,7 @@ class AdminDashboard extends React.Component {
 
         <div className="Disposable">
           <div>
-            <ClusterResources />
+            <TableList data={this.state.responseData}/>
           </div>
         </div>
         {/* <Footer /> */}
