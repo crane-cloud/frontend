99f5ef3cece6d694d3387e8385d4b438e56c54c3
Derrick Sekidde <kidricederek@gmail.com>
2020-02-25 13:14:48 +0300

    Add missing files

---
+++ src/components/ClustersList/index.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import getClustersList from '../../redux/actions/ClustersActions';
+import ClusterCard from '../ClusterCard';
+import './ClustersList.css';
+import crane from '../../assets/images/craneLogo.png';
+
+
+class ClustersList extends Component {
+  componentDidMount() {
+    const { getClustersList } = this.props;
+    getClustersList();
+  }
+
+  render() {
+    const { clusters } = this.props;
+    console.log(typeof (clusters.clusters));
+
+    return (
+      <div className="ClusterList">
+        {clusters.clusters.map((cluster) => (
+          <Link to={{ pathname: `/resources/?clusterID=${cluster.id}` }} key={cluster.id}>
+            <div key={cluster.id} className="ClusterCardItem">
+              <ClusterCard name={cluster.name} description={cluster.host} icon={crane} />
+            </div>
+          </Link>
+        ))}
+      </div>
+    );
+  }
+}
+
+// inititate props
+ClustersList.propTypes = {
+  clusters: PropTypes.object,
+  isRetrieving: PropTypes.bool
+};
+
+// assigning defaults
+ClustersList.defaultProps = {
+  clusters: [],
+  isRetrieving: false
+};
+
+export const mapStateToProps = (state) => {
+  const { isRetrieving, clusters } = state.ClustersReducer;
+  return { isRetrieving, clusters };
+};
+
+export const mapDispatchToProps = (dispatch) => bindActionCreators({
+  getClustersList
+}, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ClustersList);
