8bcb1eff2a43cdf45d816325bc5867d5ce565d8e
Derrick Sekidde <kidricederek@gmail.com>
2020-02-25 10:06:51 +0300

    Change table format

--- src/components/TableList/index.js
+++ src/components/TableList/index.js
@@ -1,11 +1,54 @@
 import React from 'react';
 import './TableList.css';
 
-const TableList = (props) => (
-    <div className="TableDiv">
-      <div className="CardHeader">{props.title}</div>
-      <div className="ResourceDigit"><h1>{props.resourceCount}</h1></div>
-    </div>
-  )
+class TableList extends React.Component {
+    
+    constructor(props){
+      super(props);
+      this.getHeader = this.getHeader.bind(this);
+      this.getRowsData = this.getRowsData.bind(this);
+      this.getKeys = this.getKeys.bind(this);
+    }
+    
+    getKeys = function(){
+      return Object.keys(this.props.data[0]);
+    }
+    
+    getHeader = function(){
+      let keys = this.getKeys();
+      return keys.map((key, index)=>{
+        return <th key={key}>{key.toUpperCase()}</th>
+      })
+    }
+    
+    getRowsData = function(){
+      let items = this.props.data;
+      let keys = this.getKeys();
+      return items.map((row, index)=>{
+        return <tr key={index}><RenderRow key={index} data={row} keys={keys}/></tr>
+      })
+    }
+    
+    render() {
+        return (
+          <div>
+            <table>
+            <thead>
+              <tr>{this.getHeader()}</tr>
+            </thead>
+            <tbody>
+              {this.getRowsData()}
+            </tbody>
+            </table>
+          </div>
+          
+        );
+    }
+}
 
+const RenderRow = (props) =>{
+  return props.keys.map((key, index)=>{
+    return <td key={props.data[key]}>{props.data[key]}</td>
+  })
+}
 export default TableList;
