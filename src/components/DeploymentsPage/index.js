4619b06394eae74573283d1ce76570350ed5f06a
StephenAraka <stevenaraka@gmail.com>
2020-03-01 22:19:12 +0300

    create simple table

--- src/components/DeploymentsPage/index.js
+++ src/components/DeploymentsPage/index.js
@@ -21,7 +21,34 @@ export default class DeploymentsPage extends Component {
           <div className="DeploymentsPageMainContent">
             <InformationBar header="Deployments" />
             <div className="DeploymentsPageTableSection">
-              Table here
+              <div className="DeploymentsPageTable">
+                <table>
+                  <thead className="uppercase extra-bold">
+                    <tr>
+                      <th>name</th>
+                      <th>ready</th>
+                      <th>age</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>man city</td>
+                      <td>1st</td>
+                      <td>champs lig</td>
+                    </tr>
+                    <tr>
+                      <td>pool</td>
+                      <td>2nd</td>
+                      <td>champs lig</td>
+                    </tr>
+                    <tr>
+                      <td>man u</td>
+                      <td>3rd</td>
+                      <td>europa</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
