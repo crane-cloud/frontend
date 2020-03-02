430e21be48abf61a6cae6328447f4ab54e910942
StephenAraka <stevenaraka@gmail.com>
2020-03-02 09:19:42 +0300

    render data to table

--- src/components/DeploymentsPage/index.js
+++ src/components/DeploymentsPage/index.js
@@ -6,21 +6,34 @@ import NavBar from '../NavBar';
 import SideNav from '../SideNav';
 import './DeploymentsPage.css';
 import InformationBar from '../InformationBar';
+import ProgressBar from '../ProgressBar';
 
 class DeploymentsPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: false
     };
+
+    // this.filterTableData = this.filterAndCreateRows.bind(this);
+    this.calculatePercentage = this.calculatePercentage.bind(this);
   }
 
   componentDidMount() {
-    this.props.getDeployments();
+    const { getDeployments } = this.props;
+    this.setState({
+      loading: true
+    });
+    getDeployments();
   }
 
+  calculatePercentage(proportion, total) {
+    return ((proportion / total) * 100);
+  }
 
   render() {
-    console.log(this.props.deployments);
+    const { deployments } = this.props;
+
     return (
       <div className="DeploymentsPageContainer">
         <NavBar />
@@ -41,21 +54,17 @@ class DeploymentsPage extends Component {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <td>man city</td>
-                      <td>1st</td>
-                      <td>champs lig</td>
-                    </tr>
-                    <tr>
-                      <td>pool</td>
-                      <td>2nd</td>
-                      <td>champs lig</td>
-                    </tr>
-                    <tr>
-                      <td>man u</td>
-                      <td>3rd</td>
-                      <td>europa</td>
-                    </tr>
+                    {deployments.deployments.map((deployment) => (
+                      <tr className="DeploymentsTableRows">
+                        <td>{deployment.metadata.name}</td>
+                        <td>
+                          <ProgressBar
+                            percentage={this.calculatePercentage(deployment.status.readyReplicas, deployment.status.availableReplicas)}
+                          />
+                        </td>
+                        <td>{deployment.metadata.creationTimestamp}</td>
+                      </tr>
+                    ))}
                   </tbody>
                 </table>
               </div>
