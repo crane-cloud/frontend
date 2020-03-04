eb3b11704be271e2f7eec6848d29d63ed5e2788b
Derrick Sekidde <kidricederek@gmail.com>
2020-03-04 17:58:34 +0300

    Create PVS component

---
+++ src/components/PvsListPage/index.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import './PvsListPage.css';
+import NavBar from '../NavBar';
+import InformationBar from '../InformationBar';
+import SideNav from '../SideNav';
+import getPvs from '../../redux/actions/PvsActions';
+import Status from '../Status';
+import timePast from '../../helpers/timeUtility';
+
+class PvsListPage extends React.Component {
+  componentDidMount() {
+    const { getPvs } = this.props;
+    const { match: { params } } = this.props;
+    getPvs(params.clusterID);
+  }
+
+  render() {
+    const { pvs } = this.props;
+    const clusterName = localStorage.getItem('clusterName');
+
+    return (
+      <div>
+        <NavBar />
+        <div className="MainSection">
+          <div className="SiteSideNav">
+            <SideNav clusterName={clusterName} clusterId={this.props.match.params.clusterID} />
+          </div>
+          <div className="Content">
+            <div className="UpperBar">
+              <InformationBar header="PVs" showBtn={false} />
+            </div>
+            <div className="LowerBar">
+              <div className="ResourcesTable">
+                <table>
+                  <tr>
+                    <th>Name</th>
+                    <th>Status</th>
+                    <th>Age</th>
+                  </tr>
+                  {
+                    pvs.length !== 0 ? (
+                      pvs.map((pv) => (
+                        <tr>
+                          <td>{pv.metadata.name}</td>
+                          <td><Status status={pv.status.phase} /></td>
+                          <td>{timePast(pv.metadata.creationTimestamp)}</td>
+                        </tr>
+
+                      ))) : (
+                      <h3 className="EmptyList">
+                        No PVs Available
+                      </h3>
+                    )
+                  }
+                </table>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+PvsListPage.propTypes = {
+  pvs: PropTypes.object,
+  isRetrieving: PropTypes.bool,
+};
+
+PvsListPage.defaultProps = {
+  pvs: [],
+  isRetrieving: false,
+};
+
+export const mapStateToProps = (state) => {
+  const { isRetrieving, pvs } = state.PvsReducer;
+  return { isRetrieving, pvs };
+};
+
+export const mapDispatchToProps = (dispatch) => bindActionCreators({
+  getPvs
+}, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PvsListPage);
