366bdaf9700d68d642f7ba153c96c2c52d750ae7
Derrick Sekidde <kidricederek@gmail.com>
2020-03-05 11:21:09 +0300

    Add more reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,13 +3,10 @@ import ClustersReducer from './clustersReducer';
 import AddClusterReducer from './addClusterReducer';
 import user from './user';
 import ClusterResourcesReducer from './ClusterResourcesReducer';
-<<<<<<< HEAD
 import PvsReducer from './PvsReducer';
-=======
 import PvcsReducer from './pvcsReducer';
 import NamespacesListReducer from './NamespacesListReducer';
 import podsReducer from './podsReducer';
->>>>>>> 7c35abe4bdf73bf1e3bbc2f06507b96723b8f59b
 
 
 export default combineReducers({
