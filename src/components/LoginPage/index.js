f1b766a0cb45cf18513a3dc4664a0728ec13c210
MutegekiHenry <henrymutegeki117@gmail.com>
2020-03-09 09:11:19 -0400

    add validate function

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -23,6 +23,7 @@ class LoginPage extends React.Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.validate = this.validate.bind(this);
   }
 
   handleChange(e) {
@@ -71,6 +72,28 @@ class LoginPage extends React.Component {
         console.log('Check your email / password...');
       });
   }
+    validate = (email, password) => {
+    // we are going to store errors for all fields
+    // in a signle array
+    const errors = [];
+  
+    if (email.length < 5) {
+      errors.push("Email should be at least 5 charcters long");
+    }
+    if (email.split("").filter(x => x === "@").length !== 1) {
+      errors.push("Email should contain a @");
+    }
+    if (email.indexOf(".") === -1) {
+      errors.push("Email should contain at least one dot");
+    }
+  
+    if (password.length < 6) {
+      errors.push("Password should be at least 6 characters long");
+    }
+  
+    return errors;
+  }
+  
 
   render() {
     const { email, password, loading } = this.state;
