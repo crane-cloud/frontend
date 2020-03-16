9caadff91746bfef4831493f7f8f4252eecc067b
StephenAraka <stevenaraka@gmail.com>
2020-03-16 17:41:56 +0300

    handle case for 0/0

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import './ProgressBar.css';
 
-const ProgressBar = (props) => {
-
+const ProgressBar = ({ percentage, fractionLabel }) => {
+  const percantageNotValid = isNaN(percentage);
   let fillColor;
-  if (props.percentage < 25) {
+  if(percantageNotValid) {
     fillColor = 'Quarter1';
-  } else if (25 <= props.percentage && props.percentage < 50) {
-    fillColor = 'Quarter2';
-  } else if (50 <= props.percentage && props.percentage < 75) {
-    fillColor = 'Quarter3';
   } else {
-    fillColor = 'Quarter4';
+    if (percentage < 25) {
+      fillColor = 'Quarter1';
+    } else if (percentage >= 25 && percentage < 50) {
+      fillColor = 'Quarter2';
+    } else if (percentage >= 50 && percentage < 75) {
+      fillColor = 'Quarter3';
+    } else {
+      fillColor = 'Quarter4';
+    }
   }
 
   return (
-    <div className="ProgressBar">
-      <div
-        className={`ProgressBarFiller ProgressBarFill${fillColor}`}
-        style={{ width: `${props.percentage}%` }}
-      >
-        {`${props.percentage}%`}
+    <div className="ProgressBarContainer">
+      <div className="ProgressBar">
+        <div
+          className={`ProgressBarFiller ProgressBarFill${fillColor}`}
+          style={{ width: `${percentage === 0 ? '100' : percentage}%` }}
+        >
+          {fractionLabel ? `${fractionLabel}` : `${percentage}%`}
+        </div>
       </div>
     </div>
   );
