287e35aaf1596c61c385401c4659c04180ca8f0a
StephenAraka <stevenaraka@gmail.com>
2020-03-23 18:54:30 +0300

    change checkbox implementation

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -2,42 +2,17 @@ import React from 'react';
 import './Checkbox.css';
 import { ReactComponent as Checkmark } from '../../assets/images/check.svg';
 
-class Checkbox extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      checked: false
-    };
-
-    this.toggleCheckmark = this.toggleCheckmark.bind(this);
-  }
-
-  toggleCheckmark() {
-    this.setState({
-      checked: !this.state.checked
-    });
-  }
-
-  render() {
-    return (
-      <div
-        className="Checkbox"
-        onClick={() => {
-          this.props.onClick(this.state.checked);
-        }}
-      >
-        <input
-          type="checkbox"
-          onChange={this.toggleCheckmark}
-          checked={this.state.checked}
-        />
-        <div className={`CheckMarkWrapper Checked-${this.state.checked}`}>
-          <Checkmark />
-        </div>
-      </div>
-    );
-  }
-}
+const Checkbox = ({ isChecked, onClick }) => (
+  <div className="Checkbox">
+    <input
+      type="checkbox"
+      onChange={onClick}
+      checked={isChecked}
+    />
+    <div className={`CheckMarkWrapper Checked-${isChecked}`}>
+      <Checkmark />
+    </div>
+  </div>
+);
 
 export default Checkbox;
