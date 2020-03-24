57a37b2913c8abb1d53766fb8969e29903793c9a
Mubangizi <mubangizia22@gmail.com>
2020-03-25 00:14:14 +0300

    Add prop fields to AppCard

--- src/components/AppsCard/index.jsx
+++ src/components/AppsCard/index.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
 import './AppsCard.css';
+import Status from '../Status';
 
 const AppsCard = (props) => (
   <div className="AppCard">
     <div className="AppCardHeader">
       <table className="AppTable">
         <tr>
-          <td>Flask</td>
-          <td>Status</td>
+          <td>{props.title}</td>
+          <td className="StatusData"><Status status={props.status} /></td>
         </tr>
       </table>
     </div>
-    <div className="AppUrl">Url</div>
+    <div className="AppUrl">{props.url}</div>
   </div>
 );
 
