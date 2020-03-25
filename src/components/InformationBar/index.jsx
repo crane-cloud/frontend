fce379120f59be3a0d51da81de5ca1a321183124
MutegekiHenry <henrymutegeki117@gmail.com>
2020-03-25 18:03:32 +0300

    restore add cluster form

--- src/components/InformationBar/index.jsx
+++ src/components/InformationBar/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
 import './InformationBar.css';
-import CreateAppForm from '../CreateAppForm';
+import AddClusterForm from '../AddClusterForm';
 import CreateButton from '../ButtonComponent';
 
 function InformationBar(props) {
@@ -14,7 +14,7 @@ function InformationBar(props) {
           <div className="InfoHeader">{ header }</div>
           <Popup trigger={<div className="CreateButton"><CreateButton /></div>} modal className="popup">
             {(close) => (
-              <CreateAppForm close={close} />
+              <AddClusterForm close={close} />
             )}
           </Popup>
         </div>
