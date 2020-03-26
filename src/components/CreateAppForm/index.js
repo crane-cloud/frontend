17d48caa25c530cedcb4272f21197f8b562ac021
MutegekiHenry <henrymutegeki117@gmail.com>
2020-03-26 15:55:59 +0300

    fix lint issue

--- src/components/CreateAppForm/index.js
+++ src/components/CreateAppForm/index.js
@@ -147,8 +147,7 @@ class CreateAppForm extends React.Component {
   }
 
   handleSubmit() {
-    const { name, uri, envVars, error 
-    } = this.state;
+    const { name, uri, envVars } = this.state;
     const app = {
       name,
       uri
