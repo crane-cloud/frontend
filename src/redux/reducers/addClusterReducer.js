370cfa36450a65e89bc30a8b36b99b88ffa2fd96
StephenAraka <stevenaraka@gmail.com>
2020-04-02 21:11:17 +0300

    modify reducer

--- src/redux/reducers/addClusterReducer.js
+++ src/redux/reducers/addClusterReducer.js
@@ -1,13 +1,17 @@
 import { ADD_CLUSTER_SUCCESS, ADD_CLUSTERS_FAIL, START_ADDING_CLUSTER } from '../actions/actionTypes';
 
 const initialState = {
-  isSending: false,
+  isAdded: false,
+  isFailed: false,
+  isCreating: false,
+  cluster: null,
+  error: null,
   message: 'Add cluster'
 };
 
-const AddClusterReducer = ( state = initialState, action) => {
+const AddClusterReducer = (state = initialState, action) => {
   switch (action.type) {
-  case ADD_CLUSTER_SUCCESS: {
+  case ADD_CLUSTER_SUCCESS:
     return {
       ...state,
       cluster: action.payload,
@@ -15,19 +19,20 @@ const AddClusterReducer = ( state = initialState, action) => {
       isAdded: true,
       message: 'Cluster Added SuccessFully'
     };
-  }
+
   case START_ADDING_CLUSTER:
     return {
       ...state,
-      isAdded: false,
-      isFailed: false
+      isCreating: true,
+      isAdded: false
     };
+
   case ADD_CLUSTERS_FAIL:
     return {
       ...state,
       isFailed: true,
       isAdded: false,
-      errorOccured: action.payload.error,
+      error: action.payload.error,
       message: 'Failed to add cluster'
     };
 
