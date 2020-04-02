73ca2ec0f9c59072fd95f6c8dfe3cbe22e1b0fe5
StephenAraka <stevenaraka@gmail.com>
2020-04-02 20:40:13 +0300

    move code to cluster page

--- src/components/ClusterPage/index.jsx
+++ src/components/ClusterPage/index.jsx
@@ -3,29 +3,173 @@ import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './ClusterPage.css';
 import InformationBar from '../InformationBar';
+import InformationBarSub from '../InformationBarSub';
+import PrimaryButton from '../PrimaryButton';
+import Spinner from '../SpinnerComponent';
+import BlackInputText from '../BlackInputText';
+import Modal from '../Modal';
 import ClustersList from '../ClustersList';
 import Header from '../Header';
+import AddCluster from '../../redux/actions/addCluster';
 
-const ClusterPage = (props) => {
-  const { user: { accessToken } } = props;
-  localStorage.setItem('token', accessToken);
+class ClusterPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      host: '',
+      token: '',
+      openModal: false
+    };
 
-  return (
-    <div className="Page">
-      <div className="TopRow">
-        <Header />
-        <InformationBar header="Select Infrastructure" showBtn />
+    this.showForm = this.showForm.bind(this);
+    this.hideForm = this.hideForm.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  showForm() {
+    this.setState({ openModal: true });
+  }
+
+  hideForm() {
+    this.setState({ openModal: false });
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit() {
+    const { AddCluster } = this.props;
+
+    const {
+      host,
+      name,
+      token,
+      description
+    } = this.state;
+
+    const cluster = {
+      host,
+      name,
+      token,
+      description
+    };
+
+    AddCluster(cluster);
+  }
+
+  render() {
+    const {
+      host,
+      token,
+      name,
+      description,
+      openModal
+    } = this.state;
+
+    const {
+      user: { accessToken },
+      isCreating
+    } = this.props;
+
+    localStorage.setItem('token', accessToken);
+
+    return (
+      <div className="Page">
+        <div className="TopRow">
+          <Header />
+          {/* <InformationBar header="Select Infrastructure" showBtn /> */}
+          <InformationBarSub header="Select Infrastructure" showBtn btnAction={this.showForm} />
+        </div>
+        <div className="MainRow">
+          <ClustersList />
+        </div>
+        <div className="FooterRow">
+          <p>Copyright © 2020 Crane Cloud. All Rights Reserved.</p>
+        </div>
+
+        {/* Modal for creating a new project
+        Its triggered by the value of state.openModal */}
+        <Modal showModal={openModal}>
+          <div className="ModalForm">
+            <div className="ModalFormHeading">
+              <h2>Add a cluster</h2>
+            </div>
+            <div className="ModalFormInputs">
+              <div className="ModalFormInputsBasic">
+                <BlackInputText
+                  placeholder="Host"
+                  name="host"
+                  value={host}
+                  onChange={(e) => {
+                    this.handleChange(e);
+                  }}
+                />
+                <BlackInputText
+                  placeholder="Token"
+                  name="token"
+                  value={token}
+                  onChange={(e) => {
+                    this.handleChange(e);
+                  }}
+                />
+                <BlackInputText
+                  placeholder="Name"
+                  name="name"
+                  value={name}
+                  onChange={(e) => {
+                    this.handleChange(e);
+                  }}
+                />
+                <BlackInputText
+                  placeholder="Description"
+                  name="description"
+                  value={description}
+                  onChange={(e) => {
+                    this.handleChange(e);
+                  }}
+                />
+
+                <div className="ModalFormButtons AddAddButtons">
+                  <PrimaryButton label="cancel" className="CancelBtn" onClick={this.hideForm} />
+                  <PrimaryButton label={isCreating ? <Spinner /> : 'add'} onClick={this.handleSubmit} />
+                </div>
+                {/* {createFeedback && (
+                  <div
+                    className={createFeedback.startsWith('Success') ? 'AppFormErrorDiv CreateSuccess' : 'AppFormErrorDiv CreateFail'}>
+                    {createFeedback}
+                  </div>
+                )} */}
+              </div>
+            </div>
+          </div>
+        </Modal>
       </div>
-      <div className="MainRow">
-        <ClustersList />
-      </div>
-      <div className="FooterRow">
-        <p>Copyright © 2020 Crane Cloud. All Rights Reserved.</p>
-      </div>
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = ({ user, AddClusterReducer }) => {
+  const {
+    isCreating,
+    isAdded,
+    isFailed,
+    cluster,
+    errorOccured
+  } = AddClusterReducer;
+
+  return {
+    user,
+    isCreating,
+    isAdded,
+    isFailed,
+    cluster,
+    errorOccured
+  };
 };
 
-const mapStateToProps = (state) => ({ user: state.user });
-
-export default connect(mapStateToProps, null)(withRouter(ClusterPage));
+export default connect(mapStateToProps, { AddCluster })(withRouter(ClusterPage));
