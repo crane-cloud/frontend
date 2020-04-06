8c8c9418ebe62f5c2e2c04c2024af164e71b888a
StephenAraka <stevenaraka@gmail.com>
2020-04-06 12:42:28 +0300

    use es6

--- src/components/InformationBarSub/index.js
+++ src/components/InformationBarSub/index.js
@@ -2,21 +2,19 @@ import React from 'react';
 import CreateButton from '../ButtonComponent';
 import './InformationBarSub.css';
 
-function InformationBarSub({ header, showBtn, btnAction }) {
-  return (
-    <div className="InformationBarSub">
-
-      {showBtn ? (
-        <div>
-          <div className="InfoHeader">{ header }</div>
-          <div className="CreateButton">
-            <CreateButton onClick={btnAction} />
-          </div>
+const InformationBarSub = ({ header, showBtn, btnAction }) => (
+  <div className="InformationBarSub">
+    {showBtn ? (
+      <div>
+        <div className="InfoHeader">{header}</div>
+        <div className="CreateButton">
+          <CreateButton onClick={btnAction} />
         </div>
-      ) : (
-        <div className="LeftHeader">{header}</div>
-      )}
-    </div>
-  );
-}
+      </div>
+    ) : (
+      <div className="LeftHeader">{header}</div>
+    )}
+  </div>
+);
+
 export default InformationBarSub;
