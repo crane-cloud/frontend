7be8e9c6d12346a9deea0c5b78adef76f6a35eb5
Mubangizi <mubangizia22@gmail.com>
2020-04-15 15:21:10 +0300

    Change Back-end Url to Staging

--- src/config.js
+++ src/config.js
@@ -1,1 +1,5 @@
-export const API_BASE_URL = 'http://crane-mak-w1.cranecloud.io:30895';
+// cluster Url
+// export const API_BASE_URL = 'http://crane-mak-w1.cranecloud.io:30895';http://staging-api.cranecloud.io/
+
+// Staging Url
+export const API_BASE_URL = 'http://staging-api.cranecloud.io/';
