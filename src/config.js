2a6961d964a756827c64966148777ce5fe11bbd5
Mubangizi <mubangizia22@gmail.com>
2020-04-16 15:09:15 +0300

    Add Api base url as env variable

--- src/config.js
+++ src/config.js
@@ -1,5 +1,2 @@
-// cluster Url
-// export const API_BASE_URL = 'http://crane-mak-w1.cranecloud.io:30895';http://staging-api.cranecloud.io/
-
 // Staging Url
-export const API_BASE_URL = 'http://staging-api.cranecloud.io/';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
