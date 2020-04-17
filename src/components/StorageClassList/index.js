e2f615c267b8a50933330c0286af63d42f02c5ee
StephenAraka <stevenaraka@gmail.com>
2020-04-17 15:24:08 +0300

    remove storageclasses action pascalcase

--- src/components/StorageClassList/index.js
+++ src/components/StorageClassList/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import getStorageClassList from '../../redux/actions/StorageClassesActions';
+import getStorageClassList from '../../redux/actions/storageClasses';
 import tellAge from '../../helpers/ageUtility';
 import './StorageClassList.css';
 import Header from '../Header';
