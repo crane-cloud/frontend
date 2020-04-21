feef943acb95ab8dabc266eb957c83899c585621
StephenAraka <stevenaraka@gmail.com>
2020-04-21 13:33:56 +0300

    make component functional

--- src/components/AppsCard/index.jsx
+++ src/components/AppsCard/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import PrimaryButton from '../PrimaryButton';
@@ -10,144 +10,125 @@ import Status from '../Status';
 import Feedback from '../Feedback';
 import './AppsCard.css';
 
-class AppsCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      openDeleteAlert: false,
-      openDropDown: false
-    };
+const AppsCard = (props) => {
+  const [openDeleteAlert, setDeleteAlert] = useState(false);
+  const [openDropDown, setDropDown] = useState(false);
 
-    this.handleDeleteApp = this.handleDeleteApp.bind(this);
-    this.showDeleteAlert = this.showDeleteAlert.bind(this);
-    this.hideDeleteAlert = this.hideDeleteAlert.bind(this);
-    this.toggleDropDown = this.toggleDropDown.bind(this);
-    this.hideDropDown = this.hideDropDown.bind(this);
-    this.showDropDown = this.showDropDown.bind(this);
-  }
+  const {
+    clearState, name, status, url, appId, isDeleted, isDeleting, isFailed, message, hasDeleted
+  } = props;
 
-  componentDidUpdate(prevProps) {
-    const { isDeleted, hasDeleted } = this.props;
+  const showDropDown = () => {
+    setDropDown(true);
+  };
 
-    if (isDeleted !== prevProps.isDeleted) {
-      hasDeleted(true);
-      this.hideDeleteAlert();
-    }
-  }
+  const hideDropDown = () => {
+    showDropDown(false);
+  };
 
-  showDropDown() {
-    this.setState({ openDropDown: true });
-  }
-
-  toggleDropDown() {
-    const { openDropDown } = this.state;
+  const toggleDropDown = () => {
     if (openDropDown) {
-      this.hideDropDown();
+      hideDropDown();
     } else {
-      this.showDropDown();
+      showDropDown();
     }
-  }
+  };
 
-  hideDropDown() {
-    this.setState({ openDropDown: false });
-  }
-
-  handleDeleteApp(e, appId) {
+  const handleDeleteApp = (e, appId) => {
     const {
       deleteApp
-    } = this.props;
+    } = props;
+
     e.preventDefault();
-
     deleteApp(appId);
-  }
+  };
 
 
-  showDeleteAlert() {
-    this.setState({ openDeleteAlert: true });
-  }
+  const showDeleteAlert = () => {
+    setDeleteAlert(true);
+  };
 
-  hideDeleteAlert() {
-    const { clearState } = this.props;
+  const hideDeleteAlert = () => {
     clearState();
-    this.setState({ openDeleteAlert: false });
-  }
+    setDeleteAlert(false);
+    if (isDeleted) {
+      hasDeleted();
+    }
+  };
 
-  render() {
-    const {
-      name, status, url, appId, isDeleting, isFailed, message
-    } = this.props;
-    const { openDeleteAlert, openDropDown } = this.state;
-    return (
-      <div className="AppCard">
-        <div className="AppCardHeader">
-          <table className="AppTable">
-            <tr>
-              <td className="AppName">{name}</td>
-              <td className="OtherData">
-                <div className="StatusData">
-                  <Status status={status} />
-                  <div
-                    className="AppDropDown"
-                    onClick={() => this.toggleDropDown()}
-                    role="presentation"
-                  >
-                    <img src={DotsImg} alt="three dots" className="DropDownImg" />
-                    {openDropDown && (
-                      <div className="AppDropDownContent">
-                        <div
-                          onClick={() => this.showDeleteAlert()}
-                          role="presentation"
-                        >
-                          Delete
-                        </div>
-                        <div>Update</div>
+  useEffect(() => (
+    hideDeleteAlert()
+  ), [isDeleted]); // eslint-disable-line
+
+  return (
+    <div className="AppCard">
+      <div className="AppCardHeader">
+        <table className="AppTable">
+          <tr>
+            <td className="AppName">{name}</td>
+            <td className="OtherData">
+              <div className="StatusData">
+                <Status status={status} />
+                <div
+                  className="AppDropDown"
+                  onClick={toggleDropDown}
+                  role="presentation"
+                >
+                  <img src={DotsImg} alt="three dots" className="DropDownImg" />
+                  {openDropDown && (
+                    <div className="AppDropDownContent">
+                      <div
+                        onClick={showDeleteAlert}
+                        role="presentation"
+                      >
+                        Delete
                       </div>
-                    )}
-                  </div>
+                      <div>Update</div>
+                    </div>
+                  )}
                 </div>
-              </td>
-            </tr>
-          </table>
-        </div>
-        <div className="AppUrlText">Url :</div>
-        <div className="AppUrl"><a target="_blank" rel="noopener noreferrer" href={url}>{url}</a></div>
+              </div>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div className="AppUrlText">Url :</div>
+      <div className="AppUrl"><a target="_blank" rel="noopener noreferrer" href={url}>{url}</a></div>
 
-        {(openDeleteAlert && (
-          <div className="AppDeleteModel">
-            <Modal showModal={openDeleteAlert}>
-              <div className="DeleteAppModel">
-                <div className="DeleteDescription">
-                  Are you sure you want to delete
-                  <span>
-                    <b>
-                      {' '}
-                      {name}
-                      {' '}
-                    </b>
-                  </span>
-                  ?
-                </div>
-                <div className="DeleteAppModelResponses">
-                  <PrimaryButton label="cancel" className="CancelBtn" onClick={this.hideDeleteAlert} />
-                  <PrimaryButton label={isDeleting ? <Spinner /> : 'Delete'} onClick={(e) => this.handleDeleteApp(e, appId)} />
-                </div>
-
-                {message && (
-                  <Feedback
-                    type={isFailed ? 'error' : 'success'}
-                    message={message}
-                  />
-                )}
-
+      {(openDeleteAlert && (
+        <div className="AppDeleteModel">
+          <Modal showModal={openDeleteAlert}>
+            <div className="DeleteAppModel">
+              <div className="DeleteDescription">
+                Are you sure you want to delete
+                <span>
+                  <b>
+                    {' '}
+                    {name}
+                    {' '}
+                  </b>
+                </span>
+                ?
               </div>
 
-            </Modal>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+              <div className="DeleteAppModelResponses">
+                <PrimaryButton label="cancel" className="CancelBtn" onClick={hideDeleteAlert} />
+                <PrimaryButton label={isDeleting ? <Spinner /> : 'Delete'} onClick={(e) => handleDeleteApp(e, appId)} />
+              </div>
+
+              {message && (
+                <Feedback
+                  type={isFailed ? 'error' : 'success'}
+                  message={message}
+                />
+              )}
+            </div>
+          </Modal>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 // inititate props
 AppsCard.propTypes = {
