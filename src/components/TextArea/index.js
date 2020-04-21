087839d195d1e474a38bbd2438a5c0b38777714d
StephenAraka <stevenaraka@gmail.com>
2020-04-21 15:16:06 +0300

    refactor textarea

--- src/components/TextArea/index.js
+++ src/components/TextArea/index.js
@@ -1,42 +1,25 @@
 import React from 'react';
 import './TextArea.css';
 
-class TextArea extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      InputBackground: 'InitialBackground'
-    };
-  }
+const TextArea = (props) => {
+  const {
+    name, value, placeholder, onChange
+  } = props;
 
-  changeBackground() {
-    let { InputBackground } = this.state;
-    InputBackground = 'WhiteBackground';
-    this.setState({ InputBackground });
-  }
-
-  render() {
-    const { InputBackground } = this.state;
-    const { name, value, placeholder } = this.props;
-
-    return (
-      <textarea
-        className={`TextArea ${InputBackground}`}
-        type="text"
-        placeholder={`${placeholder} *`}
-        rows="3"
-        cols="50"
-        name={name}
-        value={value}
-        onChange={(e) => {
-          this.props.onChange(e);
-        }}
-        onInput={() => {
-          this.changeBackground();
-        }}
-      />
-    );
-  }
-}
+  return (
+    <textarea
+      className="TextArea"
+      type="text"
+      placeholder={`${placeholder} *`}
+      rows="3"
+      cols="50"
+      name={name}
+      value={value}
+      onChange={(e) => {
+        onChange(e);
+      }}
+    />
+  );
+};
 
 export default TextArea;
