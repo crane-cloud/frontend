e1041c74feed8b94d254009f8ecd7175445b8d94
Derrick Sekidde <kidricederek@gmail.com>
2020-05-12 12:44:42 +0300

    create component

---
+++ src/components/DeleteWarning/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const DeleteWarning = () => (
+  <div className="DeleteWarning">
+    Note:<small>This action is irreversible! </small>
+  </div>
+);
+
+export default DeleteWarning;
