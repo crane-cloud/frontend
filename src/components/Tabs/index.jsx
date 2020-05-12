2f8181b7194167be9eeaffadd8a14cce7d6d7971
StephenAraka <stevenaraka@gmail.com>
2020-05-12 12:46:51 +0300

    render tabs children

--- src/components/Tabs/index.jsx
+++ src/components/Tabs/index.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Tab from './Tab';
 
-const Tabs = (props) => {
+const Tabs = ({ children }) => {
+  const [activeTab, setActiveTab] = useState(children[0].props.index);
+
+  const onClickTabItem = (tabIndex) => setActiveTab(tabIndex);
+
   return (
-    <div>
-      Tabs
+    <div className="TabsContainer">
+      <ol className="tab-list">
+        {children.map((child) => {
+          const { index, label } = child.props;
+
+          return (
+            <Tab
+              activeTab={activeTab}
+              key={index}
+              label={label}
+              onClick={onClickTabItem}
+            />
+          );
+        })}
+      </ol>
+      <div className="tab-content">
+        {children.map((child) => {
+          if (child.props.label !== activeTab) return undefined;
+          return child.props.children;
+        })}
+      </div>
     </div>
   );
 };
