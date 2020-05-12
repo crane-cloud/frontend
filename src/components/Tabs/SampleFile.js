d992dbcbd30f3a75b0d548df4ac712e666a9abca
StephenAraka <stevenaraka@gmail.com>
2020-05-12 12:37:14 +0300

    add sample tabs content

--- src/components/Tabs/SampleFile.js
+++ src/components/Tabs/SampleFile.js
@@ -2,13 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Tabs from '.';
 
-const SampleTabs = (props) => {
-  return (
-    <div>
-      <Tabs />
-    </div>
-  );
-};
+const SampleTabs = () => (
+  <div>
+    <Tabs>
+      <div index={1} label="Tab One">
+        Tab One
+      </div>
+      <div index={2} label="Tab Two">
+        Tab Two
+      </div>
+      <div index={3} label="Tab Three">
+        Tab Three
+      </div>
+    </Tabs>
+  </div>
+);
 
 SampleTabs.propTypes = {
 
