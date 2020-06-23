731b96a3acf07a0c9d63515610a2a2c0954dcc5b
Derrick Sekidde <kidricederek@gmail.com>
2020-06-23 15:40:07 +0300

    Add url in config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // Staging Url
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 // docs Url
-export const DOCS_URL = process.env.REACT_APP_DOCS_URL;
+export const DOCS_URL = "http://staging-docs.cranecloud.io";
