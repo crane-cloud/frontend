9fb42396324faf6cde1fdcb67bace56bb35c14e7
Colin Wagaba <ckwagaba@gmail.com>
2020-06-25 13:45:27 +0300

    make padding uniform in main sections

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-// import * as serviceWorker from './serviceWorker';
 import { saveState } from './helpers/localStorage';
 import Routes from './router';
 import './index.css';
@@ -15,9 +14,6 @@ store.subscribe(() => {
 ReactDOM.render(
   <Provider store={store}>
     <Routes />
-  </Provider>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+  </Provider>,
+  document.getElementById('root')
+);
