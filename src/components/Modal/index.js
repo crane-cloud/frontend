d7395be2bf0557ebb257022bc35af76b4c19c654
StephenAraka <stevenaraka@gmail.com>
2020-07-08 12:46:50 +0300

    Update state from prop change

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import './Modal.css';
 
-const Modal = ({ showModal, children }) => (
-  (showModal && (
-    <div className="ModalParentWrap">
-      <div className="ModalChildWrap">
-        <div className="ModalContent">
-          {children}
+const Modal = ({ showModal, children }) => {
+  const [show, setShow] = useState(showModal);
+
+  const toggleModal = () => {
+    setShow(showModal);
+  };
+
+  useEffect(() => (
+    toggleModal()
+  ), [showModal]); // eslint-disable-line
+
+  return (
+    (show && (
+      <div className="ModalParentWrap">
+        <div className="ModalChildWrap">
+          <div className="ModalContent">
+            {children}
+          </div>
         </div>
       </div>
-    </div>
-  ))
-);
+    ))
+  );
+};
 
 Modal.propTypes = {
   showModal: PropTypes.bool.isRequired,
