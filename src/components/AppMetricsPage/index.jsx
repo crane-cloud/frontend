6939a1d45db4be3663f39309d2cbd346f2e58b25
neelxie <kidricederek@gmail.com>
2020-08-05 18:04:40 +0300

    remove redundant code

--- src/components/AppMetricsPage/index.jsx
+++ src/components/AppMetricsPage/index.jsx
@@ -1,34 +1,13 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { v4 as uuidv4 } from 'uuid';
-import createApp, { clearState } from '../../redux/actions/createApp';
-import PrimaryButton from '../PrimaryButton';
-import BlackInputText from '../BlackInputText';
-import Modal from '../Modal';
-import RemoveIcon from '../../assets/images/remove.svg';
 import InformationBar from '../InformationBar';
-import AppsList from '../AppsList';
 import Header from '../Header';
-import Spinner from '../Spinner';
-import Feedback from '../Feedback';
-import Checkbox from '../Checkbox';
-import Tooltip from '../Tooltip';
 import SideBar from '../SideBar';
-import Tabs from '../Tabs';
-import Select from '../Select';
 import './AppMetricsPage.css';
 
 class AppMetricsPage extends React.Component {
-  static propTypes = {
-    match: PropTypes.object.isRequired,
-    location: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired
-  }
 
   render() {
-    // const { otherAppData } = this.props.location;
     const { appName, appUrl, liveAppStatus }  = this.props.location;
     const {projectID, userID} = this.props.match.params;
     
@@ -51,10 +30,6 @@ class AppMetricsPage extends React.Component {
             </div>
           </div>
         </div>
-
-        {/* Modal for creating a new app
-        Its triggered by the value of state.openModal */}
-        
       </div>
     );
   }
