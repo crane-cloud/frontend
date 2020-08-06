119674bbc451c11d769a203e884b22acb4ce9352
StephenAraka <stevenaraka@gmail.com>
2020-08-06 21:46:30 +0300

    Add line graph

--- src/components/AppsCard/index.jsx
+++ src/components/AppsCard/index.jsx
@@ -2,7 +2,36 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import AppStatus from '../AppStatus';
+import LineChartComponent from '../LineChart';
 import './AppsCard.css';
+
+const sampleData = [
+  { name: 'Sample Metric 1', uv: 250 },
+  { name: 'Sample Metric 2', uv: 270 },
+  { name: 'Sample Metric 2', uv: 10 },
+  { name: 'Sample Metric 2', uv: 100 },
+  { name: 'Sample Metric 2', uv: 70 },
+  { name: 'Sample Metric 2', uv: 150 },
+  { name: 'Sample Metric 2', uv: 60 },
+  { name: 'Sample Metric 2', uv: 100 },
+  { name: 'Sample Metric 2', uv: 190 },
+  { name: 'Sample Metric 2', uv: 290 },
+  { name: 'Sample Metric 2', uv: 150 },
+  { name: 'Sample Metric 2', uv: 100 },
+  { name: 'Sample Metric 2', uv: 130 },
+  { name: 'Sample Metric 2', uv: 0 },
+  { name: 'Sample Metric 2', uv: 270 },
+  { name: 'Sample Metric 2', uv: 280 },
+  { name: 'Sample Metric 2', uv: 300 },
+  { name: 'Sample Metric 2', uv: 100 },
+  { name: 'Sample Metric 2', uv: 170 },
+  { name: 'Sample Metric 2', uv: 290 },
+];
+
+// this function is meant to shuffle the dummy data array
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
 
 const AppsCard = (props) => {
   const {
@@ -27,6 +56,9 @@ const AppsCard = (props) => {
             <AppStatus appStatus={appStatus} />
           </div>
         </div>
+      </div>
+      <div className="AppGraphSummary">
+        <LineChartComponent data={shuffle(sampleData)} />
       </div>
     </div>
   );
