7577258dd9ac6e9929e33bb23de8c6fb09073ea9
StephenAraka <stevenaraka@gmail.com>
2020-08-20 03:22:06 +0300

    Add props to graph

--- src/components/LineChart/Sample.jsx
+++ src/components/LineChart/Sample.jsx
@@ -3,26 +3,36 @@ import LineChartComponent from './index';
 import './LineChart.css';
 
 const sampleData = [
-  { name: 'Sample Metric 1', uv: 250 },
-  { name: 'Sample Metric 2', uv: 270 },
-  { name: 'Sample Metric 2', uv: 10 },
-  { name: 'Sample Metric 2', uv: 100 },
-  { name: 'Sample Metric 2', uv: 70 },
-  { name: 'Sample Metric 2', uv: 150 },
-  { name: 'Sample Metric 2', uv: 60 },
-  { name: 'Sample Metric 2', uv: 100 },
-  { name: 'Sample Metric 2', uv: 190 },
-  { name: 'Sample Metric 2', uv: 290 },
-  { name: 'Sample Metric 2', uv: 150 },
-  { name: 'Sample Metric 2', uv: 100 },
-  { name: 'Sample Metric 2', uv: 130 },
-  { name: 'Sample Metric 2', uv: 0 },
-  { name: 'Sample Metric 2', uv: 270 },
-  { name: 'Sample Metric 2', uv: 280 },
-  { name: 'Sample Metric 2', uv: 300 },
-  { name: 'Sample Metric 2', uv: 100 },
-  { name: 'Sample Metric 2', uv: 170 },
-  { name: 'Sample Metric 2', uv: 290 },
+  { name: '01', 'memory(bytes)': 250 },
+  { name: '02', 'memory(bytes)': 270 },
+  { name: '03', 'memory(bytes)': 10 },
+  { name: '04', 'memory(bytes)': 100 },
+  { name: '05', 'memory(bytes)': 70 },
+  { name: '06', 'memory(bytes)': 150 },
+  { name: '07', 'memory(bytes)': 60 },
+  { name: '08', 'memory(bytes)': 100 },
+  { name: '09', 'memory(bytes)': 190 },
+  { name: '10', 'memory(bytes)': 290 },
+  { name: '11', 'memory(bytes)': 150 },
+  { name: '12', 'memory(bytes)': 100 },
+  { name: '13', 'memory(bytes)': 130 },
+  { name: '14', 'memory(bytes)': 0 },
+  { name: '15', 'memory(bytes)': 270 },
+  { name: '16', 'memory(bytes)': 280 },
+  { name: '17', 'memory(bytes)': 300 },
+  { name: '18', 'memory(bytes)': 100 },
+  { name: '19', 'memory(bytes)': 170 },
+  { name: '20', 'memory(bytes)': 290 },
+  { name: '21', 'memory(bytes)': 250 },
+  { name: '22', 'memory(bytes)': 270 },
+  { name: '23', 'memory(bytes)': 10 },
+  { name: '24', 'memory(bytes)': 100 },
+  { name: '25', 'memory(bytes)': 70 },
+  { name: '26', 'memory(bytes)': 150 },
+  { name: '27', 'memory(bytes)': 60 },
+  { name: '28', 'memory(bytes)': 100 },
+  { name: '29', 'memory(bytes)': 190 },
+  { name: '30', 'memory(bytes)': 290 },
 ];
 
 const SampleGraph = () => (
@@ -35,8 +45,7 @@ const SampleGraph = () => (
   }}
   >
     <div style={{ height: '500px', width: '700px' }}>
-      <LineChartComponent data={sampleData}>
-      </LineChartComponent>
+      <LineChartComponent forPreview={false} xLabel="July" yLabel="RAM(MBs)" data={sampleData} />
     </div>
   </div>
 );
--- src/components/LineChart/index.jsx
+++ src/components/LineChart/index.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
-import { LineChart, Line, ResponsiveContainer } from 'recharts';
+import {
+  LineChart,
+  Line,
+  ResponsiveContainer,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Label
+} from 'recharts';
 import './LineChart.css';
 
-const LineChartComponent = ({ data }) => {
+const LineChartComponent = ({
+  forPreview,
+  data,
+  yLabel,
+  xLabel
+}) => {
   return (
     <div className="LineChartContainer">
       <ResponsiveContainer width="100%" height="100%">
-        <LineChart data={data}>
-          <Line dot={false} strokeWidth="2px" dataKey="uv" stroke="#008AC1" />
-        </LineChart>
+        {!forPreview ? (
+          <LineChart data={data}>
+            <CartesianGrid strokeDasharray="3" />
+            <XAxis dataKey="name">
+              <Label className="ChartLabel" value={xLabel} position="insideBottomCenter" dy={12} />
+            </XAxis>
+            <YAxis dataKey="memory(bytes)">
+              <Label className="ChartLabel" value={yLabel} angle={270} position="insideLeft" dy={-10} />
+            </YAxis>
+            <Tooltip />
+            <Line dot strokeWidth="2px" dataKey="memory(bytes)" stroke="#008AC1" />
+          </LineChart>
+        ) : (
+          <LineChart>
+            <Line dot strokeWidth="2px" dataKey="memory(bytes)" stroke="#008AC1" />
+          </LineChart>
+        )}
       </ResponsiveContainer>
     </div>
   );
