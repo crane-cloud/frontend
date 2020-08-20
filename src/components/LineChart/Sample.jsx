bd65c4e1abd3fa5dd3a49454e5fe45f17f1a6020
StephenAraka <stevenaraka@gmail.com>
2020-08-20 03:30:38 +0300

    Change label

--- src/components/LineChart/Sample.jsx
+++ src/components/LineChart/Sample.jsx
@@ -3,36 +3,36 @@ import LineChartComponent from './index';
 import './LineChart.css';
 
 const sampleData = [
-  { name: '01', 'memory(bytes)': 250 },
-  { name: '02', 'memory(bytes)': 270 },
-  { name: '03', 'memory(bytes)': 10 },
-  { name: '04', 'memory(bytes)': 100 },
-  { name: '05', 'memory(bytes)': 70 },
-  { name: '06', 'memory(bytes)': 150 },
-  { name: '07', 'memory(bytes)': 60 },
-  { name: '08', 'memory(bytes)': 100 },
-  { name: '09', 'memory(bytes)': 190 },
-  { name: '10', 'memory(bytes)': 290 },
-  { name: '11', 'memory(bytes)': 150 },
-  { name: '12', 'memory(bytes)': 100 },
-  { name: '13', 'memory(bytes)': 130 },
-  { name: '14', 'memory(bytes)': 0 },
-  { name: '15', 'memory(bytes)': 270 },
-  { name: '16', 'memory(bytes)': 280 },
-  { name: '17', 'memory(bytes)': 300 },
-  { name: '18', 'memory(bytes)': 100 },
-  { name: '19', 'memory(bytes)': 170 },
-  { name: '20', 'memory(bytes)': 290 },
-  { name: '21', 'memory(bytes)': 250 },
-  { name: '22', 'memory(bytes)': 270 },
-  { name: '23', 'memory(bytes)': 10 },
-  { name: '24', 'memory(bytes)': 100 },
-  { name: '25', 'memory(bytes)': 70 },
-  { name: '26', 'memory(bytes)': 150 },
-  { name: '27', 'memory(bytes)': 60 },
-  { name: '28', 'memory(bytes)': 100 },
-  { name: '29', 'memory(bytes)': 190 },
-  { name: '30', 'memory(bytes)': 290 },
+  { name: 'July 01', 'memory(bytes)': 250 },
+  { name: 'July 02', 'memory(bytes)': 270 },
+  { name: 'July 03', 'memory(bytes)': 10 },
+  { name: 'July 04', 'memory(bytes)': 100 },
+  { name: 'July 05', 'memory(bytes)': 70 },
+  { name: 'July 06', 'memory(bytes)': 150 },
+  { name: 'July 07', 'memory(bytes)': 60 },
+  { name: 'July 08', 'memory(bytes)': 100 },
+  { name: 'July 09', 'memory(bytes)': 190 },
+  { name: 'July 10', 'memory(bytes)': 290 },
+  { name: 'July 11', 'memory(bytes)': 150 },
+  { name: 'July 12', 'memory(bytes)': 100 },
+  { name: 'July 13', 'memory(bytes)': 130 },
+  { name: 'July 14', 'memory(bytes)': 0 },
+  { name: 'July 15', 'memory(bytes)': 270 },
+  { name: 'July 16', 'memory(bytes)': 280 },
+  { name: 'July 17', 'memory(bytes)': 300 },
+  { name: 'July 18', 'memory(bytes)': 100 },
+  { name: 'July 19', 'memory(bytes)': 170 },
+  { name: 'July 20', 'memory(bytes)': 290 },
+  { name: 'July 21', 'memory(bytes)': 250 },
+  { name: 'July 22', 'memory(bytes)': 270 },
+  { name: 'July 23', 'memory(bytes)': 10 },
+  { name: 'July 24', 'memory(bytes)': 100 },
+  { name: 'July 25', 'memory(bytes)': 70 },
+  { name: 'July 26', 'memory(bytes)': 150 },
+  { name: 'July 27', 'memory(bytes)': 60 },
+  { name: 'July 28', 'memory(bytes)': 100 },
+  { name: 'July 29', 'memory(bytes)': 190 },
+  { name: 'July 30', 'memory(bytes)': 290 },
 ];
 
 const SampleGraph = () => (
@@ -45,7 +45,7 @@ const SampleGraph = () => (
   }}
   >
     <div style={{ height: '500px', width: '700px' }}>
-      <LineChartComponent xLabel="July" yLabel="RAM(MBs)" data={sampleData} />
+      <LineChartComponent xLabel="Time" yLabel="RAM(MBs)" data={sampleData} />
     </div>
   </div>
 );
