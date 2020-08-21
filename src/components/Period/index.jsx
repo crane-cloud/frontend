4bde319d3911b42cfcefdf6996d3c289d1178974
StephenAraka <stevenaraka@gmail.com>
2020-08-21 10:19:06 +0300

    Add style for active button

--- src/components/Period/index.jsx
+++ src/components/Period/index.jsx
@@ -29,15 +29,16 @@ class Period extends React.Component {
 
 
   render() {
+    const { period } = this.state;
     return (
       <div className="PeriodButtonsContainer">
-        <div className="PeriodButton" name="1hour" value="1h" role="presentation" onClick={(e) => this.handleChange(e)}>1h</div>
-        <div className="PeriodButton" name="1day" value="1d" role="presentation" onClick={(e) => this.handleChange(e)}>1d</div>
-        <div className="PeriodButton" name="7days" value="7d" role="presentation" onClick={(e) => this.handleChange(e)}>7d</div>
-        <div className="PeriodButton" name="1month" value="1m" role="presentation" onClick={(e) => this.handleChange(e)}>1m</div>
-        <div className="PeriodButton" name="3months" value="3m" role="presentation" onClick={(e) => this.handleChange(e)}>3m</div>
-        <div className="PeriodButton" name="1year" value="1y" role="presentation" onClick={(e) => this.handleChange(e)}>1y</div>
-        <div className="PeriodButton" name="all" value="all" role="presentation" onClick={(e) => this.handleChange(e)}>all</div>
+        <div className={`${period === '1h' && 'PeriodButtonActive'} PeriodButton`} name="1hour" value="1h" role="presentation" onClick={(e) => this.handleChange(e)}>1h</div>
+        <div className={`${period === '1d' && 'PeriodButtonActive'} PeriodButton`} name="1day" value="1d" role="presentation" onClick={(e) => this.handleChange(e)}>1d</div>
+        <div className={`${period === '7d' && 'PeriodButtonActive'} PeriodButton`} name="7days" value="7d" role="presentation" onClick={(e) => this.handleChange(e)}>7d</div>
+        <div className={`${period === '1m' && 'PeriodButtonActive'} PeriodButton`} name="1month" value="1m" role="presentation" onClick={(e) => this.handleChange(e)}>1m</div>
+        <div className={`${period === '3m' && 'PeriodButtonActive'} PeriodButton`} name="3months" value="3m" role="presentation" onClick={(e) => this.handleChange(e)}>3m</div>
+        <div className={`${period === '1y' && 'PeriodButtonActive'} PeriodButton`} name="1year" value="1y" role="presentation" onClick={(e) => this.handleChange(e)}>1y</div>
+        <div className={`${period === 'all' && 'PeriodButtonActive'} PeriodButton`} name="all" value="all" role="presentation" onClick={(e) => this.handleChange(e)}>all</div>
       </div>
     );
   }
