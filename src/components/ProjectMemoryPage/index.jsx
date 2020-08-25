e381e21db402a7f8a4f7683792f4752d14bc9209
StephenAraka <stevenaraka@gmail.com>
2020-08-25 21:19:54 +0300

    Add dummy graph

--- src/components/ProjectMemoryPage/index.jsx
+++ src/components/ProjectMemoryPage/index.jsx
@@ -6,6 +6,38 @@ import Header from '../Header';
 import Spinner from '../Spinner';
 import SideBar from '../SideBar';
 import './ProjectMemoryPage.css';
+import getProjectMemory from '../../redux/actions/projectMemory';
+import MetricsCard from '../MetricsCard';
+import PeriodSelector from '../Period';
+import LineChartComponent from '../LineChart';
+
+const sampleData = [
+  { name: 'Sample Metric 1', uv: 250 },
+  { name: 'Sample Metric 2', uv: 270 },
+  { name: 'Sample Metric 2', uv: 10 },
+  { name: 'Sample Metric 2', uv: 100 },
+  { name: 'Sample Metric 2', uv: 70 },
+  { name: 'Sample Metric 2', uv: 150 },
+  { name: 'Sample Metric 2', uv: 60 },
+  { name: 'Sample Metric 2', uv: 100 },
+  { name: 'Sample Metric 2', uv: 190 },
+  { name: 'Sample Metric 2', uv: 290 },
+  { name: 'Sample Metric 2', uv: 150 },
+  { name: 'Sample Metric 2', uv: 100 },
+  { name: 'Sample Metric 2', uv: 130 },
+  { name: 'Sample Metric 2', uv: 0 },
+  { name: 'Sample Metric 2', uv: 270 },
+  { name: 'Sample Metric 2', uv: 280 },
+  { name: 'Sample Metric 2', uv: 300 },
+  { name: 'Sample Metric 2', uv: 100 },
+  { name: 'Sample Metric 2', uv: 170 },
+  { name: 'Sample Metric 2', uv: 290 },
+];
+
+// this function is meant to shuffle the dummy data array
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
 
 class ProjectMemoryPage extends React.Component {
   constructor(props) {
@@ -14,12 +46,16 @@ class ProjectMemoryPage extends React.Component {
     };
   }
 
+  componentDidMount() {
+    const { match: { params }, getProjectMemory } = this.props;
+    const { projectID } = params;
+    getProjectMemory(projectID, {});
+  }
+
   render() {
     const {
       match: { params }
     } = this.props;
-
-    console.log(params);
 
     const { name } = this.props.location;
     const { appID } = params;
@@ -41,7 +77,9 @@ class ProjectMemoryPage extends React.Component {
               />
             </div>
             <div className="ContentSection">
-              hey
+              <MetricsCard className="MetricsCardGraph" title={<PeriodSelector onChange={() => {}} />}>
+                <LineChartComponent yLabel="Memory(bytes)" xLabel="Time" lineDataKey="uv" data={shuffle(sampleData)} />
+              </MetricsCard>
             </div>
           </div>
         </div>
@@ -61,11 +99,16 @@ ProjectMemoryPage.defaultProps = {
 };
 
 const mapStateToProps = (state) => {
-
+  const { isFetching, metrics, message: metricsMessage } = state.projectMemoryReducer;
+  return {
+    isFetching,
+    metrics,
+    metricsMessage
+  };
 };
 
 const mapDispatchToProps = {
-
+  getProjectMemory
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProjectMemoryPage);
