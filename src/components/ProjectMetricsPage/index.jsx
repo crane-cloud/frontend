1c8993e2ce38122c6e68f82ec0b2515791461ee2
neelxie <kidricederek@gmail.com>
2020-08-28 11:13:08 +0300

    Create component

--- src/components/ProjectMetricsPage/index.jsx
+++ src/components/ProjectMetricsPage/index.jsx
@@ -1,55 +1,51 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import InformationBar from '../InformationBar';
-import Header from '../Header';
-import SideBar from '../SideBar';
-import MetricsCard from '../MetricsCard';
-import { ReactComponent as MetricIcon } from '../../assets/images/resource-icon.svg';
-import './ProjectMetricsPage.css';
-import LineChartComponent from '../LineChart';
-
+import React from "react";
+import PropTypes from "prop-types";
+import InformationBar from "../InformationBar";
+import Header from "../Header";
+import SideBar from "../SideBar";
+import MetricsCard from "../MetricsCard";
+import { ReactComponent as MetricIcon } from "../../assets/images/resource-icon.svg";
+import "./ProjectMetricsPage.css";
+import LineChartComponent from "../LineChart";
 
 class ProjectMetricsPage extends React.Component {
 
-  state = {
-    appRelatedInfo: this.props.location.state
-  };
-
-  static getDerivedStateFromProps(props, state) {
-    if (props.location.state !== state.appRelatedInfo) {
-      return {
-        appRelatedInfo: props.location.state
-      };
-    }
-    return null;
-  }
-
   render() {
-    const { appName, appUrl, liveAppStatus } = this.state.appRelatedInfo;
-    const { params } = this.props.match;
-    const { projectID, userID, appID } = params;
-    
+    const {
+      match: { params },
+      projects,
+    } = this.props;
+
+    const { projectID, userID } = params;
+    const projectDetails = {
+      name: this.getProjectName(projects, params.projectID),
+      description: this.getProjectDescription(projects, params.projectID),
+    };
+
+    localStorage.setItem("project", JSON.stringify(projectDetails));
+
     return (
       <div className="Page">
-        <div className="TopBarSection"><Header /></div>
+        <div className="TopBarSection">
+          <Header />
+        </div>
         <div className="MainSection">
           <div className="SideBarSection">
-            <SideBar 
-              name={appName}
+            <SideBar
+              name={this.getProjectName(projects, params.projectID)}
               params={params}
+              description={this.getProjectName(projects, params.projectID)}
               pageRoute={this.props.location.pathname}
-              allMetricsLink={`/users/${userID}/projects/${projectID}/apps/${appID}/metrics/`}
-              cpuLink={`/users/${userID}/projects/${projectID}/apps/${appID}/cpu/`}
-              memoryLink={`/users/${userID}/projects/${projectID}/apps/${appID}/memory/`}
-              storageLink={`/users/${userID}/projects/${projectID}/apps/${appID}/storage/`}
-              networkLink={`/users/${userID}/projects/${projectID}/apps/${appID}/network/`}
-              />
+              allMetricsLink={`/users/${userID}/projects/${projectID}/metrics/`}
+              cpuLink={`/users/${userID}/projects/${projectID}/cpu/`}
+              memoryLink={`/users/${userID}/projects/${projectID}/memory/`}
+              storageLink={`/users/${userID}/projects/${projectID}/storage/`}
+              networkLink={`/users/${userID}/projects/${projectID}/network/`}
+            />
           </div>
           <div className="MainContentSection">
             <div className="InformationBarSection">
-              <InformationBar
-                header="Project Metrics"
-              />
+              <InformationBar header="Project Metrics" />
             </div>
             <div className="ContentSection">
               <div className="TopCardsSection">
@@ -74,9 +70,9 @@ class ProjectMetricsPage extends React.Component {
 ProjectMetricsPage.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
-      projectID: PropTypes.string.isRequired
-    }).isRequired
-  }).isRequired
+      projectID: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 export default ProjectMetricsPage;
