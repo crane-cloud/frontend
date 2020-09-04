ec827d30ddc88ae1d5bc03467fd557bcf5c255e5
neelxie <kidricederek@gmail.com>
2020-09-04 07:38:31 +0300

    Add component to router

--- src/router.js
+++ src/router.js
@@ -34,6 +34,7 @@ import AppSettingsPage from './components/AppSettingsPage';
 import ProjectMemoryPage from './components/ProjectMemoryPage';
 import ProjectCPUPage from './components/ProjectCPUPage';
 import ProjectNetworkPage from './components/ProjectNetworkPage';
+import AppMemoryPage from './components/AppMemoryPage';
 
 // Protected route should have token. If not, login.
 const ProtectedRoute = ({ isAllowed, ...props }) => (
@@ -60,6 +61,7 @@ const Routes = () => (
       {/* projects */}
       <ProtectedRoute isAllowed={hasToken} exact path="/users/:userID/projects" component={UserProjectsPage} />
       <ProtectedRoute isAllowed={hasToken} exact path="/users/:userID/projects/:projectID/apps" component={AppsPage} />
+      <ProtectedRoute isAllowed={hasToken} exact path="/users/:userID/projects/:projectID/apps/:appID/memory" component={AppMemoryPage} />
       <ProtectedRoute isAllowed={hasToken} exact path="/users/:userID/projects/:projectID/apps/:appID/metrics" component={AppMetricsPage} />
       <ProtectedRoute isAllowed={hasToken} exact path="/users/:userID/projects/:projectID/apps/:appID/settings" component={AppSettingsPage} />
       <ProtectedRoute isAllowed={hasToken} exact path="/users/:userID/projects/:projectID/memory/" component={ProjectMemoryPage} />
