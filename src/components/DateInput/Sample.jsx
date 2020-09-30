b23c6058ac00a6448e987f1130cbb986d520b304
StephenAraka <stevenaraka@gmail.com>
2020-09-30 14:04:47 +0300

    create files and sample route

---
+++ src/components/DateInput/Sample.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import DateInput from './index';
+
+const style = {
+
+};
+
+const Sample = () => {
+  return (
+    <div style={style}>
+      <DateInput />
+    </div>
+  );
+};
+
+export default Sample;
---
+++ src/components/DateInput/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const DateInput = () => {
+  return (
+    <div className="DateInputContainer">
+      <div className="DateInputWrapper">
+        <div className="DateInputLabel">
+
+        </div>
+        <div className="DateInputDisplay">
+
+        </div>
+      </div>
+      <div className="DateInputCalendar">
+        {/* <Calendar /> */}
+      </div>
+    </div>
+  );
+};
+
+export default DateInput;
