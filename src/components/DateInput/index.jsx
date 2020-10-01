7347b199bb57f45bfdf7c995541c22cc5ea85f64
StephenAraka <stevenaraka@gmail.com>
2020-10-01 08:18:11 +0300

    show date on input field

--- src/components/DateInput/index.jsx
+++ src/components/DateInput/index.jsx
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Calendar from '../Calendar';
 import './DateInput.css';
 
+const monthNames = [
+  'January', 'February', 'March', 'April',
+  'May', 'June', 'July', 'August',
+  'September', 'October', 'November', 'December'
+];
+
+const today = new Date().getDate();
+const currentMonth = new Date().getMonth();
+const currentYear = new Date().getFullYear();
+
 const DateInput = ({ position, label }) => {
   const [showCalendar, setShowCalendar] = useState(true);
+  const [date, setDate] = useState(null);
 
-  const getDate = (date) => {
-    console.log(date);
-  };
+  const getDate = (date) => setDate(date);
 
   return (
     <div className="DateInputContainer">
@@ -17,7 +26,11 @@ const DateInput = ({ position, label }) => {
           {label}
         </div>
         <div className="DateInputDisplay">
-          Jul 22, 2020
+          {date ? (
+            `${date.month}, ${date.day}, ${date.year}`
+          ) : (
+            `${currentMonth}, ${today}, ${currentYear}`
+          )}
         </div>
         <div className={`DateInputCalendar ${position === 'left' && 'PositionLeft'}`}>
           {showCalendar && <Calendar onChange={getDate} />}
