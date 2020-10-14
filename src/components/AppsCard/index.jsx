345a42a98c48fa421dee984b4c251815a9f977f9
neelxie <kidricederek@gmail.com>
2020-10-14 16:11:51 +0300

    remove dummy data metrics for card

--- src/components/AppsCard/index.jsx
+++ src/components/AppsCard/index.jsx
@@ -5,33 +5,6 @@ import AppStatus from '../AppStatus';
 import LineChartComponent from '../LineChart';
 import './AppsCard.css';
 
-const sampleData = [
-  { name: 'Sample Metric 1', uv: 250 },
-  { name: 'Sample Metric 2', uv: 270 },
-  { name: 'Sample Metric 2', uv: 10 },
-  { name: 'Sample Metric 2', uv: 100 },
-  { name: 'Sample Metric 2', uv: 70 },
-  { name: 'Sample Metric 2', uv: 150 },
-  { name: 'Sample Metric 2', uv: 60 },
-  { name: 'Sample Metric 2', uv: 100 },
-  { name: 'Sample Metric 2', uv: 190 },
-  { name: 'Sample Metric 2', uv: 290 },
-  { name: 'Sample Metric 2', uv: 150 },
-  { name: 'Sample Metric 2', uv: 100 },
-  { name: 'Sample Metric 2', uv: 130 },
-  { name: 'Sample Metric 2', uv: 0 },
-  { name: 'Sample Metric 2', uv: 270 },
-  { name: 'Sample Metric 2', uv: 280 },
-  { name: 'Sample Metric 2', uv: 300 },
-  { name: 'Sample Metric 2', uv: 100 },
-  { name: 'Sample Metric 2', uv: 170 },
-  { name: 'Sample Metric 2', uv: 290 },
-];
-
-// this function is meant to shuffle the dummy data array
-function shuffle(array) {
-  return array.sort(() => Math.random() - 0.5);
-}
 
 const AppsCard = (props) => {
   const {
@@ -60,7 +33,7 @@ const AppsCard = (props) => {
         <div className="AppCardBottomSection">
           <div className="AppGraphSummaryLabel">Memory (1d)</div>
           <div className="AppGraphSummary">
-            <LineChartComponent lineDataKey="uv" preview data={shuffle(sampleData)} />
+            <LineChartComponent lineDataKey="uv" preview  />
           </div>
         </div>
       </div>
