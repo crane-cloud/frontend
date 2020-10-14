d453d59ecb45c5e47c2b62e6e74cf6a4d33f2a5b
StephenAraka <stevenaraka@gmail.com>
2020-10-14 08:19:46 +0300

    add component files

---
+++ src/components/TimeInput/Sample.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Time from './index';
+
+const style = {
+  marginTop: '300px',
+  marginLeft: '300px',
+};
+
+const Sample = () => {
+  return (
+    <div style={style}>
+      <Time />
+    </div>
+  );
+};
+
+export default Sample;
---
+++ src/components/TimeInput/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TimeInput.css';
+
+const TimeInput = () => {
+  return (
+    <div>
+      holla
+    </div>
+  );
+};
+
+export default TimeInput;
