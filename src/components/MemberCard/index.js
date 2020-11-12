5f04e50f669f87e6eee65e1b9a71306233168daa
neelxie <kidricederek@gmail.com>
2020-11-12 05:13:58 +0300

    restructure team member list

--- src/components/MemberCard/index.js
+++ src/components/MemberCard/index.js
@@ -6,7 +6,7 @@ const MemberCard = (props) => {
   const { name, title, icon } = props;
   return (
     <div className="MemberCard">
-      <div className="CardImageDiv" style={{ backgroundImage: `url(${icon})` }} />
+      <div className="MemberImageDiv" style={{ backgroundImage: `url(${icon})` }} />
       <div className="BottomContainer">
         <div className="MemberCardName">{name}</div>
         <div className="MemberCardDesc">{title}</div>
