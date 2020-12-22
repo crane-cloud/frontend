1eacd39d6ab9d99a088a76b68a3640ca9d5021b3
StephenAraka <stevenaraka@gmail.com>
2020-12-22 15:57:09 +0200

    vertically Center content

--- src/components/404page/index.jsx
+++ src/components/404page/index.jsx
@@ -6,17 +6,13 @@ import './404page.css';
 
 const PageNotFound = () => {
   return (
-    <div>
+    <div className="Page">
       <Header />
-      <div>
-        <div className="ErrorPageContainer">
-          <div className="ErrorPageFirstDiv">
-            <div className="ErrorPageDigit">404</div>
-          </div>
-          <div className="ErrorPageText">
-            <div className="ErrorPageTextFirst">PAGE NOT FOUND</div>
-            <div className="ErrorPageTextSecond">Sorry, the page you are looking for does not exist.</div>
-          </div>
+      <div className="ErrorPageConent">
+        <div className="ErrorPageDigit">404</div>
+        <div className="ErrorPageText">
+          <div className="ErrorPageTextFirst">PAGE NOT FOUND</div>
+          <div className="ErrorPageTextSecond">Sorry, the page you are looking for does not exist.</div>
           <div className="ErrorPageButton">
             <Link to="/"><PrimaryButton label="Home" /></Link>
           </div>
