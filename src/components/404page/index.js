7dd6e1fc869f9149fea6d0263004eb7a29481391
neelxie <kidricederek@gmail.com>
2020-12-22 09:27:08 +0300

    add styling to components

--- src/components/404page/index.js
+++ src/components/404page/index.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { Link} from 'react-router-dom';
 import Header from "../Header";
 import PrimaryButton from '../PrimaryButton';
+import './404page.css';
 
 const WrongUrl = () => {
     return (
         <div>
             <Header />
             <div>
-                <div>
-                    <div></div>
+                <div className="ErrorPageContainer">
                     <div>
-                        <div></div>
-                        <div></div>
+                        <div className="ErrorPageDigit">404</div>
                     </div>
                     <div>
+                        <div>PAGE NOT FOUND.</div>
+                        <div>Sorry, the page you are looking for does not exist.</div>
+                    </div>
+                    <div className="ErrorPageButton">
                         <Link to="/"><PrimaryButton label="Home" /></Link>
                     </div>
                 </div>
