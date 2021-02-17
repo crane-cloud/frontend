b28e6b38967eb93afb81c5d80a6859795250c1ef
neelxie <kidricederek@gmail.com>
2021-02-17 16:29:47 +0300

    change .env variables

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,11 @@
 
-import axios from "axios"
+import axios from 'axios';
 import {
-   API_BASE_URL
+  API_BASE_URL
 } from './config';
 
 const instance = axios.create({
-   baseURL: API_BASE_URL,
+  baseURL: API_BASE_URL,
 });
 
 instance.defaults.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
