0d77ef478a90a73bd86e08940d865d60ea6645ee
neelxie <kidricederek@gmail.com>
2021-04-07 18:22:01 +0300

    Make changes

--- src/components/LandingFooter/index.js
+++ src/components/LandingFooter/index.js
@@ -1,11 +1,72 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { DOCS_URL } from '../../config';
 import './LandingFooter.css';
+import { ReactComponent as TwitterIcon } from '../../assets/images/twitter.svg';
+import { ReactComponent as FacebookIcon } from '../../assets/images/facebook.svg';
 
 const LandingFooter = () => (
   <footer className="LandingFooter">
-    <p className="LandingFooterCopyright">
-      Copyright {new Date().getFullYear()} Crane Cloud. All Rights Reserved.
-    </p>
+    <div className="FooterRow">
+      <div className="FooterParent">
+        <div className="FooterChildOne">
+          <div className="">
+            <h5 className="FooterLogoName LogoBrandName CenteredDiv">Crane Cloud</h5>
+          </div>
+          <div></div>
+          <div>
+            <p className="LandingFooterCopyright LogoBrandName">
+              Copyright {new Date().getFullYear()} Crane Cloud, Ltd.
+            </p>
+            <p className="CountryOfOrigin">
+              Made in Uganda.
+            </p>
+            <div className="SocialsIcons">
+              <TwitterIcon />
+              <FacebookIcon />
+            </div>
+          </div>
+          <div></div>
+        </div>
+        <div className="FooterChildTwo">
+          <h5 className="CountryOfOrigin FooterLogoName">Company</h5>
+          <ul className="FooterList">
+            <li className="CountryOfOrigin">
+              <Link to="/team">Team</Link>
+            </li>
+            <li className="CountryOfOrigin">Terms and Privacy</li>
+          </ul>
+        </div>
+        <div className="FooterChildTwo">
+          <h5 className="CountryOfOrigin FooterLogoName">Resources</h5>
+          <ul className="FooterList">
+            <li className="CountryOfOrigin">
+              <a href="https://docs.cranecloud.io/quickstart/quickstart/" rel="noopener noreferrer" target="_blank">Quick Start Guide</a>
+            </li>
+            <li className="CountryOfOrigin">
+              <a href={`${DOCS_URL}`} rel="noopener noreferrer" target="_blank">Docs</a>
+            </li>
+          </ul>
+        </div>
+        <div className="FooterChildTwo">
+          <h5 className="CountryOfOrigin FooterLogoName">Community</h5>
+          <ul className="FooterList">
+            <li className="CountryOfOrigin">Contribute</li>
+            <li className="CountryOfOrigin">
+              <a href="https://medium.com/cranecloud" rel="noopener noreferrer" target="_blank">Blog</a>
+            </li>
+          </ul>
+        </div>
+        <div className="FooterChildTwo">
+          {/* <h5 className="CountryOfOrigin">Community</h5>
+          <ul className="FooterList">
+            <li className="CountryOfOrigin">Contribute</li>
+            <li className="CountryOfOrigin">Blog</li>
+          </ul> */}
+        </div>
+      </div>
+    </div>
+    
   </footer>
 );
 
