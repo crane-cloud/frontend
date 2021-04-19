180cd1a538c618a4ef58bc9f3c10be7d40e65168
neelxie <kidricederek@gmail.com>
2021-04-19 20:42:25 +0300

    Add provision for longer name

--- src/components/UserProjectsPage/index.jsx
+++ src/components/UserProjectsPage/index.jsx
@@ -128,9 +128,9 @@ class UserProjectsPage extends React.Component {
       this.setState({
         error: 'name may only contain letters and a hypen -'
       });
-    } else if (projectName.length > 18) {
+    } else if (projectName.length > 22) {
       this.setState({
-        error: 'project name may not exceed 18 characters'
+        error: 'project name may not exceed 22 characters'
       });
     } else {
       const newProject = {
