1945090bf111d71c7f2ef46e0840a071a91dc086
neelxie <kidricederek@gmail.com>
2021-05-03 12:21:01 +0300

    Add database password copying

--- src/components/DBSettingsPage/index.jsx
+++ src/components/DBSettingsPage/index.jsx
@@ -32,7 +32,8 @@ class DBSettingsPage extends React.Component {
       portChecked: false,
       userChecked: false,
       hostChecked: false,
-      uriChecked: false
+      uriChecked: false,
+      passwordChecked: false
     };
 
     this.handleDeleteDatabase = this.handleDeleteDatabase.bind(this);
@@ -48,6 +49,7 @@ class DBSettingsPage extends React.Component {
     this.userOnClick = this.userOnClick.bind(this);
     this.hostOnClick = this.hostOnClick.bind(this);
     this.uriOnClick = this.uriOnClick.bind(this);
+    this.passwordOnClick = this.passwordOnClick.bind(this);
   }
   componentDidMount(){
     const { clearDatabaseResetState } = this.props;
@@ -157,6 +159,13 @@ class DBSettingsPage extends React.Component {
     navigator.clipboard.writeText(`${`mysql -u ${dbInfo.user} -p -P ${dbInfo.port} -h ${dbInfo.host} -D ${dbInfo.name}`}`);
     this.setState({uriChecked: true});
   }
+
+  passwordOnClick(){
+    const { databaseID } = this.props.match.params;
+    const dbInfo = this.getDatabaseInfo(databaseID);
+    navigator.clipboard.writeText(dbInfo.password);
+    this.setState({passwordChecked: true});
+  }
   
   render() {
     const {
@@ -177,7 +186,8 @@ class DBSettingsPage extends React.Component {
       portChecked,
       hostChecked,
       userChecked,
-      uriChecked
+      uriChecked,
+      passwordChecked
     } = this.state;
     return (
       <div className="Page">
@@ -230,6 +240,10 @@ class DBSettingsPage extends React.Component {
                     <div className="DBColumn1 DBThead">Password</div>
                     <div className="DBColumn">
                       {hidden? '***************************': dbInfo.password}
+                    </div>
+                    <div className="DBIcon">
+                      <CopyText onClick={this.passwordOnClick}/>
+                      {passwordChecked ? <Checked /> : null}
                     </div>
                     <div className="DBPassword">
                       <div onClick={this.togglePassword}>{hidden ?<Open /> :<Closed />}</div>
