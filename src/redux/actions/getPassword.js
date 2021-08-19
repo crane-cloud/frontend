fdff6fe585319ed858df92ceedfdfa4b2810d07c
neelxie <kidricederek@gmail.com>
2021-08-19 17:20:12 +0300

    Add actions

---
+++ src/redux/actions/getPassword.js
@@ -0,0 +1,32 @@
+import axios from "../../axios";
+import {
+  GETTING_PASSWORD,
+  PASSWORD_FAIL,
+  PASSWORD_SUCCESS,
+} from "./actionTypes";
+
+export const startTheFetch = () => ({
+  type: GETTING_PASSWORD,
+});
+
+export const getPasswordSuccess = (response) => ({
+  type: PASSWORD_SUCCESS,
+  payload: response.data.data.password,
+});
+
+export const getPasswordFailed = (error) => ({
+  type: PASSWORD_FAIL,
+  payload: error.status,
+});
+
+const getPassword = (projectID, databaseID) => (dispatch) => {
+  dispatch(startTheFetch());
+  return axios
+    .get(`/projects/${projectID}/databases/${databaseID}/password`)
+    .then((response) => dispatch(getPasswordSuccess(response)))
+    .catch((error) => {
+      dispatch(getPasswordFailed(error));
+    });
+};
+
+export default getPassword;
