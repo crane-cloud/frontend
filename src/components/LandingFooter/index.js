9db388a864255408cf23912015c3d68c8efa90bd
neelxie <kidricederek@gmail.com>
2021-08-30 17:27:55 +0300

    Add privacy policy to landing page

--- src/components/LandingFooter/index.js
+++ src/components/LandingFooter/index.js
@@ -66,6 +66,9 @@ const LandingFooter = () => (
                 <li className="TextDivSpacing">
                   <Link to="/terms-of-service" target="_blank">Terms of service</Link>
                 </li>
+                <li className="TextDivSpacing">
+                  <Link to="/privacy-policy" target="_blank">Privacy policy</Link>
+                </li>
               </ul>
             </div>
             <div className="FooterChildTwo">
