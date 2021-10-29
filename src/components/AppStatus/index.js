218cdc78694db0c91f36bd66b344b31463d2bbd4
neelxie <kidricederek@gmail.com>
2021-10-29 12:55:20 +0300

    Add new changes

--- src/components/AppStatus/index.js
+++ src/components/AppStatus/index.js
@@ -1,36 +1,33 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 import "./AppStatus.css";
 
-class AppStatus extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { statusClassName: "StatusSignal StatusOther" };
-    this.setClassName = this.setClassName.bind(this);
-  }
+import React, { useEffect, useState } from "react";
 
-  setClassName() {
-    const currentClassName = this.props.appStatus;
-    if (currentClassName === "running") {
-      this.setState({ statusClassName: "StatusSignal StatusRunning" });
-    } else if (currentClassName === "failed") {
-      this.setState({ statusClassName: "StatusSignal StatusFailed" });
-    } else {
-      // let the default class Name remain
-    }
-    return currentClassName;
-  }
-  componentDidMount() {
-    this.setClassName();
-  }
-  render() {
-    return (
-      <div>
-        <div className="StatusWrapper">
-          <div className={this.state.statusClassName} />
-        </div>
+const AppStatus = (props) => {
+  const [statusClassName, setStatusClassName] = useState(
+    "StatusSignal StatusOther"
+  );
+
+  useEffect(() => {
+    const setClassName = () => {
+      let currentClassName = props.appStatus;
+      if (currentClassName === "running") {
+        setStatusClassName("StatusSignal StatusRunning");
+      } else if (currentClassName === "failed") {
+        setStatusClassName("StatusSignal StatusFailed");
+      } else {
+        // let default class remain
+      }
+    };
+    setClassName();
+  }, [props.appStatus]);
+  return (
+    <div>
+      <div className="StatusWrapper">
+        <div className={statusClassName} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default AppStatus;
