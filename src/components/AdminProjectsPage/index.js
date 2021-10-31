77b65dc7e0bb9dbbbf5fdc1f1a10e3895583b800
neelxie <kidricederek@gmail.com>
2021-10-31 17:50:11 +0300

    refactor admin projects page

--- src/components/AdminProjectsPage/index.js
+++ src/components/AdminProjectsPage/index.js
@@ -1,5 +1,4 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import "./AdminProjectsPage.css";
 import InformationBar from "../InformationBar";
@@ -8,19 +7,16 @@ import SideNav from "../SideNav";
 import getAdminProjects from "../../redux/actions/adminProjects";
 import getUsersList from "../../redux/actions/users";
 import Spinner from "../Spinner";
+import { useParams } from "react-router-dom";
 
-class AdminProjectsPage extends React.Component {
-  componentDidMount() {
-    const { getAdminProjects, getUsersList } = this.props;
-    getAdminProjects();
-    getUsersList();
-  }
+const AdminProjectsPage = ({adminProjects, usersList}) => {
 
-  getUserName(userId) {
+  const {clusterID } = useParams();
+
+  const getUserName = (userId) => {
     let username = "";
-    const { users, isFetched } = this.props;
-    if (isFetched) {
-      users.map((user) => {
+    if (usersList.isFetched) {
+      usersList.users.map((user) => {
         if (userId === user.id) {
           username = user.name;
         }
@@ -28,121 +24,92 @@ class AdminProjectsPage extends React.Component {
       });
     }
     return username;
-  }
+  };
 
-  render() {
-    const { projects, isRetrieving, isRetrieved } = this.props;
-    const clusterName = localStorage.getItem("clusterName");
-    const {
-      match: { params },
-    } = this.props;
+  useEffect(() => {
+    getAdminProjects();
+    getUsersList();
+  }, []);
 
-    return (
-      <div className="MainPage">
-        <div className="TopBarSection">
-          <Header />
+  const clusterName = localStorage.getItem("clusterName");
+
+  return (
+    <div className="MainPage">
+      <div className="TopBarSection">
+        <Header />
+      </div>
+      <div className="MainSection">
+        <div className="SideBarSection">
+          <SideNav clusterName={clusterName} clusterId={clusterID} />
         </div>
-        <div className="MainSection">
-          <div className="SideBarSection">
-            <SideNav clusterName={clusterName} clusterId={params.clusterID} />
+        <div className="MainContentSection">
+          <div className="InformationBarSection">
+            <InformationBar header="Projects" showBtn={false} />
           </div>
-          <div className="MainContentSection">
-            <div className="InformationBarSection">
-              <InformationBar header="Projects" showBtn={false} />
-            </div>
-            <div className="ContentSection">
-              <div
-                className={
-                  isRetrieving
-                    ? "ResourcesTable LoadingResourcesTable"
-                    : "ResourcesTable"
-                }
-              >
-                <table>
-                  <thead className="uppercase">
-                    <tr>
-                      <th>name</th>
-                      <th>owner</th>
-                      <th>description</th>
+          <div className="ContentSection">
+            <div
+              className={
+                adminProjects.isRetrieving
+                  ? "ResourcesTable LoadingResourcesTable"
+                  : "ResourcesTable"
+              }
+            >
+              <table>
+                <thead className="uppercase">
+                  <tr>
+                    <th>name</th>
+                    <th>owner</th>
+                    <th>description</th>
+                  </tr>
+                </thead>
+                {adminProjects.isRetrieving ? (
+                  <tbody>
+                    <tr className="TableLoading">
+                      <td>
+                        <div className="SpinnerWrapper">
+                          <Spinner size="big" />
+                        </div>
+                      </td>
                     </tr>
-                  </thead>
-                  {isRetrieving ? (
-                    <tbody>
-                      <tr className="TableLoading">
-                        <td>
-                          <div className="SpinnerWrapper">
-                            <Spinner size="big" />
-                          </div>
-                        </td>
-                      </tr>
-                    </tbody>
-                  ) : (
-                    <tbody>
-                      {projects !== 0 &&
-                        projects !== undefined &&
-                        projects.map((project) => (
-                          <tr key={projects.indexOf(project)}>
-                            <td>{project.name}</td>
-                            <td>{this.getUserName(project.owner_id)}</td>
-                            <td>{project.description}</td>
-                          </tr>
-                        ))}
-                    </tbody>
-                  )}
-                </table>
-                {isRetrieved && projects.length === 0 && (
-                  <div className="NoResourcesMessage">
-                    <p>No projects available</p>
-                  </div>
+                  </tbody>
+                ) : (
+                  <tbody>
+                    {adminProjects.projects !== 0 &&
+                      adminProjects.projects !== undefined &&
+                      adminProjects.projects.map((project) => (
+                        <tr key={adminProjects.projects.indexOf(project)}>
+                          <td>{project.name}</td>
+                          <td>{getUserName(project.owner_id)}</td>
+                          <td>{project.description}</td>
+                        </tr>
+                      ))}
+                  </tbody>
                 )}
-                {!isRetrieving && !isRetrieved && (
-                  <div className="NoResourcesMessage">
-                    <p>
-                      Oops! Something went wrong! Failed to retrieve projects.
-                    </p>
-                  </div>
-                )}
-              </div>
+              </table>
+              {adminProjects.isRetrieved && adminProjects.projects.length === 0 && (
+                <div className="NoResourcesMessage">
+                  <p>No projects available</p>
+                </div>
+              )}
+              {!adminProjects.isRetrieving && !adminProjects.isRetrieved && (
+                <div className="NoResourcesMessage">
+                  <p>
+                    Oops! Something went wrong! Failed to retrieve projects.
+                  </p>
+                </div>
+              )}
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
-
-AdminProjectsPage.propTypes = {
-  projects: PropTypes.arrayOf(PropTypes.object),
-  isRetrieved: PropTypes.bool,
-  isRetrieving: PropTypes.bool,
-  users: PropTypes.arrayOf(PropTypes.object),
-  isFetched: PropTypes.bool,
-  getAdminProjects: PropTypes.func.isRequired,
-  getUsersList: PropTypes.func.isRequired,
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      clusterID: PropTypes.string.isRequired,
-    }).isRequired,
-  }).isRequired,
-};
-
-AdminProjectsPage.defaultProps = {
-  projects: [],
-  isRetrieved: false,
-  isRetrieving: false,
-  users: [],
-  isFetched: false,
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
-  const { isRetrieving, projects, isRetrieved } = state.adminProjectsReducer;
-  const { users, isFetched } = state.usersListReducer;
   return {
-    isRetrieving,
-    projects,
-    isRetrieved,
-    users,
-    isFetched,
+    adminProjects: state.adminProjectsReducer,
+    usersList: state.usersListReducer,
   };
 };
 
