f80e38acd3e5f5203b5e0ccdda182dfbbdfe80c4
neelxie <kidricederek@gmail.com>
2022-01-14 13:59:31 +0300

    set up testing

---
+++ src/components/App/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import App from './index';
+
+describe('App Component', () => {
+  it('should match the snapshot', () => {
+    const component = shallow(<App />);
+
+    expect(toJson(component)).toMatchSnapshot();
+  });
+});
