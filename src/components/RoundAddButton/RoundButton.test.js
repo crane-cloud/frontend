d82452a7f260eb549982c1a547226f93a859b1a6
neelxie <kidricederek@gmail.com>
2022-02-09 00:07:43 +0300

    Fix issue with warning

--- src/components/RoundAddButton/RoundButton.test.js
+++ src/components/RoundAddButton/RoundButton.test.js
@@ -1,21 +1,40 @@
-import React from 'react';
 
-import { shallow } from 'enzyme';
+import React from "react";
 
-import RoundAddButton from './index';
+import { shallow } from "enzyme";
+import ShallowRenderer from 'react-test-renderer/shallow';
+import { ReactComponent as ButtonPlus } from "../../assets/images/buttonplus.svg";
+import RoundAddButton from "./index";
 
-describe('Button Component', () => {
-  it('Renders a button component', () => {
 
+// in your test:
+// const renderer = new ShallowRenderer();
+// renderer.render(<MyComponent />);
+// renderer.render(<RoundAddButton className="RoundAddButton" />)
+// const result = renderer.getRenderOutput();
+
+// expect(result.type).toBe('button');
+// expect(result.props.children).toEqual([
+//   <ButtonPlus />
+// ]);
+
+
+describe("Button Component", () => {
+  it("Renders a button component", () => {
     const buttonEvent = jest.fn();
-    const ButtonComponent = shallow(<RoundAddButton className="RoundAddButton" onClick={buttonEvent} />);
+    const ButtonComponent = shallow(
+      <RoundAddButton className="RoundAddButton" onClick={buttonEvent} />
+    );
 
-    expect(ButtonComponent.find('.RoundAddButton').hasClass('RoundAddButton')).toBe(true);
+    expect(
+      ButtonComponent.find(".RoundAddButton").hasClass("RoundAddButton")
+    ).toBe(true);
 
-    ButtonComponent.find('.RoundAddButton').simulate('click');
+    ButtonComponent.find(".RoundAddButton").simulate("click");
     expect(buttonEvent).toHaveBeenCalled();
 
     expect(ButtonComponent).toMatchSnapshot();
+    expect(ButtonComponent.contains(<ButtonPlus />)).toEqual(true);
 
   });
 });
--- src/components/ToggleButton/ToggleButton.test.js
+++ src/components/ToggleButton/ToggleButton.test.js
@@ -1,18 +1,13 @@
 import React from 'react';
-
 import { shallow } from 'enzyme';
-
 import ToggleButton from './index';
 
 describe('Toggle Button Component', () => {
   it('Renders a Toggle button component', () => {
 
-    // const buttonEvent = jest.fn();
-    const ButtonComponent = shallow(<ToggleButton />);
-    
-    // expect(ButtonComponent.find('.ToggleButtonContainer').hasClass('ToggleButtonContainer')).toBe(true);
-    // expect(component.find('#item-id').prop('style')).toHaveProperty('backgroundColor' 'black');
-    expect(ButtonComponent).toMatchSnapshot();
+    const buttonEvent = jest.fn();
+    const ButtonComponent = shallow(<ToggleButton  onClick={buttonEvent}/>);
 
+    expect(ButtonComponent).toMatchSnapshot();
   });
 });
