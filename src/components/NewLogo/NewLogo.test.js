01d6fed658edb3dcd11b601c3c2ba68ba2d5a757
fel-w <wangottafelix@gmail.com>
2022-03-18 10:31:20 +0300

    Change new logo test suite

--- src/components/NewLogo/NewLogo.test.js
+++ src/components/NewLogo/NewLogo.test.js
@@ -5,13 +5,13 @@ import { shallow } from 'enzyme';
 import NewLogo from './index';
 
 describe('Test new logo', () => {
-    it('checks whether the component rendered correctly', () => {
-        const NewLogoComponent = shallow(<NewLogo />);
-        
-        expect(NewLogoComponent).toMatchSnapshot();
-
+    const NewLogoComponent = shallow(<NewLogo />);
+    it('checks whether required elements rendered', () => {
         const BrandLogoType = NewLogoComponent.find("div > div");
         expect(BrandLogoType.hasClass("BrandLogoType")).toEqual(true);
         expect(BrandLogoType.text()).toBe("Crane Cloud");
-    })
+    });
+    it('checks rendered component matches snapshot', () => {
+        expect(NewLogoComponent).toMatchSnapshot();
+    });
 })
