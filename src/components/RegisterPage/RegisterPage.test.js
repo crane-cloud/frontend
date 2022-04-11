b8ab8258d353b17ce118bcebb3d0edecd02ba2e5
khalifan-kfan <khalifanmuwonge@gmail.com>
2022-04-11 12:46:27 +0300

    more tests 2

---
+++ src/components/RegisterPage/RegisterPage.test.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from "../Header";
+
+import RegisterPage from './index';
+
+describe('<RegisterPage /> Component', () => {
+    const handleOnChange = jest.fn(); 
+    const toGithubauth = jest.fn();
+    const validateEmail = jest.fn();
+    const handleSubmit = jest.fn();
+
+    
+  const RegisterPageComponent = shallow(<RegisterPage />);
+
+  it(' component should match the snapshot', () => {
+    expect(RegisterPageComponent).toMatchSnapshot();
+    expect(RegisterPageComponent.contains(<Header />)).toEqual(true);
+    expect(handleOnChange).toBeDefined();
+    expect(toGithubauth).toBeDefined();
+    expect(validateEmail).toBeDefined();
+    expect(handleSubmit).toBeDefined();
+  });
+
+  it('should have an email field', () => {
+    expect(RegisterPageComponent.find('InputText[name="email"]').length).toEqual(1);
+  });
+
+  it('should have proper props for email field', () => {
+    expect(RegisterPageComponent.find('InputText[name="email"]').props()).toEqual({
+      onChange: expect.any(Function),
+      placeholder: "Email Address",
+      required: true,
+      value: "",
+      name:"email"
+    }); 
+  });
+
+  it('should have a password field', () => { 
+    expect(RegisterPageComponent.find('InputPassword[name="password"]').length).toEqual(1);
+    });
+  it('should have proper props for password field', () => { 
+    expect(RegisterPageComponent.find('InputPassword[name="password"]').props()).toEqual({
+        onChange: expect.any(Function),
+        placeholder: 'Password',
+        required: true,
+        value: "",
+        name:"password"
+      });
+   });
+
+  
+  it('should have proper props for submit button', () => { 
+    expect(RegisterPageComponent.find('.SignupBtn').props()).toEqual({
+        onClick: expect.any(Function),
+        className: "SignupBtn AuthBtn",     
+        label:"Register"
+      });
+   });
+   
+});
