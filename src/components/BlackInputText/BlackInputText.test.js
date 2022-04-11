56a98640d2139136e570d23c34e42c8130319da9
khalifan-kfan <khalifanmuwonge@gmail.com>
2022-04-11 10:06:20 +0300

    more tests

---
+++ src/components/BlackInputText/BlackInputText.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { shallow } from "enzyme";
+
+import BlackInputText from "./index";
+
+const BlackInputTextProps = {
+    value: "text",
+    required: false,
+    type: "text",
+  };
+
+describe('Test the component', () => {
+    const OnChangeEvent = jest.fn();
+        const BlackInputTextComponent = shallow(<BlackInputText   name="" placeholder="*" value=""  onChange={OnChangeEvent} />);
+
+    it("should match the snapshot for TextArea after adding props", () => {
+        BlackInputTextComponent.setProps(BlackInputTextProps);
+        expect(BlackInputTextProps).toBeDefined();
+      });
+    it('checks if BlackInputText rendered as expected', () => {
+
+        BlackInputTextComponent.find("input").simulate("change", { target: { value: 'typing' } });
+        expect(OnChangeEvent).toHaveBeenCalled();
+        expect(BlackInputTextComponent).toMatchSnapshot();
+    })
+   
+}) 
