4c4b1d4badf636c58dfbab156caf088f8d216fbe
khalifan-kfan <khalifanmuwonge@gmail.com>
2022-04-11 10:06:20 +0300

    more tests

---
+++ src/pages/ProjectSettingsPage/ProjectSettingsPage.test.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable no-undef */
+import React from "react";
+import { shallow } from "enzyme";
+import ProjectSettingsPage, { mapStateToProps } from "./";
+
+const ProjectSettingsPageProps = {
+  match: { params: { projectID: "1" } },
+  location: {pathname:"path"},
+  deleteProject: jest.fn(),
+  updateProject: jest.fn(),
+  clearDeleteProjectState: jest.fn(),
+  clearUpdateProjectState: jest.fn(),
+};
+
+
+describe("Testing the Project Settings Page component", () => {
+  const WrapperProjectSettingsPage = ProjectSettingsPage.WrappedComponent;
+  const ProjectSettingsPageComponent = shallow(<WrapperProjectSettingsPage {...ProjectSettingsPageProps}/>);
+
+
+  
+  it("should match the snapshot for ProjectSettingsPage after adding props", () => {
+    ProjectSettingsPageComponent.setProps(ProjectSettingsPage);
+    expect(ProjectSettingsPageComponent).toBeDefined();
+  });
+  it("matchs the ProjectSettingsPage component snapshot", () => {
+    expect(ProjectSettingsPageComponent).toMatchSnapshot();
+  });
+});
+
+describe("Testing the exported mapstate to props and dispatch for ProjectSettingsPage", () => {
+  it("matches the ProjectSettingsPage mapstostate", () => {
+    expect(
+      mapStateToProps({
+
+        deleteProjectReducer: { isDeleting:false, isDeleted:false, isFailed:false,  message:""},
+        updateProjectReducer: { isUpdated:false, isUpdating:false, errorMessage: null }
+      })
+    ).toEqual({
+        isUpdated: false,
+        isUpdating: false,
+        message: "",
+        isDeleting: false,
+        isFailed: false,
+        isDeleted: false,
+        errorMessage: null,
+    });
+  });
+});
