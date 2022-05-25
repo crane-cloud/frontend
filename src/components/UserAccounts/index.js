5df117431bbf1600d40a8e52826f8e3c58042a3a
neelxie <kidricederek@gmail.com>
2022-05-25 20:05:49 +0300

    Add modals for context menu

--- src/components/UserAccounts/index.js
+++ src/components/UserAccounts/index.js
@@ -7,11 +7,66 @@ import Header from "../Header";
 import Spinner from "../Spinner";
 import InformationBar from "../InformationBar";
 import SideNav from "../SideNav";
+import Modal from "../Modal";
+import { ReactComponent as MoreIcon } from "../../assets/images/more-verticle.svg";
+import PrimaryButton from "../PrimaryButton";
 
 class UserAccounts extends Component {
+  constructor() {
+    super();
+    this.state = {
+      actionsMenu: false,
+      betaUserModal: false,
+    };
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    // this.showModal = this.showModal.bind(this);
+    // this.closeModal = this.closeModal.bind(this);
+    this.showMenu = this.showMenu.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleBetaUserSubmit = this.handleBetaUserSubmit.bind(this);
+    this.closeBetaUserModal = this.closeBetaUserModal.bind(this);
+    this.showBetaUserModal = this.showBetaUserModal.bind(this);
+  }
   componentDidMount() {
     const { getUsersList } = this.props;
     getUsersList();
+  }
+  handleClick = (e) => {
+    if (this.state.actionsMenu) {
+      this.closeModal();
+      return;
+    }
+    this.setState({ actionsMenu: true });
+    e.stopPropagation();
+    document.addEventListener("click", this.hideModal);
+  };
+
+  hideModal = () => {
+    this.setState({ actionsMenu: false });
+    document.removeEventListener("click", this.hideModal);
+  };
+
+  showMenu(id) {
+    this.setState({
+      selectedUser: id,
+    });
+  }
+
+  showBetaUserModal() {
+    this.setState({
+      betaUserModal: true,
+    });
+  }
+
+  closeBetaUserModal() {
+    this.setState({
+      betaUserModal: false,
+    });
+  }
+
+  handleBetaUserSubmit(){
+    console.log("Got it");
   }
 
   render() {
@@ -20,6 +75,7 @@ class UserAccounts extends Component {
     const {
       match: { params },
     } = this.props;
+    const { actionsMenu, selectedUser, betaUserModal } = this.state;
 
     return (
       <div className="MainPage">
@@ -48,6 +104,7 @@ class UserAccounts extends Component {
                       <th>Name</th>
                       <th>Beta User</th>
                       <th>Email</th>
+                      <th>Actions</th>
                     </tr>
                   </thead>
                   {isFetching ? (
@@ -67,8 +124,36 @@ class UserAccounts extends Component {
                         users?.map((user) => (
                           <tr key={users.indexOf(user)}>
                             <td>{user?.name}</td>
-                            <td>{user.is_beta_user?"True":"False"}</td>
+                            <td>{user.is_beta_user ? "True" : "False"}</td>
                             <td>{user?.email}</td>
+                            <td
+                              onClick={(e) => {
+                                this.showMenu(user.id);
+                                this.handleClick(e);
+                              }}
+                            >
+                              <MoreIcon />
+                              {actionsMenu && user.id === selectedUser && (
+                                <div className="BelowHeader bg-light">
+                                  <div className="context-menu">
+                                    <div
+                                      className="DropDownLink"
+                                      role="presentation"
+                                      onClick={this.showBetaUserModal}
+                                    >
+                                      Add Beta User
+                                    </div>
+                                    {/* <div
+                                      className="DropDownLink"
+                                      role="presentation"
+                                      onClick={this.showDeletionModal}
+                                    >
+                                      Delete
+                                    </div> */}
+                                  </div>
+                                </div>
+                              )}
+                            </td>
                           </tr>
                         ))}
                     </tbody>
@@ -89,6 +174,40 @@ class UserAccounts extends Component {
                 )}
               </div>
             </div>
+            <Modal
+              showModal={betaUserModal}
+              onClickAway={this.closeBetaUserModal}
+            >
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLongTitle">
+                    Add user as Beta user?
+                  </h5>
+                </div>
+                <div class="modal-footer">
+                  <PrimaryButton
+                    type="button"
+                    class="btn btn-info"
+                    label="Cancel"
+                    onClick={this.closeBetaUserModal}
+                  >
+                    Cancel
+                  </PrimaryButton>
+                  <PrimaryButton
+                    type="button"
+                    className="btn btn-danger"
+                    label={1 > 2 ? <Spinner /> : "Confirm"}
+                    onClick={this.handleBetaUserSubmit}
+                  />
+                </div>
+                {/* {addingCountryErr && (
+                  <Feedback
+                    message={addingCountryErr !== "" ? addingCountryMsg : null}
+                    type={countryIsDeleted ? "success" : "error"}
+                  />
+                )} */}
+              </div>
+            </Modal>
           </div>
         </div>
       </div>
