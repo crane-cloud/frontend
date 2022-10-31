f9e80dd060e6cd9b752f1ffd13b36d107dc85917
rhodinemma <rhodinemma10@gmail.com>
2022-10-31 10:42:03 +0300

    fix failing test

--- src/helpers/tests/dateConstants.test.js
+++ src/helpers/tests/dateConstants.test.js
@@ -1,24 +1,22 @@
-import {DisplayDateTime,
-    getBackDate,
-} from "../dateConstants"
-var assert = require('assert')
-var today = new Date()
-var thisYear = today.getFullYear()
-var thisMonth = today.getMonth()
+import { DisplayDateTime, getBackDate } from "../dateConstants";
+var assert = require("assert");
+var today = new Date();
+var thisYear = today.getFullYear();
+var thisMonth = today.getMonth();
 //since month returns 0 to 11
-thisMonth +=1;
+thisMonth += 1;
 
-describe('data constants helper', () => {
-    it('test fomart changer with get back date funtion', () => {
-        //function returns date amonth ago for this test
-        //get the year
-           thisYear = ((thisMonth - 1) <= 0) ? thisYear - 1 : thisYear;
-        //get the month
-           thisMonth = ((thisMonth - 1) <= 0)? 12+(thisMonth - 1) : thisMonth - 1;
-        //assert equality
-            assert.equal(getBackDate(1),thisYear+"-"+thisMonth+"-1");
-    }); 
-    it('test Display date ', () => {
-        assert.equal(DisplayDateTime(new Date(2022,4,18)),'5-18-2022  12:00AM');
-});   
+describe("data constants helper", () => {
+  it("test fomart changer with get back date funtion", () => {
+    //function returns date amonth ago for this test
+    //get the year
+    thisYear = thisMonth - 1 <= 0 ? thisYear - 1 : thisYear;
+    //get the month
+    thisMonth = thisMonth - 1 <= 0 ? 12 + (thisMonth - 1) : thisMonth;
+    //assert equality
+    assert.equal(getBackDate(1), thisYear + "-" + thisMonth + "-1");
+  });
+  it("test Display date ", () => {
+    assert.equal(DisplayDateTime(new Date(2022, 4, 18)), "5-18-2022  12:00AM");
+  });
 });
