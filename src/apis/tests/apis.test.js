a90e86ec774ae4b5f4d9c1ebb5bdce69cbeda4d0
khalifan-kfan <khalifanmuwonge@gmail.com>
2022-12-01 09:23:07 +0300

    fix api test

--- src/apis/tests/apis.test.js
+++ src/apis/tests/apis.test.js
@@ -1,14 +1,14 @@
-import { handlePostRequest, handlePatchRequest, handleGetRequest } from "../apis";
+import { handlePostRequestWithDataObject, handlePatchRequest, handleGetRequest } from "../apis";
 
 describe('tests for post funtion for metrics', () => {
     it('test for failure of post request', async () => {
-        return handlePostRequest({ timestamps: true },
+        return handlePostRequestWithDataObject({ timestamps: true },
             "/projects/0/apps/0/logs").then(data => {
                 expect(data).toBeFalsy;
         }).catch(error =>{
             expect(error).toBeTruthy;
         })
-    }, 7000);   
+    }, 8000);   
     it('test for failure of get request', async () => {
         return handleGetRequest(
             "/projects/0/users").then(data => {
