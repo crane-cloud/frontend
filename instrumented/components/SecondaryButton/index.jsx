373f63f2969bd154e02c5573838f577d4c2ffa25
neelxie <kidricederek@gmail.com>
2023-04-03 17:27:21 +0300

    Revert "Working on this"
    This reverts commit 63d299a5f8e3b20c84b361f4dce4e59261168d3f.

--- instrumented/components/SecondaryButton/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./SecondaryButton.css";
-
-const SecondaryButton = ({className, label, onClick}) => {
-  return (<button className={`Secondary-Btn ${className}`} onClick={onClick}>{label}</button>)  
-};
-
-export default SecondaryButton;
