c00be0c6d8aa89aa863cd0e12f2c0a16094d9de6
khaifan-kfan <khalifanmuwonge@gmail.com>
2023-06-12 01:31:45 -0700

    add and improve tests

--- src/components/PrimaryButton/PrimaryButton.test.js
+++ src/components/PrimaryButton/PrimaryButton.test.js
@@ -1,24 +1,42 @@
 import React from "react";
-
 import { shallow } from "enzyme";
-
 import PrimaryButton from "./index";
 
-describe("Button Component", () => {
-  it("Renders a button component", () => {
+describe("PrimaryButton Component", () => {
+  it("renders a button component with text and triggers click event", () => {
     const buttonEvent = jest.fn();
+    const buttonLabel = "Login";
     const ButtonComponent = shallow(
-      <PrimaryButton className="Primary-Btn" onClick={buttonEvent}>
-        Login
+      <PrimaryButton onClick={buttonEvent}>{buttonLabel}</PrimaryButton>
+    );
+
+    // Verify that the button text is rendered correctly
+    expect(ButtonComponent.text()).toBe(buttonLabel);
+
+    // Trigger a click event on the button
+    ButtonComponent.simulate("click");
+
+    // Verify that the click event was triggered
+    expect(buttonEvent).toHaveBeenCalled();
+  });
+
+  it("applies additional class names and props", () => {
+    const className = "Primary-Btn";
+    const buttonLabel = "Submit";
+    const additionalProps = {
+      disabled: true,
+    };
+
+    const ButtonComponent = shallow(
+      <PrimaryButton className={className} {...additionalProps}>
+        {buttonLabel}
       </PrimaryButton>
     );
 
-    expect(ButtonComponent.find(".Primary-Btn").text()).toBe("Login");
-    // expect(
-    //   ButtonComponent.find(".Primary-Btn").hasClass("Primary-Btn uppercase")
-    // ).toBe(true);
+    // Verify that the additional class names are applied
+    expect(ButtonComponent.hasClass(className)).toBe(true);
 
-    ButtonComponent.find(".Primary-Btn").simulate("click");
-    expect(buttonEvent).toHaveBeenCalled();
+    // Verify that additional props are applied
+    expect(ButtonComponent.prop("disabled")).toBe(true);
   });
 });
