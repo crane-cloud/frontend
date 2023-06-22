f58072d47a7e83775060dfaed1b5b83864e8002f
khalifan-kfan <khalifanmuwonge@gmail.com>
2023-06-22 20:31:19 +0300

    fix test

--- src/pages/DBSettingsPage/DBSettingsPage.test.js
+++ src/pages/DBSettingsPage/DBSettingsPage.test.js
@@ -2,45 +2,54 @@
 import React from "react";
 import { shallow } from "enzyme";
 import DBSettingsPage from "./";
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 
 const SettingsPageProps = {
   match: { params: { projectID: "1" } },
   location: { pathname: "path" },
   data: { beta: [] },
-
 };
 
 describe("Testing the Project Settings Page component", () => {
-  const WrapperDBSettingsPage = DBSettingsPage;
-  const DBSettingsPageComponent = shallow(
-    <WrapperDBSettingsPage {...SettingsPageProps} />
-  );
+  const mockStore = configureStore();
+  const initialState = {
+    userProjectsReducer: {
+      projects: [],
+    },
+  }; 
+  let store; 
+
+  beforeEach(() => {
+    store = mockStore(initialState);
+  });
 
   it("should match the snapshot for DBSettingsPage after adding props", () => {
-    DBSettingsPageComponent.setProps(DBSettingsPage);
-    expect(DBSettingsPageComponent).toBeDefined();
-  });
-  it("matchs the DBSettingsPage component snapshot", () => {
+    const DBSettingsPageComponent = shallow(
+      <Provider store={store}>
+        <DBSettingsPage {...SettingsPageProps} />
+      </Provider>
+    );
     expect(DBSettingsPageComponent).toMatchSnapshot();
   });
 });
 
-// describe("Testing the exported mapstate to props and dispatch for DBSettingsPage", () => {
-//   it("matches the DBSettingsPage mapstostate", () => {
-//     expect(
-//       mapStateToProps({
-//         userProjectsReducer: {
-//           projects: [],
-//         },
+describe("Testing the exported mapStateToProps for DBSettingsPage", () => {
+  it("matches the DBSettingsPage mapStateToProps", () => {
+    const mapStateToProps = (state) => ({
+      projects: state.userProjectsReducer.projects,
+    });
 
+    const initialState = {
+      userProjectsReducer: {
+        projects: [],
+      },
+    };
+    const store = configureStore()(initialState);
+    const props = mapStateToProps(store.getState());
 
-//       })
-//     ).toEqual({
-//       projects: [],
-//       errorMessage: "",
-//       password: "",
-//       isRetrievingPassword: false,
-//       passwordFetched: false,
-//     });
-//   });
-// });
+    expect(props).toEqual({
+      projects: [],
+    });
+  });
+});
