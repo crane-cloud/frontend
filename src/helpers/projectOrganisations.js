20bac43304b726c8ed2433e2581ead48aa29375f
Sekidde Derrick <32802973+neelxie@users.noreply.github.com>
2023-06-29 14:44:17 +0300

    Update projectOrganisations types

--- src/helpers/projectOrganisations.js
+++ src/helpers/projectOrganisations.js
@@ -2,9 +2,8 @@ export const namedOrganisations = () =>{
     const organisation = [
         {name: "Start-up",id: 1, value:"Startup"},
         {name: "Company", id: 2, value:"Company"},
-        {name: "University", id: 3, value:"University"},
-        {name: "University Project", id: 4, value: "University-Project"},
-        {name: "Final Year Project", id: 5, value: "Final-Year-Project"},
+        {name: "University Project", id: 3, value: "University-Project"},
+        {name: "Final Year Project", id: 4, value: "Final-Year-Project"},
         {name: "Individual", id: 6, value: "Individual"},
     ];
     return organisation;
