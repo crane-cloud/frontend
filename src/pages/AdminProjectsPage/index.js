4e0e6ed9dafe420d987cfcc0fb95763a54b33891
mosesmulumba <mulumbamoses94@gmail.com>
2023-07-12 22:17:45 +0300

    error in search fix

--- src/pages/AdminProjectsPage/index.js
+++ src/pages/AdminProjectsPage/index.js
@@ -184,7 +184,7 @@ const AdminProjectsPage = () => {
                     <th>Actions</th>
                   </tr>
                 </thead>
-                {isRetrieving ? (
+                {projects.isRetrieving ? (
                   <tbody>
                     <tr className="TableLoading">
                     <td className="TableTdSpinner">
