a64716234e9aee1964cbfc8bdb144b7709d48132
Nagujja Mariam <mirembemariam3@gmail.com>
2023-07-27 22:16:34 +0300

    modified project filter dropdown and fixing small screen menu dropdown

---
+++ src/helpers/projectCategories.js
@@ -0,0 +1,12 @@
+export const projectCategories = () => {
+    const categories = [
+      { id: 1, name: "All", value: "All" },
+      { id: 2, name: "My Projects", value: "My Projects" },
+      { id: 3, name: "Shared Projects", value: "Shared Projects" },
+    ];
+    return categories;
+  };
+
+
+
+  
