2a097f6d46b9dc3fd9343d0db2dfcfd6deca7ebf
rhodinemma <rhodinemma10@gmail.com>
2023-08-31 10:44:15 +0300

    fix: fix failing tests

--- src/helpers/tests/projectPieCat.test.js
+++ src/helpers/tests/projectPieCat.test.js
@@ -1,38 +1,38 @@
-import assert from 'assert'; 
+import assert from "assert";
 import {
   projectPieCategories,
   projectGraphCategories,
-  projectLists
-} from '../projectPieCat';
+  projectLists,
+} from "../projectPieCat";
 
-describe('Test Project Categories Functions', () => {
-  it('should correctly retrieve projectPieCategories', () => {
+describe("Test Project Categories Functions", () => {
+  it("should correctly retrieve projectPieCategories", () => {
     const expected = [
-      { id: 1, name: "Project Categories Summary", value: "Projects" },
-      { id: 2, name: "Orgaisation Categories Summary", value: "Organizations" },
+      { id: 1, name: "Project Categories", value: "Projects" },
+      { id: 2, name: "Orgaisation Categories", value: "Organizations" },
     ];
-    
+
     const result = projectPieCategories();
     assert.deepStrictEqual(result, expected);
   });
 
-  it('should correctly retrieve projectGraphCategories', () => {
+  it("should correctly retrieve projectGraphCategories", () => {
     const expected = [
-      { id : 1, name : "Projects" , value: "Projects"},
-      { id : 2, name : "Types" , value : "Types"},
+      { id: 1, name: "All Projects", value: "Projects" },
+      { id: 2, name: "Project Types", value: "Types" },
     ];
-    
+
     const result = projectGraphCategories();
     assert.deepStrictEqual(result, expected);
   });
 
-  it('should correctly retrieve projectLists', () => {
+  it("should correctly retrieve projectLists", () => {
     const expected = [
-      { id : 1, name : "All Projects" , value: "All Projects"},
-      { id : 2, name : "Active Projects" , value : "Active Projects"},
-      { id : 3, name : "Disabled Projects" , value : "Disabled Projects"},
+      { id: 1, name: "All Projects", value: "All Projects" },
+      { id: 2, name: "Active Projects", value: "Active Projects" },
+      { id: 3, name: "Disabled Projects", value: "Disabled Projects" },
     ];
-    
+
     const result = projectLists();
     assert.deepStrictEqual(result, expected);
   });
