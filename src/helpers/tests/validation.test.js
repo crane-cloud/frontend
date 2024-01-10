c5830176d35716aa510097c940ade86804e9a673
rhodinemma <rhodinemma10@gmail.com>
2024-01-10 23:34:07 +0300

    chore: include missing functionality for adding a custom domain and reverting process

--- src/helpers/tests/validation.test.js
+++ src/helpers/tests/validation.test.js
@@ -1,64 +1,90 @@
-import handleProjectValidation,{validateDomain,
-    validateDomainName,
-    validateName,
-    validateProjectDescription,
-    validateOrganizationName,
-    validateProjectName,
-    validateProjectType
-} from "../validation.js"
-var assert = require('assert')
+import handleProjectValidation, {
+  validateDomain,
+  validateName,
+  validateProjectDescription,
+  validateOrganizationName,
+  validateProjectName,
+  validateProjectType,
+} from "../validation.js";
+var assert = require("assert");
 
-describe('checks valiidation', () => {
-    it(' domain name errors ', () => {
-            assert.equal(validateDomain(''), "Please provide domain name");
-            assert.equal(validateDomain('2john@'), "Domain name should start with a letter");
-            assert.equal(validateDomain('johnr.com'),undefined);
-    });    
-    it('validate domain name', () => {
-        assert.equal(validateDomainName(''), false);
-        assert.equal(validateDomainName('2john@'), false);
-        assert.equal(validateDomainName('johnr.com'),true);
-   });
-   it('validate names', () => {
-    assert.equal(validateName(''), false);
-    assert.equal(validateName('2john@'), false);
-    assert.equal(validateName('john'),true);
-    assert.equal(validateName('joh@n'),"false_convention");
+describe("checks valiidation", () => {
+  it(" domain name errors ", () => {
+    assert.equal(validateDomain(""), "Please provide a domain name");
+    assert.equal(
+      validateDomain("2john@"),
+      "Use accepted formats, for example, google.com, yourdomain.ug"
+    );
+    assert.equal(validateDomain("johnr.com"), false);
   });
-  it('validate descriptions', () => {
-    assert.equal(validateProjectDescription(''), "Add Project Description");
-    assert.equal(validateProjectDescription('High low'),undefined);
+  it("validate names", () => {
+    assert.equal(validateName(""), false);
+    assert.equal(validateName("2john@"), false);
+    assert.equal(validateName("john"), true);
+    assert.equal(validateName("joh@n"), "false_convention");
   });
-  it('validate organisation name', () => {
+  it("validate descriptions", () => {
+    assert.equal(validateProjectDescription(""), "Add Project Description");
+    assert.equal(validateProjectDescription("High low"), undefined);
+  });
+  it("validate organisation name", () => {
     // assert.equal(validateOrganizationName(''),
     //  "Project organisation must start with a letter and may only contain letters and a hypen -");
-    assert.equal(validateOrganizationName('Highlow'),undefined);
-    // assert.equal(validateOrganizationName('fh d'), 
+    assert.equal(validateOrganizationName("Highlow"), undefined);
+    // assert.equal(validateOrganizationName('fh d'),
     // "Project organisation must start with a letter and may only contain letters and a hypen -");
   });
-  it('validate descriptions', () => {
-    assert.equal(validateProjectDescription(''), "Add Project Description");
-    assert.equal(validateProjectDescription('High low'),undefined);
+  it("validate descriptions", () => {
+    assert.equal(validateProjectDescription(""), "Add Project Description");
+    assert.equal(validateProjectDescription("High low"), undefined);
   });
-  it('validating project name function', () => {
-    assert.equal(validateProjectName(''), "Project Name cannot be empty");
-    assert.equal(validateProjectName('2na'), "Project name should start with a letter");
+  it("validating project name function", () => {
+    assert.equal(validateProjectName(""), "Project Name cannot be empty");
+    assert.equal(
+      validateProjectName("2na"),
+      "Project name should start with a letter"
+    );
     // assert.equal(validateProjectName('High low'),"Project name may only contain letters and a hypen -");
-    assert.equal(validateProjectName('Highndsjjdjnbhjvbhfdvbadjvbfdhvbbfvbvhfdbvhfdhdvjhsdjjdjds'),
-    "Project name may not exceed 30 characters");
+    assert.equal(
+      validateProjectName(
+        "Highndsjjdjnbhjvbhfdvbadjvbfdhvbbfvbvhfdbvhfdhdvjhsdjjdjds"
+      ),
+      "Project name may not exceed 30 characters"
+    );
   });
-  it('validating project types errors', () => {
-    assert.equal(validateProjectType('2najd'), "Project Type should start with a letter");
-    assert.equal(validateProjectType(''), "Project Type cannot be empty");
-    assert.equal(validateProjectType('Hig'),"Project Type must be at least 4 characters");
+  it("validating project types errors", () => {
+    assert.equal(
+      validateProjectType("2najd"),
+      "Project Type should start with a letter"
+    );
+    assert.equal(validateProjectType(""), "Project Type cannot be empty");
+    assert.equal(
+      validateProjectType("Hig"),
+      "Project Type must be at least 4 characters"
+    );
     // assert.equal(validateProjectType('Hig k0'),"Project Type may only contain letters and a hypen -");
   });
-  it('validate project handler', () => {
-    assert.equal(handleProjectValidation('projectName','projectDescription',
-    'organisationType','organisation','clusterID'
-    ), undefined);
-    assert.equal(handleProjectValidation('projectName','projectDescription',
-    'organisationType','organisation',''),"All fields are required");
+  it("validate project handler", () => {
+    assert.equal(
+      handleProjectValidation(
+        "projectName",
+        "projectDescription",
+        "organisationType",
+        "organisation",
+        "clusterID"
+      ),
+      undefined
+    );
+    assert.equal(
+      handleProjectValidation(
+        "projectName",
+        "projectDescription",
+        "organisationType",
+        "organisation",
+        ""
+      ),
+      "All fields are required"
+    );
   });
   // assert.equal(handleProjectValidation('','',
   // '','',''),"All fields are required");
@@ -71,6 +97,3 @@ describe('checks valiidation', () => {
   // assert.equal(handleProjectValidation('letters','letters',
   // 'letters','letters','hfdld zx'),undefined);
 });
-
-
-   
