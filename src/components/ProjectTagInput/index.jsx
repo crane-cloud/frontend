b0781e9cf9008c41a85438385283f9569ec8e799
khalifan-kfan <khalifanmuwonge@gmail.com>
2024-07-25 17:00:57 +0300

    update frontend and add tanstack

--- src/components/ProjectTagInput/index.jsx
+++ src/components/ProjectTagInput/index.jsx
@@ -1,33 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./TagInput.module.css";
+import { useTags } from "../../hooks/useTags";
+import Spinner from "../Spinner";
 
-const TagInput = ({ suggestions }) => {
+const TagInput = ({ userTags }) => {
   const [tags, setTags] = useState([]);
   const [input, setInput] = useState("");
   const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+  const {
+    data: tagData,
+    isPending,
+    isRefetching,
+    error,
+    isSuccess,
+    refetch,
+  } = useTags(input);
 
-  const handleInputChange = (e) => {
-    const value = e.target.value;
-    setInput(value);
-
-    if (value) {
-      const filtered = suggestions.filter((suggestion) =>
-        suggestion.toLowerCase().includes(value.toLowerCase())
-      );
-      setFilteredSuggestions(filtered);
+  useEffect(() => {
+    if (input && tagData?.data?.data?.length > 0) {
+      setFilteredSuggestions(tagData?.data?.data);
     } else {
       setFilteredSuggestions([]);
     }
+  }, [input, tagData]);
+
+  useEffect(() => {
+    setTags([...tags,...userTags]);
+  }, [userTags]);
+
+  useEffect(() => {
+    refetch(input);
+  }, [input]);
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
   };
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter" && input) {
-      if (!tags.includes(input)) {
-        setTags([...tags, input]);
-      }
-      setInput("");
-      setFilteredSuggestions([]);
+      addTag(input);
     }
+  };
+
+  const addTag = (tag) => {
+    if (!tags.includes(tag)) {
+      setTags([...tags, tag]);
+    }
+    setInput("");
+    setFilteredSuggestions([]);
   };
 
   const handleTagRemove = (index) => {
@@ -35,17 +55,13 @@ const TagInput = ({ suggestions }) => {
   };
 
   const handleSuggestionClick = (suggestion) => {
-    if (!tags.includes(suggestion)) {
-      setTags([...tags, suggestion]);
-    }
-    setInput("");
-    setFilteredSuggestions([]);
+    addTag(suggestion.name);
   };
 
   return (
     <div className={styles.tagInputContainer}>
       <div className={styles.tagList}>
-        {tags.map((tag, index) => (
+        {tags?.map((tag, index) => (
           <div key={index} className={styles.tag}>
             {tag}
             <button onClick={() => handleTagRemove(index)}>x</button>
@@ -58,19 +74,35 @@ const TagInput = ({ suggestions }) => {
         onChange={handleInputChange}
         onKeyDown={handleKeyDown}
         className={styles.input}
+        placeholder="Type to add a tag..."
       />
-      {filteredSuggestions.length > 0 && (
-        <div className={styles.suggestions}>
-          {filteredSuggestions.map((suggestion, index) => (
-            <div
-              key={index}
-              className={styles.suggestion}
-              onClick={() => handleSuggestionClick(suggestion)}
-            >
-              {suggestion}
-            </div>
-          ))}
-        </div>
+
+      {error && <div className={styles.error}>Failed to fetch tags</div>}
+      {isSuccess && input && (
+        <>
+          <div className={styles.suggestions}>
+            {(isPending || isRefetching) && input && <Spinner />}
+            {filteredSuggestions?.slice(0, 6)?.map((suggestion, index) => (
+              <div
+                key={index}
+                className={styles.suggestion}
+                onClick={() => handleSuggestionClick(suggestion)}
+              >
+                {suggestion.name}
+              </div>
+            ))}
+            {filteredSuggestions.length === 0 &&
+              input &&
+              !error &&
+              !isPending &&
+              isSuccess &&
+              !isRefetching && (
+                <div className={styles.addNewTag} onClick={() => addTag(input)}>
+                  Add tag
+                </div>
+              )}
+          </div>
+        </>
       )}
     </div>
   );
