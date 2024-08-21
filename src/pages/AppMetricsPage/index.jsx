54bb70575057b6111275de0a9865098485fbc420
khalifan-kfan <khalifanmuwonge@gmail.com>
2024-08-21 17:03:04 +0300

    feat: implement AI app deployment flow

--- src/pages/AppMetricsPage/index.jsx
+++ src/pages/AppMetricsPage/index.jsx
@@ -1,15 +1,12 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
+import React, { useState, useEffect, useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useParams, Redirect,useLocation } from "react-router-dom";
 import Spinner from "../../components/Spinner";
-import { Redirect } from "react-router-dom";
 import MetricsCard from "../../components/MetricsCard";
 import { ReactComponent as RedeployICon } from "../../assets/images/redeploy.svg";
 import { ReactComponent as CPUIcon } from "../../assets/images/cpu.svg";
 import { ReactComponent as NetworkIcon } from "../../assets/images/wifi.svg";
 import { ReactComponent as MemoryIcon } from "../../assets/images/hard-drive.svg";
-
 import styles from "./AppMetricsPage.module.css";
 import LineChartComponent from "../../components/LineChart";
 import LogsFrame from "../../components/LogsFrame";
@@ -18,51 +15,59 @@ import {
   handlePostRequestWithOutDataObject,
   handleGetRequest,
 } from "../../apis/apis";
-import getAppMemory, { clearAppMemory } from "../../redux/actions/appMemory";
+import getAppMemory,{
+  clearAppMemory,
+} from "../../redux/actions/appMemory";
 import getAppCPU from "../../redux/actions/appCPU";
+import getAppNetwork from "../../redux/actions/appNetwork";
 import {
   formatAppMemoryMetrics,
   formatAppCPUMetrics,
   formatAppNetworkMetrics,
 } from "../../helpers/formatMetrics";
-import revertUrl, { clearUrlRevertState } from "../../redux/actions/revertUrl";
-import getAppNetwork from "../../redux/actions/appNetwork";
+import revertUrl, {
+  clearUrlRevertState,
+} from "../../redux/actions/revertUrl";
 import AppStatus from "../../components/AppStatus";
 import { ReactComponent as CopyText } from "../../assets/images/copytextblue.svg";
 import { ReactComponent as Checked } from "../../assets/images/checked.svg";
 import DashboardLayout from "../../components/Layouts/DashboardLayout";
 
-class AppMetricsPage extends React.Component {
-  constructor(props) {
-    super(props);
+const AppMetricsPage = () => {
+  const { projectID, appID } = useParams();
+  const dispatch = useDispatch();
 
-    this.state = {
-      urlChecked: false,
-      app: [],
-      logs: [],
-      fetchingLogs: true,
-      logsError: "",
-      redeploy: false,
-      deployError: "",
-      spin: false,
-    };
+  const location = useLocation(); 
 
-    this.getAppMemoryMetrics = this.getAppMemoryMetrics.bind(this);
-    this.getAppCPUMetrics = this.getAppCPUMetrics.bind(this);
-    this.getAppNetworkMetrics = this.getAppNetworkMetrics.bind(this);
-    this.regenerate = this.regenerate.bind(this);
-    this.copyUrl = this.copyUrl.bind(this);
-    this.renderRedirect = this.renderRedirect.bind(this);
-  }
+  const queryParams = new URLSearchParams(location.search); 
+  const jupyterNotebook = queryParams.get("jupyterNotebook") === "true"; 
 
-  getAppInfo() {
-    const { app } = this.state;
+  const [urlChecked, setUrlChecked] = useState(false);
+  const [app, setApp] = useState([]);
+  const [logs, setLogs] = useState([]);
+  const [fetchingLogs, setFetchingLogs] = useState(true);
+  const [logsError, setLogsError] = useState("");
+  const [redeploy, setRedeploy] = useState(false);
+  const [deployError, setDeployError] = useState("");
+  const [spin, setSpin] = useState(false);
 
-    if (!app) {
-      return null;
-    }
+  const {
+    appMemoryMetrics,
+    appCPUMetrics,
+    appNetworkMetrics,
+    isReverting,
+    isReverted,
+  } = useSelector((state) => ({
+    appMemoryMetrics: state.appMemoryReducer.appMemoryMetrics,
+    appCPUMetrics: state.appCpuReducer.appCPUMetrics,
+    appNetworkMetrics: state.appNetworkReducer.appNetworkMetrics,
+    isReverting: state.revertUrlReducer.isReverting,
+    isReverted: state.revertUrlReducer.isReverted,
+  }));
 
-    const info = {
+  const getAppInfo = () => {
+    if (!app) return null;
+    return {
       name: app.name,
       status: app.app_running_status,
       url: app.url,
@@ -72,109 +77,68 @@ class AppMetricsPage extends React.Component {
       port: app.port,
       disable: app.disabled,
     };
+  };
 
-    return info;
-  }
+  useEffect(() => {
+    setSpin(true);
 
-  componentDidMount() {
-    this.setState({ spin: true });
-    const {
-      getAppMemory,
-      getAppCPU,
-      getAppNetwork,
-      clearUrlRevertState,
-      match: { params },
-    } = this.props;
-    const { projectID, appID } = params;
     handlePostRequestWithDataObject(
       { timestamps: true },
       `/projects/${projectID}/apps/${appID}/logs`
     )
       .then((response) => {
-        this.setState({
-          logs: response.data.data.pods_logs,
-          fetchingLogs: false,
-        });
+        setLogs(response.data.data.pods_logs);
+        setFetchingLogs(false);
       })
-      .catch((error) => {
-        this.setState({
-          logsError: "Failed to fetch logs",
-          fetchingLogs: false,
-        });
+      .catch(() => {
+        setLogsError("Failed to fetch logs");
+        setFetchingLogs(false);
       });
 
     handleGetRequest(`/apps/${appID}`)
       .then((response) => {
-        this.setState({
-          app: response.data.data.apps,
-          spin: false,
-        });
+        setApp(response.data.data.apps);
+        setSpin(false);
       })
       .catch((error) => {
-        this.setState({
-          redeploy: true,
-          deployError: error?.response?.data?.message,
-        });
+        setRedeploy(true);
+        setDeployError(error?.response?.data?.message);
       });
 
-    clearAppMemory();
-    clearUrlRevertState();
-    getAppMemory(projectID, appID, {});
-    getAppCPU(projectID, appID, {});
-    getAppNetwork(projectID, appID, {});
-  }
+    dispatch(clearAppMemory());
+    dispatch(clearUrlRevertState());
+    dispatch(getAppMemory(projectID, appID, {}));
+    dispatch(getAppCPU(projectID, appID, {}));
+    dispatch(getAppNetwork(projectID, appID, {}));
+  }, [dispatch, projectID, appID]);
 
-  getAppMemoryMetrics() {
-    const { appID } = this.props.match.params;
-    const { appMemoryMetrics } = this.props;
-    const results = formatAppMemoryMetrics(appID, appMemoryMetrics);
-    return results;
-  }
-  // "Application does not exist on the cluster"
-  copyUrl() {
-    const app = this.getAppInfo();
-    navigator.clipboard.writeText(app.url);
-    this.setState({ urlChecked: true });
-  }
+  const getAppMemoryMetrics = useCallback(() => {
+    return formatAppMemoryMetrics(appID, appMemoryMetrics);
+  }, [appID, appMemoryMetrics]);
 
-  getAppCPUMetrics() {
-    const { appID } = this.props.match.params;
-    const { appCPUMetrics } = this.props;
-    const results = formatAppCPUMetrics(appID, appCPUMetrics);
-    return results;
-  }
+  const getAppCPUMetrics = useCallback(() => {
+    return formatAppCPUMetrics(appID, appCPUMetrics);
+  }, [appID, appCPUMetrics]);
 
-  getAppNetworkMetrics() {
-    const { appID } = this.props.match.params;
-    const { appNetworkMetrics } = this.props;
-    const results = formatAppNetworkMetrics(appID, appNetworkMetrics);
-    return results;
-  }
-  regenerate() {
-    const {
-      revertUrl,
-      match: { params },
-    } = this.props;
-    revertUrl(params.appID);
-  }
-  renderRedirect = () => {
-    const { isReverted, clearUrlRevertState } = this.props;
-    if (isReverted) {
-      clearUrlRevertState();
-      return <Redirect to={`/projects`} noThrow />;
-    }
+  const getAppNetworkMetrics = useCallback(() => {
+    return formatAppNetworkMetrics(appID, appNetworkMetrics);
+  }, [appID, appNetworkMetrics]);
+
+  const copyUrl = () => {
+    const appInfo = getAppInfo();
+    navigator.clipboard.writeText(appInfo.url);
+    setUrlChecked(true);
   };
 
-  handleRedeployApp = () => {
-    this.setState({ spin: true });
-    const {
-      match: { params },
-    } = this.props;
+  const regenerate = () => {
+    dispatch(revertUrl(appID));
+  };
 
-    const { appID } = params;
+  const handleRedeployApp = () => {
+    setSpin(true);
 
     handlePostRequestWithOutDataObject({}, `/apps/${appID}/redeploy`)
-      .then((response) => {
+      .then(() => {
         window.location.reload();
       })
       .catch((error) => {
@@ -182,23 +146,23 @@ class AppMetricsPage extends React.Component {
       });
   };
 
-  render() {
-    const { params } = this.props.match;
-    const { projectID, appID } = params;
-    // eslint-disable-next-line no-unused-vars
-    const { isReverting, isReverted } = this.props;
-    const { logs, logsError, fetchingLogs, urlChecked, redeploy, spin } =
-      this.state;
+  if (isReverted) {
+    dispatch(clearUrlRevertState());
+    return <Redirect to="/projects" noThrow />;
+  }
 
-    const formattedMemoryMetrics = this.getAppMemoryMetrics();
-    const formattedCPUMetrics = this.getAppCPUMetrics();
-    const formattedNetworkMetrics = this.getAppNetworkMetrics();
-    const appInfo = this.getAppInfo();
+  const formattedMemoryMetrics = getAppMemoryMetrics();
+  const formattedCPUMetrics = getAppCPUMetrics();
+  const formattedNetworkMetrics = getAppNetworkMetrics();
+  const appInfo = getAppInfo();
+
     return (
       <DashboardLayout
         name={appInfo.name}
         header={appInfo.name}
         viewAppLink={appInfo.url}
+        // specify text for jupiter notebook
+        viewAppLinkText={jupyterNotebook? "Open Notebook": "" }
       >
         <div
           className={
@@ -216,7 +180,7 @@ class AppMetricsPage extends React.Component {
             </div>
           ) : (
             <div className={styles.CardBodySection}>
-              <div className={styles.InnerCard}>
+              <div className={jupyterNotebook ? styles.NoteBookInnerCard : styles.InnerCard}>
                 <div className={styles.InnerCardSections}>
                   <div className={styles.InnerContentGrid}>
                     <div className={styles.InnerTitlesStart}>App Name</div>
@@ -231,7 +195,7 @@ class AppMetricsPage extends React.Component {
                         {redeploy ? (
                           <div
                             className={styles.Redeploy}
-                            onClick={this.handleRedeployApp}
+                            onClick={handleRedeployApp}
                           >
                             <RedeployICon />
                             <span className={styles.DeployText}>
@@ -251,7 +215,7 @@ class AppMetricsPage extends React.Component {
                             </div>
                             <div className={styles.CopierDiv}>
                               <div className={styles.Icons}>
-                                <CopyText onClick={this.copyUrl} />
+                                <CopyText onClick={copyUrl} />
                                 {urlChecked === true ? <Checked /> : null}
                               </div>
                             </div>
@@ -266,7 +230,7 @@ class AppMetricsPage extends React.Component {
                           <div
                             className={styles.InnerContentWarnText}
                             onClick={() => {
-                              this.regenerate();
+                              regenerate();
                             }}
                           >
                             Click to re-generate url
@@ -301,7 +265,9 @@ class AppMetricsPage extends React.Component {
                     <div className={styles.InnerContentAge}>{appInfo.age}</div>
                   </div>
                 </div>
-                <hr />
+                {!jupyterNotebook &&<>
+                  <hr />
+                
                 <div className={styles.InnerCardSections}>
                   <div className={styles.InnerContentGrid}>
                     <div className={styles.InnerTitlesEnd}>App Alias</div>
@@ -313,7 +279,7 @@ class AppMetricsPage extends React.Component {
                     <div className={styles.InnerTitlesEnd}>Port</div>
                     <div className={styles.InnerContentEnd}>{appInfo.port}</div>
                   </div>
-                </div>
+                </div> </>}
               </div>
             </div>
           )}
@@ -388,48 +354,5 @@ class AppMetricsPage extends React.Component {
       </DashboardLayout>
     );
   }
-}
 
-export const mapStateToProps = (state) => {
-  const { isFetchingAppMemory, appMemoryMetrics, appMemoryMessage } =
-    state.appMemoryReducer;
-  const { isFetchingCPU, appCPUMetrics, cpuMessage } = state.appCpuReducer;
-  const { appNetworkMetrics, isFetchingAppNetwork, appNetworkMessage } =
-    state.appNetworkReducer;
-  const { apps } = state.appsListReducer;
-  const { isReverting, isReverted } = state.revertUrlReducer;
-
-  return {
-    isFetchingAppMemory,
-    appMemoryMetrics,
-    appMemoryMessage,
-    isFetchingCPU,
-    appCPUMetrics,
-    cpuMessage,
-    appNetworkMetrics,
-    isFetchingAppNetwork,
-    appNetworkMessage,
-    apps,
-    isReverted,
-    isReverting,
-  };
-};
-
-const mapDispatchToProps = {
-  getAppMemory,
-  clearAppMemory,
-  getAppCPU,
-  getAppNetwork,
-  clearUrlRevertState,
-  revertUrl,
-};
-
-AppMetricsPage.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      projectID: PropTypes.string.isRequired,
-    }).isRequired,
-  }).isRequired,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(AppMetricsPage);
+export default (AppMetricsPage);
