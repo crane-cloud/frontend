e0684d7567230d98eede713f9befcd763082709e
rhodinemma <rhodinemma10@gmail.com>
2024-10-16 15:26:40 +0300

    chore: limit number of replicas to 3

--- src/components/CreateApp/index.js
+++ src/components/CreateApp/index.js
@@ -30,7 +30,6 @@ const replicaOptions = [
   { id: 1, name: "1" },
   { id: 2, name: "2" },
   { id: 3, name: "3" },
-  { id: 4, name: "4" },
 ];
 class CreateApp extends React.Component {
   constructor(props) {
