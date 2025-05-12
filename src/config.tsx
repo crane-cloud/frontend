8a2de9cc1ad091db7454bb4c61b35debf6da1eed
rhodinemma <rhodinemma10@gmail.com>
2025-05-12 09:24:13 +0300

    chore: user registration, github oauth and forgot password functionality

--- src/config.tsx
+++ src/config.tsx
@@ -7,13 +7,12 @@ export const STATUS_MONITORING_URL = import.meta.env.VITE_APP_MONITORING_APP;
 //mira Url
 export const MIRA_API_URL = import.meta.env.VITE_APP_MIRA_API_URL;
 //github auth urls
-export const GIT_REDIRECT_URL = `https://github.com/login/oauth/authorize?client_id=${import.meta.env.VITE_APP_GITHUB_CLEINT_ID}&scope=read:user,user:email`;
+export const GIT_REDIRECT_URL = `https://github.com/login/oauth/authorize?client_id=${import.meta.env.VITE_APP_GITHUB_CLIENT_ID}&scope=read:user,user:email`;
 export const FLUTTER_WAVE_PUBLIC_KEY = import.meta.env
   .VITE_APP_FLUTTERWAVE_PUBLIC_KEY_TESTING;
 //for live exchange rate
 export const LIVE_EXCHANGE_RATE_API = `https://openexchangerates.org/api/latest.json?app_id=${import.meta.env.VITE_APP_EXCHANGE_RATE_KEY}`;
-export const GITLAB_URL =
-  "https://gitlab-ce-32b64b3f-fc91-4331-bf7e.cranecloud.io/users/sign_in";
+export const GITLAB_URL ="https://gitlab.cranecloud.io/users/sign_in";
 export const GOPLAYGROUND_URL = "https://go.dev.cranecloud.io/";
 export const MIRA_DOCS_URL =
   "https://docs.cranecloud.io/applications/deployWithMira/";
--- src/pages/Auth/loginPage.tsx
+++ src/pages/Auth/loginPage.tsx
@@ -5,6 +5,7 @@ import {
   Divider,
   Group,
   Loader,
+  Modal,
   Paper,
   PaperProps,
   PasswordInput,
@@ -12,11 +13,11 @@ import {
   Text,
   TextInput,
 } from "@mantine/core";
-import { FaGithub, FaGoogle } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 import { useForm } from "@mantine/form";
 import { upperFirst, useToggle } from "@mantine/hooks";
 import usePost from "@/utils/usePost";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "@/utils/AuthContext";
 import { useNavigate } from "react-router-dom";
 import {
@@ -24,16 +25,26 @@ import {
   MdOutlineLock,
   MdDriveFileRenameOutline,
   MdOutlinePerson,
+  MdOutlineBusiness,
 } from "react-icons/md";
 import { GIT_REDIRECT_URL } from "@/config";
 import { GuestHeader } from "@/components/Header";
 import { GuestFooter } from "@/components/Footer";
+import { API_USERS } from "@/utils/apis";
 
 export function LoginForm(props: PaperProps) {
-  const [type, toggle] = useToggle(["login", "register"]);
-  const { uploadData, submitting, success, data } = usePost();
   const { login } = useAuth();
   const navigate = useNavigate();
+
+  const [type, toggle] = useToggle(["login", "register"]);
+  const [passwordReset, setShowPasswordReset] = useState(false);
+  const [resetLinkModalOpened, setResetLinkModalOpened] = useState(false)
+  const [registrationModalOpened, setRegistrationModalOpened] = useState(false);
+
+  const { uploadData: gitOAuth, submitting: gitLogin, success: gitLoginSuccess, data: gitUserDetails } = usePost()
+  const { uploadData: loginUser, submitting: loggingIn, success: loginSuccess, data: loginDetails } = usePost();
+  const { uploadData: registerUser, submitting: registering, success: registerSuccess } = usePost();
+  const { uploadData: resetPassword, submitting: sendingResetLink, success: linkSentSuccess } = usePost(); 
 
   const form = useForm({
     initialValues: {
@@ -41,6 +52,7 @@ export function LoginForm(props: PaperProps) {
       name: "",
       username: "",
       password: "",
+      organisation: "",
       confirmPassword: "",
       terms: true,
     },
@@ -60,30 +72,101 @@ export function LoginForm(props: PaperProps) {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (form.validate().hasErrors) {
-      return;
+
+    if(type === "login"){
+      const emailError = form.validateField('email');
+      const passwordError = form.validateField('password');
+
+      if (emailError.hasError || passwordError.hasError) return;
+
+      loginUser({
+        api: `${API_USERS}/login`,
+        params: {
+          email: form.values.email,
+          password: form.values.password
+        },
+        successMessage: "Login successful",
+        errorMessage: "Login failed",
+      })
+    } else {
+        if(form.validate().hasErrors) return;
+
+        registerUser({
+          api: `${API_USERS}`,
+          params: {
+            email: form.values.email,
+            name: form.values.name,
+            organisation: form.values.organisation,
+            password: form.values.password
+          },
+          successMessage: "Account created successfully",
+          errorMessage: "Account registration failed",
+        })
     }
-    uploadData({
-      api: "/users/login",
-      params: form.values,
-      successMessage: "Login successful",
-      errorMessage: "Login failed",
-    });
   };
 
+  const handlePasswordReset = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+   
+    const emailError = form.validateField('email');
+    if (emailError.hasError) return;
+
+    resetPassword({
+      api: `${API_USERS}/forgot_password`,
+      params: { email: form.values.email },
+      successMessage: "Reset link sent",
+      errorMessage: "Failed to send reset link",
+    })
+  }
+
+  const initiateGitHubLogin = (code: string) => {
+    gitOAuth({
+      api: `${API_USERS}/oauth`,
+      params: { code },
+      successMessage: "Git user login successful",
+      errorMessage: "Failed to authorize git user",
+    })
+  }
+
   useEffect(() => {
-    if (success) {
-      login(data);
+    if (loginSuccess && !passwordReset) {
+      login(loginDetails);
       navigate("/");
     }
-  }, [success]);
+  }, [loginSuccess]);
+
+  useEffect(() => {
+    if (registerSuccess) {
+      setRegistrationModalOpened(true);
+      form.reset();
+      toggle();
+    }
+  }, [registerSuccess]);
+
+  useEffect(() => {
+    if(linkSentSuccess){
+      setResetLinkModalOpened(true)
+    }
+  },[linkSentSuccess])
+
+  useEffect(() => {
+    if(gitLoginSuccess){
+      login(gitUserDetails);
+      navigate("/");
+    }
+  },[gitLoginSuccess])
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.search);
+    const code = queryParams.get("code");
+    localStorage.clear(); 
+    if (code) {
+      initiateGitHubLogin(code);
+    }
+  }, []);
 
   const handleGithubAuth = () => {
     window.location.href = GIT_REDIRECT_URL;
-  };
-
-  const handleGoogleAuth = () => {
-    window.location.href = "";
   };
 
   return (
@@ -95,148 +178,249 @@ export function LoginForm(props: PaperProps) {
         withBorder
         {...props}
       >
-        <Text
-          variant="gradient"
-          gradient={{ from: "blue", to: "cyan", deg: 90 }}
-          size="xl"
-          fw={700}
-          ta="center"
-        >
-          Welcome {type === "login" && "back"} to Cranecloud
-        </Text>
-        <Group justify="center" mt="lg" gap="sm">
-          <Button
-            radius="xl"
-            leftSection={<FaGithub />}
-            color="theme.black"
-            variant="default"
-            style={{ borderColor: "theme.black" }}
-            onClick={handleGithubAuth}
-            flex={1}
+         {!passwordReset ? (
+          <>
+          <Text
+            variant="gradient"
+            gradient={{ from: "blue", to: "cyan", deg: 90 }}
+            size="xl"
+            fw={700}
+            ta="center"
           >
-            Github
-          </Button>
-          <Button
-            radius="xl"
-            flex={1}
-            leftSection={
-              <FaGoogle
-                style={{
-                  color: "#EA4335",
-                }}
-              />
-            }
-            variant="default"
-            style={{ borderColor: "theme.red" }}
-            onClick={handleGoogleAuth}
-          >
-            Google
-          </Button>
-        </Group>
-        <Divider
-          label="Or continue with email"
-          labelPosition="center"
-          my="lg"
-        />
-        <form onSubmit={handleSubmit}>
-          <Stack gap="sm">
-            {type === "register" && (
-              <Stack>
-                <TextInput
-                  required
-                  label="Name"
-                  placeholder="Your name"
-                  {...form.getInputProps("name")}
-                  radius="sm"
-                  color="blue"
-                  leftSection={<MdDriveFileRenameOutline />}
-                />
-                <TextInput
-                  required
-                  label="Username"
-                  placeholder="Your username"
-                  {...form.getInputProps("username")}
-                  radius="sm"
-                  color="blue"
-                  leftSection={<MdOutlinePerson />}
-                />
-              </Stack>
-            )}
-            <TextInput
-              required
-              label="Email"
-              placeholder="Email Address"
-              {...form.getInputProps("email")}
-              error={form.errors.email && "Invalid email"}
-              radius="sm"
-              color="blue"
-              leftSection={<MdOutlineEmail />}
-            />
-
-            <PasswordInput
-              required
-              label="Password"
-              placeholder="Your password"
-              {...form.getInputProps("password")}
-              radius="sm"
-              color="blue"
-              leftSection={<MdOutlineLock />}
-            />
-            {type === "register" && (
-              <Stack>
-                <PasswordInput
-                  required
-                  label="Confirm Password"
-                  placeholder="Confirm your password"
-                  {...form.getInputProps("confirmPassword")}
-                  radius="sm"
-                  color="blue"
-                  leftSection={<MdOutlineLock />}
-                />
-                <Checkbox
-                  required
-                  label="I agree to the terms and conditions"
-                  checked={form.values.terms}
-                  onChange={(event) =>
-                    form.setFieldValue("terms", event.currentTarget.checked)
-                  }
-                />
-              </Stack>
-            )}
-          </Stack>
-
-          <Stack mt="xl">
-            <Group justify="space-between">
-              <Anchor
-                component="button"
-                type="button"
-                c="dimmed"
-                onClick={() => toggle()}
-                size="xs"
-              >
-                {type === "register"
-                  ? "Already have an account? Login"
-                  : "Don't have an account? Register"}
-              </Anchor>
-              {type === "login" && (
-                <Anchor component="button" type="button" size="sm">
-                  Forgot password?
-                </Anchor>
-              )}
-            </Group>
+            Welcome {type === "login" && "back"} to Crane Cloud
+          </Text>
+          <Group justify="center" mt="lg" gap="sm">
             <Button
-              type="submit"
-              variant="gradient"
-              gradient={{ from: "blue", to: "cyan", deg: 90 }}
+              radius="xl"
+              leftSection={<FaGithub />}
+              color="theme.black"
+              variant="default"
+              style={{ borderColor: "theme.black" }}
+              onClick={handleGithubAuth}
+              flex={1}
             >
-              {submitting ? (
+              {gitLogin ? (
                 <Loader size="sm" color="white" />
               ) : (
-                upperFirst(type)
-              )}
+              "Continue with GitHub"
+            )}
             </Button>
+            {/* <Button
+              radius="xl"
+              flex={1}
+              leftSection={
+                <FaGoogle
+                  style={{
+                    color: "#EA4335",
+                  }}
+                />
+              }
+              variant="default"
+              style={{ borderColor: "theme.red" }}
+              onClick={handleGoogleAuth}
+            >
+              Google
+            </Button> */}
+          </Group>
+          <Divider
+            label="Or continue with email"
+            labelPosition="center"
+            my="lg"
+          />
+          <form onSubmit={handleSubmit}>
+            <Stack gap="sm">
+              {type === "register" && (
+                <Stack>
+                  <TextInput
+                    required
+                    label="Name"
+                    placeholder="Your name"
+                    {...form.getInputProps("name")}
+                    radius="sm"
+                    color="blue"
+                    leftSection={<MdDriveFileRenameOutline />}
+                  />
+                  <TextInput
+                    required
+                    label="Username"
+                    placeholder="Your username"
+                    {...form.getInputProps("username")}
+                    radius="sm"
+                    color="blue"
+                    leftSection={<MdOutlinePerson />}
+                  />
+
+                  <TextInput
+                    required
+                    label="Organisation"
+                    placeholder="Your organisation"
+                    {...form.getInputProps("organisation")}
+                    radius="sm"
+                    color="blue"
+                    leftSection={<MdOutlineBusiness />}
+                  />
+                </Stack>
+              )}
+              <TextInput
+                required
+                label="Email"
+                placeholder="Email Address"
+                {...form.getInputProps("email")}
+                error={form.errors.email && "Invalid email"}
+                radius="sm"
+                color="blue"
+                leftSection={<MdOutlineEmail />}
+              />
+
+              <PasswordInput
+                required
+                label="Password"
+                placeholder="Your password"
+                {...form.getInputProps("password")}
+                radius="sm"
+                color="blue"
+                leftSection={<MdOutlineLock />}
+              />
+              {type === "register" && (
+                <Stack>
+                  <PasswordInput
+                    required
+                    label="Confirm Password"
+                    placeholder="Confirm your password"
+                    {...form.getInputProps("confirmPassword")}
+                    radius="sm"
+                    color="blue"
+                    leftSection={<MdOutlineLock />}
+                  />
+                  <Checkbox
+                    required
+                    label="I agree to the terms and conditions"
+                    checked={form.values.terms}
+                    onChange={(event) =>
+                      form.setFieldValue("terms", event.currentTarget.checked)
+                    }
+                  />
+                </Stack>
+              )}
+            </Stack>
+
+            <Stack mt="xl">
+              <Group justify="space-between">
+                <Anchor
+                  component="button"
+                  type="button"
+                  c="dimmed"
+                  onClick={() => toggle()}
+                  size="xs"
+                >
+                  {type === "register"
+                    ? "Already have an account? Login"
+                    : "Don't have an account? Register"}
+                </Anchor>
+                {type === "login" && (
+                  <Anchor component="button" type="button" size="sm" onClick={() => setShowPasswordReset(true)}>
+                    Forgot password?
+                  </Anchor>
+                )}
+              </Group>
+              <Button
+                type="submit"
+                variant="gradient"
+                gradient={{ from: "blue", to: "cyan", deg: 90 }}
+              >
+                {loggingIn || registering ? (
+                  <Loader size="sm" color="white" />
+                ) : (
+                  upperFirst(type)
+                )}
+              </Button>
+            </Stack>
+          </form>
+          </>
+        ) : (
+          <Stack>
+            <Text
+              variant="gradient"
+              gradient={{ from: "blue", to: "cyan", deg: 90 }}
+              size="xl"
+              fw={700}
+              ta="center"
+            >
+             Reset Your Password 
+            </Text>
+            <Text ta="center" size="sm" c="dimmed">
+              Enter your email address so we can send you a link to reset your password.
+            </Text>
+  
+            <form onSubmit={handlePasswordReset}>
+              <Stack>
+                <TextInput
+                  required
+                  label="Email Address"
+                  placeholder="you@example.com"
+                  {...form.getInputProps("email")}
+                  leftSection={<MdOutlineEmail />}
+                />
+                <Button
+                  type="submit"
+                  variant="gradient"
+                  gradient={{ from: "blue", to: "cyan", deg: 90 }}
+                >
+                  {sendingResetLink ? <Loader size="sm" color="white" /> : "Reset"}
+                </Button>
+                <Anchor
+                  component="button"
+                  type="button"
+                  c="dimmed"
+                  onClick={() => setShowPasswordReset(false)}
+                  size="xs"
+                >
+                  Back to Login
+                </Anchor>
+              </Stack>
+            </form>
           </Stack>
-        </form>
+        )}
+
+            <Modal
+              opened={registrationModalOpened}
+              onClose={() => {setRegistrationModalOpened(false); form.setFieldValue('email', '');}}
+              title={<Text fw={700}>Registration Successful</Text>}
+              centered
+              size="md"
+            >
+              <div>
+                <Text>
+                  We've sent a link to your email address:{" "}
+                  <strong>{form.values.email}</strong>.
+                  <br />
+                  <br />
+                  The link will expire after 24 hours. Please use this link to activate and
+                  start using your account.
+                </Text>
+              </div>
+            </Modal>
+
+            <Modal
+              opened={resetLinkModalOpened}
+              onClose={() => setResetLinkModalOpened(false)}
+              title={<Text fw={700}>Password Reset Link</Text>}
+              centered
+              size="md"
+            >
+              <div>
+                <h2></h2>
+                <Text>
+                  We&apos;ve sent a link to your email address to create a new
+                  password:{" "}
+                  <strong>{form.values.email}</strong>.
+                  <br />
+                  <br />
+                  The link will expire after 24 hours. Please use this link to
+                  update password and resume using your account.
+                </Text>
+              </div>
+            </Modal>
       </Paper>
     </Stack>
   );
