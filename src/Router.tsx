6ce3165a1d9a4d1a2abde4b567d32a972470751c
Rhodin Nagwere <63339234+rhodinemma@users.noreply.github.com>
2025-06-13 13:48:25 +0300

    fix: create experiment user action (#969)

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { LoginPage } from "./pages/Auth/loginPage";
 import CreateNewPassword from "./pages/Auth/passwordResetPage";
-import ExperiementsDetailsPage from "./pages/Experirments/ExperiementsDetailsPage";
-import RunsDetailsPage from "./pages/Experirments/RunsDetailsPage";
+import ExperimentsDetailsPage from "./pages/Experiments/ExperimentsDetailsPage";
+import RunsDetailsPage from "./pages/Experiments/RunsDetailsPage";
 import UserProfilePage from "./pages/Users/UserProfilePage";
 import AccountVerification from "./pages/Auth/accountVerificationPage";
 import AppMetrics from "./pages/Apps/AppMetrics";
@@ -32,8 +32,8 @@ const DatabaseDetails = React.lazy(
 const AppSettingsPage = React.lazy(
   () => import("./pages/Apps/AppSettingsPage"),
 );
-const ExperiementsListPage = React.lazy(
-  () => import("./pages/Experirments/ExperiementsListPage"),
+const ExperimentsListPage = React.lazy(
+  () => import("./pages/Experiments/ExperimentsListPage"),
 );
 const ProjectDetailsPage = React.lazy(
   () => import("./pages/Projects/ProjectDetailsPage"),
@@ -80,11 +80,11 @@ export const DashboardRoutes = [
   // Experiments
   {
     path: "/projects/:project_id/apps/:app_id/experiments",
-    element: <ExperiementsListPage />,
+    element: <ExperimentsListPage />,
   },
   {
     path: "/projects/:project_id/apps/:app_id/experiments/:experiment_id",
-    element: <ExperiementsDetailsPage />,
+    element: <ExperimentsDetailsPage />,
   },
   {
     path: "/projects/:project_id/apps/:app_id/experiments/:experiment_id/runs/:run_id",
