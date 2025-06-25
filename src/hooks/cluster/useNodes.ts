e201e455fc46408e73b7463da6045fd22535f681
jim-junior <jimjunior854@gmail.com>
2025-06-25 12:39:37 +0300

    fix: code `useNodeList` failing Cognitive Complexity test

--- src/hooks/cluster/useNodes.ts
+++ src/hooks/cluster/useNodes.ts
@@ -13,6 +13,32 @@ const useNodeList = ({ cluster_id }: any) => {
     { id: "conditions", header: "Conditions" },
   ];
 
+  const getRole = (labels: any) => {
+    if (!labels) {
+      return "Unknown";
+    }
+    const keys = Object.keys(labels);
+    if (keys.includes("node-role.kubernetes.io/control-plane")) {
+      return "Control Plane";
+    }
+    return "Worker";
+  };
+
+  const isNodeReady = (conditions: any[]) => {
+    return conditions?.find((condition) => condition.type === "Ready")
+      ? "Ready"
+      : "Not Ready";
+  };
+
+  const parseNode = (item: any) => ({
+    name: item?.metadata?.name,
+    taints: item?.spec?.taints?.length,
+    roles: getRole(item?.metadata?.labels),
+    version: item?.status?.nodeInfo?.kubeletVersion,
+    age: moment(item?.metadata?.creationTimestamp).fromNow(),
+    conditions: isNodeReady(item?.status?.conditions),
+  });
+
   const tableData = (data: any) => {
     if (!data) {
       return [];
@@ -20,27 +46,7 @@ const useNodeList = ({ cluster_id }: any) => {
     if (!Array.isArray(data)) {
       return [];
     }
-    return data.map((item: any) => {
-      const row = {
-        // ...item,
-        name: item?.metadata?.name,
-        taints: item?.spec?.taints?.length,
-        roles: Object.keys(item?.metadata?.labels).find((key) => {
-          return key === "node-role.kubernetes.io/control-plane";
-        })
-          ? "Control Plane"
-          : "Worker",
-
-        version: item?.status?.nodeInfo?.kubeletVersion,
-        age: moment(item?.metadata?.creationTimestamp).fromNow(),
-        conditions: item?.status?.conditions.find((condition: any) => {
-          return condition.type === "Ready";
-        })
-          ? "Ready"
-          : "Not Ready",
-      };
-      return row;
-    });
+    return data.map((item: any) => parseNode(item));
   };
   const dataParent = "nodes";
   const apiRoute = `/clusters/${cluster_id}/nodes`;
