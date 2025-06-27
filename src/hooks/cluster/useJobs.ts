980e722462566238daaa23ab2be144e9facbdfea
Manzi.alex <alexbagabe@gmail.com>
2025-06-27 12:19:41 +0300

    update on the job lsit table columns

--- src/hooks/cluster/useJobs.ts
+++ src/hooks/cluster/useJobs.ts
@@ -1,13 +1,38 @@
-import { useSetAdminClusterSidebar } from "../../utils/helpers";
+import { useSetAdminClusterSidebar } from "@/utils/helpers";
 import moment from "moment";
+
+const getRole = (labels: any) => {
+  if (!labels) {
+    return "Unknown";
+  }
+  const keys = Object.keys(labels);
+  if (keys.includes("job-role.kubernetes.io/control-plane")) {
+    return "Control Plane";
+  }
+  return "not yet";
+};
+
+const isJobReady = (conditions: any[]) => {
+  return conditions?.find((condition) => condition.type === "Ready")
+    ? "Ready"
+    : "Not Ready";
+};
+
+const parseNode = (item: any) => ({
+  name: item?.metadata?.name,
+  namespace: item?.metadata?.namespace?.length,
+  completion: getRole(item?.metadata?.labels),
+  age: moment(item?.metadata?.creationTimestamp).fromNow(),
+  conditions: isJobReady(item?.status?.conditions),
+});
 
 const useJobs = ({ cluster_id }: any) => {
   useSetAdminClusterSidebar();
 
   const tableColumns = () => [
     { id: "name", header: "Name" },
-    { id: "namespace", header: "Namespace" },
-    { id: "completion", header: "Completion" },
+    { id: "namespace", header: "namespace" },
+    { id: "completion", header: "completion" },
     { id: "age", header: "Age" },
     { id: "conditions", header: "Conditions" },
   ];
@@ -19,17 +44,7 @@ const useJobs = ({ cluster_id }: any) => {
     if (!Array.isArray(data)) {
       return [];
     }
-    return data.map((item: any) => {
-      const row = {
-        // ...item,
-        name: item?.metadata?.name,
-        namespace: item?.metadata?.namespace,
-        completion: moment(item?.metadata?.completion).fromNow(),
-        age: moment(item?.metadata?.creationTimestamp).fromNow(),
-        conditions: moment(item?.metadata?.conditions).fromNow(),
-      };
-      return row;
-    });
+    return data.map((item: any) => parseNode(item));
   };
   const dataParent = "jobs";
   const apiRoute = `/clusters/${cluster_id}/jobs`;
