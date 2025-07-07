21fdd6ba419f6a6c5d53fa2a86cfa75fb70560c1
Joanitah-jai <joanitahnankya555@gmail.com>
2025-07-07 09:39:33 +0300

    Edited code

--- src/hooks/cluster/useStorageClasses.ts
+++ src/hooks/cluster/useStorageClasses.ts
@@ -1,40 +1,37 @@
 import { useSetAdminClusterSidebar } from "@/utils/helpers";
 import moment from "moment";
 
+const extractTableRow = (item: any) => {
+  const isDefault =
+    item?.metadata?.annotations?.[
+      "storageclass.kubernetes.io/is-default-class"
+    ] === "true";
+
+  return {
+    name: item?.metadata?.name,
+    provisioner: item?.provisioner,
+    reclaimPolicy: item?.reclaimPolicy,
+    default: isDefault ? "Yes" : "No",
+    age: moment(item?.metadata?.creationTimestamp).fromNow(),
+  };
+};
+
+const tableColumns = () => [
+  { id: "name", header: "Name" },
+  { id: "provisioner", header: "Provisioner" },
+  { id: "reclaimPolicy", header: "Reclaim Policy" },
+  { id: "default", header: "Default" },
+  { id: "age", header: "Age" },
+];
+
 const useStorageClasses = ({ cluster_id }: any) => {
   useSetAdminClusterSidebar();
 
-  const tableColumns = () => [
-    { id: "name", header: "Name" },
-    { id: "provisioner", header: "Provisioner" },
-    { id: "reclaimPolicy", header: "Reclaim Policy" },
-    { id: "default", header: "Default" },
-    { id: "age", header: "Age" },
-  ];
-
   const tableData = (data: any) => {
-    if (!data) {
-      return [];
-    }
-    if (!Array.isArray(data)) {
-      return [];
-    }
-    return data.map((item: any) => {
-      const isDefault =
-        item?.metadata?.annotations?.[
-          "storageclass.kubernetes.io/is-default-class"
-        ] === "true";
-      const row = {
-        // ...item,
-        name: item?.metadata?.name,
-        provisioner: item?.provisioner,
-        reclaimPolicy: item?.reclaimPolicy,
-        default: isDefault ? "Yes" : "No", // 👈 You can customize this label
-        age: moment(item?.metadata?.creationTimestamp).fromNow(),
-      };
-      return row;
-    });
+    if (!Array.isArray(data)) return [];
+    return data.map(extractTableRow);
   };
+
   const dataParent = "storage_classes";
   const apiRoute = `/clusters/${cluster_id}/storage_classes`;
 
