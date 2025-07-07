042de82a55267c8c06ab1ca0fef32e84aaac9be1
Mubangizi Allan <mubangizia22@gmail.com>
2025-07-07 13:52:29 +0300

    fix failing error

--- src/hooks/cluster/useStorageClasses.ts
+++ src/hooks/cluster/useStorageClasses.ts
@@ -1,35 +1,36 @@
 import { useSetAdminClusterSidebar } from "@/utils/helpers";
 import moment from "moment";
 
-const extractTableRow = (item: any) => {
-  const isDefault =
-    item?.metadata?.annotations?.[
-      "storageclass.kubernetes.io/is-default-class"
-    ] === "true";
-
-  return {
-    name: item?.metadata?.name,
-    provisioner: item?.provisioner,
-    reclaimPolicy: item?.reclaimPolicy,
-    default: isDefault ? "Yes" : "No",
-    age: moment(item?.metadata?.creationTimestamp).fromNow(),
-  };
-};
-
-const tableColumns = () => [
-  { id: "name", header: "Name" },
-  { id: "provisioner", header: "Provisioner" },
-  { id: "reclaimPolicy", header: "Reclaim Policy" },
-  { id: "default", header: "Default" },
-  { id: "age", header: "Age" },
-];
-
 const useStorageClasses = ({ cluster_id }: any) => {
   useSetAdminClusterSidebar();
 
+  const tableColumns = () => [
+    { id: "name", header: "Name" },
+    { id: "provisioner", header: "Provisioner" },
+    { id: "reclaimPolicy", header: "Reclaim Policy" },
+    { id: "default", header: "Default" },
+    { id: "age", header: "Age" },
+  ];
+
   const tableData = (data: any) => {
-    if (!Array.isArray(data)) return [];
-    return data.map(extractTableRow);
+    if (!data || !Array.isArray(data)) {
+      return [];
+    }
+
+    return data.map((item) => {
+      const isDefault =
+        item?.metadata?.annotations?.[
+          "storageclass.kubernetes.io/is-default-class"
+        ] === "true";
+
+      return {
+        name: item?.metadata?.name,
+        provisioner: item?.provisioner,
+        reclaimPolicy: item?.reclaimPolicy,
+        default: isDefault ? "Yes" : "No",
+        age: moment(item?.metadata?.creationTimestamp).fromNow(),
+      };
+    });
   };
 
   const dataParent = "storage_classes";
