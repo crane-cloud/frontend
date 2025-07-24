760180142f4da0eacf169240289fb1f0c8371cf7
Mubangizi Allan <mubangizia22@gmail.com>
2025-07-24 11:46:55 +0300

    remove eslint ignore file

--- eslint.config.js
+++ eslint.config.js
@@ -6,31 +6,42 @@ import eslintPluginPrettier from "eslint-plugin-prettier";
 import eslintPluginReact from "eslint-plugin-react";
 import typescriptEslintParser from "@typescript-eslint/parser";
 
-export default tseslint.config(...mantine, {
-  ignores: [
-    "**/*.{mjs,cjs,js,d.ts,d.mts}",
-    "./.storybook/main.ts",
-    "node_modules",
-    "build",
-    "dist",
-    "public",
-  ],
-  languageOptions: {
-    parser: typescriptEslintParser,
-    ecmaVersion: 2020,
-  },
-  plugins: {
-    "react-hooks": reactHooks,
-    "react-refresh": reactRefresh,
-    prettier: eslintPluginPrettier,
-    react: eslintPluginReact,
-  },
-  rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-      },
+export default tseslint.config(
+  {
+    ignores: [
+      "dist/",
+      "dist/**",
+      "build/",
+      "build/**",
+      "node_modules/",
+      "node_modules/**",
+      "public/",
+      "public/**",
+      "**/*.{mjs,cjs,js,d.ts,d.mts}",
+      ".storybook/main.ts",
+      "vite-env.d.ts",
+      "*.config.js",
     ],
   },
-});
+  ...mantine,
+  {
+    languageOptions: {
+      parser: typescriptEslintParser,
+      ecmaVersion: 2020,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier: eslintPluginPrettier,
+      react: eslintPluginReact,
+    },
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+        },
+      ],
+    },
+  }
+);
